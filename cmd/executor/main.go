3eda200e7d8ef37013049e1d08a81a2f93aa4bdf
JamesMurkin <grdevelopment\jamesmu@a-1n6ucdokcbzv4.gr.development>
2019-07-05 10:56:57 +0100

    Removing unused createPod from executor/main
    This is just cluttering the file and is never used (And if it was used, should never be placed here)

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -6,17 +6,12 @@ import (
 	"github.com/G-Research/k8s-batch/internal/service"
 	"github.com/G-Research/k8s-batch/internal/startup"
 	"github.com/G-Research/k8s-batch/internal/submitter"
-	"github.com/oklog/ulid"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	v12 "k8s.io/client-go/informers/core/v1"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
-	"math/rand"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -58,41 +53,6 @@ func main() {
 		time.Sleep(5 * time.Second)
 
 		kubernetesAllocationService.FillInSpareClusterCapacity()
-	}
-}
-
-func createPod(kubernetesClient kubernetes.Interface) {
-	terminationGracePeriod := int64(0)
-
-	t := time.Now()
-	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-
-	pod := v1.Pod {
-		ObjectMeta: metav1.ObjectMeta {
-			Name: "test-" + strings.ToLower(ulid.MustNew(ulid.Timestamp(t), entropy).String()),
-		},
-		Spec: v1.PodSpec {
-			TerminationGracePeriodSeconds: &terminationGracePeriod,
-			RestartPolicy: v1.RestartPolicyNever,
-			Containers: []v1.Container {
-				{
-					Name: "sleeper",
-					Image: "mcr.microsoft.com/dotnet/core/runtime:2.2",
-					ImagePullPolicy: v1.PullIfNotPresent,
-					Args: []string {
-						"sleep", "20s",
-					},
-				},
-			},
-		},
-	}
-
-	result, err := kubernetesClient.CoreV1().Pods("default").Create(&pod)
-
-	if err != nil {
-		fmt.Printf("Failed creating pod %s\n", pod.ObjectMeta.Name)
-	} else {
-		fmt.Printf("Created pod %s\n", result.ObjectMeta.Name)
 	}
 }
 
