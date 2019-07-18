5b83c259f7b540e221bd1f9f73a355da4e93e557
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2019-07-18 09:59:44 +0100

    Graceful shutdown.

--- internal/armada/server.go
+++ internal/armada/server.go
@@ -13,31 +13,35 @@ import (
 )
 
 func Serve(config *configuration.ArmadaConfig) (*grpc.Server, *sync.WaitGroup) {
-	db := redis.NewClient(&redis.Options{
-		Addr:     config.Redis.Addr,
-		Password: config.Redis.Password,
-		DB:       config.Redis.DB,
-	})
-
-	jobRepository := &repository.RedisJobRepository{ Db: db }
-	submitServer := &service.SubmitServer{ JobRepository: jobRepository }
-	aggregatedQueueServer := &service.AggregatedQueueServer{ JobRepository: jobRepository }
-
-	lis, err := net.Listen("tcp", config.GrpcPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	server := grpc.NewServer()
-	api.RegisterSubmitServer(server, submitServer)
-	api.RegisterAggregatedQueueServer(server, aggregatedQueueServer)
-
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
+	server := grpc.NewServer()
 	go func () {
+		log.Printf("Grpc listening on %s", config.GrpcPort)
+		defer log.Println("Stopping server.")
+
+		db := redis.NewClient(&redis.Options{
+			Addr:     config.Redis.Addr,
+			Password: config.Redis.Password,
+			DB:       config.Redis.Db,
+		})
+
+		jobRepository := &repository.RedisJobRepository{ Db: db }
+		submitServer := &service.SubmitServer{ JobRepository: jobRepository }
+		aggregatedQueueServer := &service.AggregatedQueueServer{ JobRepository: jobRepository }
+
+		lis, err := net.Listen("tcp", config.GrpcPort)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		api.RegisterSubmitServer(server, submitServer)
+		api.RegisterAggregatedQueueServer(server, aggregatedQueueServer)
+
 		if err := server.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
+
 		wg.Done()
 	} ()
 	return server, wg
