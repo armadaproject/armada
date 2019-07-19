4dd7502ebea4fc6a55955737426c879558f47a6d
JamesMurkin <jamesmurkin@hotmail.com>
2019-07-19 14:34:41 +0100

    Handling shutdown gracefully
    In the case of SIGINT, SIGTERM and panic the application now attempts to shutdown gracefully
    Stopping all background threads + closing the gRPC connections
    
    I still need to handle stopping JobEventReporter but I am just about to rework that class so it'll become clearer once that has happened the best way to shut it down

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"github.com/G-Research/k8s-batch/internal/executor/domain"
 	"github.com/G-Research/k8s-batch/internal/executor/startup"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func main() {
 	configuration := startup.LoadConfiguration()
-	startup.StartUp(configuration)
+
+	wg, shutdownChannel := startup.StartUp(configuration)
+
+	signal.Notify(shutdownChannel, syscall.SIGINT, syscall.SIGTERM)
+	defer handleGracefulShutdownDuringPanic(wg, shutdownChannel)
+
+	wg.Wait()
+}
+
+func handleGracefulShutdownDuringPanic(wg *sync.WaitGroup, shutdownChannel chan os.Signal) {
+	if err := recover(); err != nil {
+		sig := domain.PanicSignal{}
+		shutdownChannel <- sig
+		wg.Wait()
+		panic(err)
+	}
 }
--- internal/executor/startup/application.go
+++ internal/executor/startup/application.go
@@ -9,16 +9,16 @@ import (
 	"github.com/G-Research/k8s-batch/internal/executor/submitter"
 	"google.golang.org/grpc"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	informer "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
-func StartUp(config configuration.Configuration) {
+func StartUp(config configuration.Configuration) (*sync.WaitGroup, chan os.Signal) {
 	kubernetesClient, err := CreateKubernetesClientWithDefaultConfig(config.Application.InClusterDeployment)
 	if err != nil {
 		fmt.Println(err)
@@ -29,11 +29,11 @@ func StartUp(config configuration.Configuration) {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
 
 	queueClient := api.NewAggregatedQueueClient(conn)
 	usageClient := api.NewUsageClient(conn)
 
+	//TODO Decide how to gracefully stop event reporter
 	var eventReporter reporter.EventReporter = reporter.New(
 		kubernetesClient,
 		queueClient,
@@ -41,13 +41,11 @@ func StartUp(config configuration.Configuration) {
 		config.Events.EventReportingBatchSize,
 	)
 
-	defer runtime.HandleCrash()
 	factory := informers.NewSharedInformerFactoryWithOptions(kubernetesClient, 0)
 	podInformer := factory.Core().V1().Pods()
 	nodeLister := factory.Core().V1().Nodes().Lister()
 	addPodEventHandler(podInformer, eventReporter)
 	informerStopper := startInformers(factory)
-	defer close(informerStopper)
 
 	jobSubmitter := submitter.JobSubmitter{KubernetesClient: kubernetesClient}
 
@@ -71,27 +69,60 @@ func StartUp(config configuration.Configuration) {
 		KubernetesClient: kubernetesClient,
 	}
 
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
 	tasks := make([]chan bool, 0)
-	tasks = append(tasks, scheduleBackgroundTask(clusterUtilisationService.ReportClusterUtilisation, config.Task.UtilisationReportingInterval))
-	tasks = append(tasks, scheduleBackgroundTask(jobLeaseService.RequestJobLeasesAndFillSpareClusterCapacity, config.Task.RequestNewJobsInterval))
-	tasks = append(tasks, scheduleBackgroundTask(jobLeaseService.RenewJobLeases, config.Task.JobLeaseRenewalInterval))
-	tasks = append(tasks, scheduleBackgroundTask(podCleanupService.ReportForgottenCompletedPods, config.Task.ForgottenCompletedPodReportingInterval))
-	tasks = append(tasks, scheduleBackgroundTask(podCleanupService.DeletePodsReadyForCleanup, config.Task.PodDeletionInterval))
+	tasks = append(tasks, scheduleBackgroundTask(clusterUtilisationService.ReportClusterUtilisation, config.Task.UtilisationReportingInterval, wg))
+	tasks = append(tasks, scheduleBackgroundTask(jobLeaseService.RequestJobLeasesAndFillSpareClusterCapacity, config.Task.RequestNewJobsInterval, wg))
+	tasks = append(tasks, scheduleBackgroundTask(jobLeaseService.RenewJobLeases, config.Task.JobLeaseRenewalInterval, wg))
+	tasks = append(tasks, scheduleBackgroundTask(podCleanupService.ReportForgottenCompletedPods, config.Task.ForgottenCompletedPodReportingInterval, wg))
+	tasks = append(tasks, scheduleBackgroundTask(podCleanupService.DeletePodsReadyForCleanup, config.Task.PodDeletionInterval, wg))
 	defer stopTasks(tasks)
 
-	for {
-		time.Sleep(10 * time.Second)
+	shutdown := make(chan os.Signal, 1)
+
+	go func() {
+		shutdownSignal := <-shutdown
+		fmt.Printf("Caught shutdown signal: %+v \n", shutdownSignal)
+
+		close(informerStopper)
+		conn.Close()
+
+		fmt.Println("Shutdown complete")
+		wg.Done()
+		if waitForShutdownCompletion(wg, 2*time.Second) {
+			fmt.Println("Graceful shutdown timed out")
+		}
+	}()
+
+	return wg, shutdown
+}
+
+func waitForShutdownCompletion(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
 	}
 }
 
-func scheduleBackgroundTask(task func(), interval time.Duration) chan bool {
+func scheduleBackgroundTask(task func(), interval time.Duration, wg *sync.WaitGroup) chan bool {
 	stop := make(chan bool)
 
+	wg.Add(1)
 	go func() {
 		for {
 			select {
 			case <-time.After(interval):
 			case <-stop:
+				wg.Done()
 				return
 			}
 			task()
