dcc4ca401d722d7fcefda0055f0a8ffdda784fe4
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2019-07-25 14:01:42 +0100

    Implement queue repository.

---
+++ internal/armada/repository/queue.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"github.com/G-Research/k8s-batch/internal/armada/api"
+	"github.com/go-redis/redis"
+	"github.com/gogo/protobuf/proto"
+)
+
+type Queue struct {
+}
+
+const queueHashKey = "Queue"
+
+type QueueRepository interface {
+	GetQueues() ([]*api.Queue, error)
+	CreateQueue(queue *api.Queue) error
+}
+
+type RedisQueueRepository struct {
+	db *redis.Client
+}
+
+func NewRedisQueueRepository(db *redis.Client) *RedisQueueRepository {
+	return &RedisQueueRepository{db: db}
+}
+
+func (r RedisQueueRepository) GetQueues() ([]*api.Queue, error) {
+	result, err := r.db.HGetAll(clusterReportKey).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	queues := make([]*api.Queue, 0)
+	for _, v := range result {
+		queue := &api.Queue{}
+		e := proto.Unmarshal([]byte(v), queue)
+		if e!= nil {
+			return nil, e
+		}
+		queues = append(queues, queue)
+	}
+	return queues, nil
+}
+
+func (r RedisQueueRepository) CreateQueue(queue *api.Queue) error {
+
+	data, e := proto.Marshal(queue)
+	if e != nil {
+		return e
+	}
+	result := r.db.HSet(queueHashKey, queue.Name, data)
+	return result.Err()
+}
