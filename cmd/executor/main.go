7ef35071397e7e4dcee7754594d0a1fb5d9d55b5
JamesMurkin <jamesmurkin@hotmail.com>
2019-07-30 13:38:12 +0100

    Allowing users to specify the location of their configuration file
    The existing config file found in /config/(armada|executor)/config is now going to be the "default" config
    
    Users can specify their own, via passing a command line argument --config that specifies the location of their config file, i.e:
     --config /some/location/config.yaml
    
    This will be merged with the default, with the user specified config overwriting the default

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -4,16 +4,27 @@ import (
 	"github.com/G-Research/k8s-batch/internal/common"
 	"github.com/G-Research/k8s-batch/internal/executor"
 	"github.com/G-Research/k8s-batch/internal/executor/configuration"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-func main() {
+const CustomConfigLocation string = "config"
 
+func init() {
+	pflag.String(CustomConfigLocation, "", "Fully qualified path to application configuration file")
+	pflag.Parse()
+}
+
+func main() {
 	common.ConfigureLogging()
+	common.BindCommandlineArguments()
+
 	var config configuration.ExecutorConfiguration
-	common.LoadConfig(&config, "./config/executor")
+	userSpecifiedConfig := viper.GetString(CustomConfigLocation)
+	common.LoadConfig(&config, "./config/executor", userSpecifiedConfig)
 
 	shutdownChannel := make(chan os.Signal, 1)
 	signal.Notify(shutdownChannel, syscall.SIGINT, syscall.SIGTERM)
--- internal/common/startup.go
+++ internal/common/startup.go
@@ -2,17 +2,43 @@ package common
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func LoadConfig(config interface{}, path string) {
+func BindCommandlineArguments() {
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		log.Error()
+		os.Exit(-1)
+	}
+}
+
+func LoadConfig(config interface{}, defaultPath string, customConfigPath string) {
 	viper.SetConfigName("config")
-	viper.AddConfigPath(path)
+	viper.AddConfigPath(defaultPath)
 	if err := viper.ReadInConfig(); err != nil {
 		log.Error(err)
 		os.Exit(-1)
 	}
+
+	if customConfigPath != "" {
+		dir, file := filepath.Split(customConfigPath)
+		fileName := strings.TrimSuffix(file, filepath.Ext(file))
+
+		viper.SetConfigName(fileName)
+		viper.AddConfigPath(dir)
+
+		err := viper.MergeInConfig()
+		if err != nil {
+			log.Error(err)
+			os.Exit(-1)
+		}
+	}
+
 	err := viper.Unmarshal(config)
 	if err != nil {
 		log.Error(err)
