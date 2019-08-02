70bab21bb4cde5c628b249766ec878fc1ecb69c6
JamesMurkin <jamesmurkin@hotmail.com>
2019-08-02 15:11:59 +0100

    Fixing kind dockerfile path

--- test/end_to_end/setup/setup_environment.sh
+++ test/end_to_end/setup/setup_environment.sh
@@ -6,10 +6,10 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 echo ">>> Installing Helm"
 curl -L https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz > helm.tar.gz
 tar -zxvf helm.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
 
 echo "Building kind"
-docker build -t kind:src . -f ${REPO_ROOT}/test/Dockerfile.kind
+docker build -t kind:src . -f ${REPO_ROOT}/test/end_to_end/setup/Dockerfile
 docker create -ti --name dummy kind:src sh
 docker cp dummy:/go/bin/kind ./kind
 docker rm -f dummy
