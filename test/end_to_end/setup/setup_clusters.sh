de04e97a0fc44f37a1e6f9c20256b41208734f6d
JamesMurkin <jamesmurkin@hotmail.com>
2019-08-05 11:18:23 +0100

    Generating executor components with Helm in circleci

--- test/end_to_end/setup/setup_clusters.sh
+++ test/end_to_end/setup/setup_clusters.sh
@@ -5,3 +5,4 @@ kind create cluster --config ${REPO_ROOT}/test/end_to_end/setup/kind/worker-mast
 kind load --name cluster1 docker-image ${ECR_REPOSITORY}/armada-executor:b91e651cba4ed065cbbbea5bf43ef688b40bd766
 kind get clusters
 KUBECONFIG=$(kind get kubeconfig-path --name=cluster1) kubectl get nodes
+KUBECONFIG=$(kind get kubeconfig-path --name=cluster1) helm template ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set executor.image.repository=${ECR_REPOSITORY}/armada-executor --set executor.image.tag=b91e651cba4ed065cbbbea5bf43ef688b40bd766
