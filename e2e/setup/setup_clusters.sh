e8161a61d69ddd9eb0bdabfbd04331f3794b4369
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-03 12:51:31 +0100

    Using current build docker image rather than hard coded

--- e2e/setup/setup_clusters.sh
+++ e2e/setup/setup_clusters.sh
@@ -2,7 +2,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 echo "Setting up cluster1"
 kind create cluster --config ${REPO_ROOT}/e2e/setup/worker-master-config.yaml --name cluster1 --wait 3m
-kind load --name cluster1 docker-image ${ECR_REPOSITORY}/armada-executor:b91e651cba4ed065cbbbea5bf43ef688b40bd766
+kind load --name cluster1 docker-image ${ECR_REPOSITORY}/armada-executor:branch-${CIRCLE_BRANCH}-${CIRCLE_SHA1}
 export KUBECONFIG=$(kind get kubeconfig-path --name=cluster1)
 kubectl create ns armada
-helm template ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set image.repository=${ECR_REPOSITORY}/armada-executor --set image.tag=b91e651cba4ed065cbbbea5bf43ef688b40bd766 | kubectl apply -f -
+helm template ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set image.repository=${ECR_REPOSITORY}/armada-executor --set image.tag=branch-${CIRCLE_BRANCH}-${CIRCLE_SHA1} | kubectl apply -f -
