274c23e306c605c1392df939ffc5e55059b38987
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-03 10:35:24 +0100

    Using helm install rather than template + apply

--- e2e/setup/setup_clusters.sh
+++ e2e/setup/setup_clusters.sh
@@ -4,4 +4,4 @@ echo "Setting up cluster1"
 kind create cluster --config ${REPO_ROOT}/e2e/setup/worker-master-config.yaml --name cluster1 --wait 3m
 kind load --name cluster1 docker-image ${ECR_REPOSITORY}/armada-executor:b91e651cba4ed065cbbbea5bf43ef688b40bd766
 export KUBECONFIG=$(kind get kubeconfig-path --name=cluster1)
-helm template ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set executor.image.repository=${ECR_REPOSITORY}/armada-executor --set executor.image.tag=b91e651cba4ed065cbbbea5bf43ef688b40bd766 | kubectl apply -f -
+helm install ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set executor.image.repository=${ECR_REPOSITORY}/armada-executor --set executor.image.tag=b91e651cba4ed065cbbbea5bf43ef688b40bd766
