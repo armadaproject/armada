51759af92ba20d6330f1a85dcbe8ceec3b3391f3
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-03 15:20:15 +0100

    No longer running executor in cluster
    As kind runs in its own network it cannot talk to the api
    
    Potentially options to look into in the future:
      - Run executor and api in cluster and communicate via internal cluster communication
        - Issues will be contacting redis + having multiple clusters
      - Wait for Kind to support --network=host
      - Use a Kind alternative
      - Sort out networking so they can communicate (This is probably a great option but probably complicated too)

--- e2e/setup/setup_clusters.sh
+++ e2e/setup/setup_clusters.sh
@@ -2,7 +2,3 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 echo "Setting up cluster1"
 kind create cluster --config ${REPO_ROOT}/e2e/setup/worker-master-config.yaml --name cluster1 --wait 3m
-kind load --name cluster1 docker-image ${ECR_REPOSITORY}/armada-executor:branch-${CIRCLE_BRANCH}-${CIRCLE_SHA1}
-export KUBECONFIG=$(kind get kubeconfig-path --name=cluster1)
-kubectl create ns armada
-helm template ${REPO_ROOT}/deployment/executor --name=armada-executor --namespace=armada --set image.repository=${ECR_REPOSITORY}/armada-executor --set image.tag=branch-${CIRCLE_BRANCH}-${CIRCLE_SHA1} | kubectl apply -f -
