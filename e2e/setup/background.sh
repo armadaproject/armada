66925fb018dd87cf025733146e1149a65be79c0d
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-04 08:59:01 +0100

    Adding debugging

--- e2e/setup/background.sh
+++ e2e/setup/background.sh
@@ -1,3 +1,10 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
+echo ${REPO_ROOT}
+ls
+pwd
 sleep 10
 echo hello > ${REPO_ROOT}/done.txt
+
+echo ${REPO_ROOT}
+ls
+pwd
--- e2e/setup/wait.sh
+++ e2e/setup/wait.sh
@@ -1,5 +1,14 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
+
+echo ${REPO_ROOT}
+ls
+pwd
+
 while [ ! -f .${REPO_ROOT}/done.txt ]
 do
   sleep 1
 done
+
+echo ${REPO_ROOT}
+ls
+pwd
