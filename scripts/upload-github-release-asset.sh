56a3311c3acf30638aa980d328ba67a474a4b6d0
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-18 13:38:49 +0100

    Adding script to allow us to upload assets built as part of a release to the github release

---
+++ scripts/upload-github-release-asset.sh
@@ -0,0 +1,22 @@
+API_TOKEN=$1
+RELEASE_TAG=$2
+FILE=$3
+
+if [ ! -f $FILE ]; then
+    echo "Could not find file to upload, $FILE is not a valid file path"
+    exit -1
+fi
+
+RELEASE_ID=$(curl -H "Authorization: token $API_TOKEN" https://api.github.com/repos/G-Research/armada/releases/tags/$RELEASE_TAG | jq -r '.id')
+
+if [ $RELEASE_ID = null ]; then
+    echo "Failed to get id of release with tag $RELEASE_TAG"
+    exit -1
+fi
+
+curl -H "Authorization: token $API_TOKEN" -H "Content-Type: application/octet-stream" --data-binary @$FILE "https://uploads.github.com/repos/G-Research/armada/releases/$RELEASE_ID/assets?name=$(basename $FILE)"
+
+if [ $? -eq 0 ]; then
+    echo "Failed to upload file to Github"
+    exit -1
+fi
