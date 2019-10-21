c4953e8b18cdeb54362cfb39fa449fdef0a5881f
JamesMurkin <jamesmurkin@hotmail.com>
2019-10-21 11:55:55 +0100

    Adding make release artifacts + adding them to GitHub release to config.yml

---
+++ scripts/add-checksum-summary.sh
@@ -0,0 +1,35 @@
+API_TOKEN=$1
+RELEASE_TAG=$2
+
+RELEASE_INFO_REPLY=$(curl --fail -H "Authorization: token $API_TOKEN" https://api.github.com/repos/G-Research/armada/releases/tags/$RELEASE_TAG)
+
+if [ $? -ne 0 ]; then
+    echo "Failed to get release information from Github"
+    exit -1
+fi
+
+BODY=$(echo $RELEASE_INFO_REPLY | jq '.body')
+
+if [ "{$BODY}" = null ]; then
+    echo "Failed to get body of release with tag $RELEASE_TAG"
+    exit -1
+fi
+
+BODY="${BODY:1:${#BODY}-2}"
+echo $BODY
+
+DETAILS="$BODY"
+DETAILS="${DETAILS}\n# Checksums"
+
+for artifact in ./dist/*; do
+   SHA=$(sha256sum "${artifact}"  | cut -d " " -f 1)
+   DETAILS="${DETAILS}\n"
+   DETAILS="${DETAILS}* $(basename $artifact) SHA256 \`$SHA\`"
+done
+
+JSON_STRING="{\"body\" : \"$DETAILS\"}"
+
+echo "$JSON_STRING"
+
+
+curl -H "Authorization: token $API_TOKEN" -X PATCH https://api.github.com/repos/G-Research/armada/releases/20811859 --data "$JSON_STRING"
