cbdc541011af3fc3e33c29c1d8cf23a4f66f9bd2
jankaspar <2270833+jankaspar@users.noreply.github.com>
2019-11-08 16:28:25 +0000

    Support basic auth properly. (#232)

--- internal/armada/authorization/basic.go
+++ internal/armada/authorization/basic.go
@@ -2,11 +2,12 @@ package authorization
 
 import (
 	"context"
+	"encoding/base64"
+	"strings"
 
-	"google.golang.org/grpc/metadata"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 
 	"github.com/G-Research/armada/internal/armada/configuration"
-	"github.com/G-Research/armada/internal/common"
 )
 
 type BasicAuthService struct {
@@ -18,28 +19,23 @@ func NewBasicAuthService(users map[string]configuration.UserInfo) *BasicAuthServ
 }
 
 func (authService *BasicAuthService) Authenticate(ctx context.Context) (Principal, error) {
-	if md, ok := metadata.FromIncomingContext(ctx); ok {
-		if len(md[common.UsernameField]) <= 0 || len(md[common.PasswordField]) <= 0 {
-			return nil, missingCredentials
-		}
-
-		userName := md[common.UsernameField][0]
-
-		user, err := authService.loginUser(userName, md[common.PasswordField][0])
+	basicAuth, err := grpc_auth.AuthFromMD(ctx, "basic")
+	if err == nil {
+		payload, err := base64.StdEncoding.DecodeString(basicAuth)
 		if err != nil {
 			return nil, err
 		}
-
-		return NewStaticPrincipal(userName, user.Groups), nil
+		pair := strings.SplitN(string(payload), ":", 2)
+		return authService.loginUser(pair[0], pair[1])
 	}
-
 	return nil, missingCredentials
 }
 
-func (authService *BasicAuthService) loginUser(username string, password string) (*configuration.UserInfo, error) {
-	val, ok := authService.users[username]
-	if ok && val.Password == password {
-		return &val, nil
+func (authService *BasicAuthService) loginUser(username string, password string) (Principal, error) {
+	userInfo, ok := authService.users[username]
+	if ok && userInfo.Password == password {
+
+		return NewStaticPrincipal(username, userInfo.Groups), nil
 	}
 	return nil, invalidCredentials
 }
--- internal/armada/authorization/basic_test.go
+++ internal/armada/authorization/basic_test.go
@@ -17,22 +17,29 @@ func TestBasicAuthService(t *testing.T) {
 		"root": {"toor", []string{}},
 	})
 
-	principal, e := service.Authenticate(metadata.NewIncomingContext(context.Background(), map[string][]string{
-		common.UsernameField: {"root"},
-		common.PasswordField: {"toor"},
-	}))
+	principal, e := service.Authenticate(
+		metadata.NewIncomingContext(context.Background(), basicPassword("root", "toor")))
 
 	assert.Nil(t, e)
 	assert.Equal(t, principal.GetName(), "root")
 
-	_, e = service.Authenticate(metadata.NewIncomingContext(context.Background(), map[string][]string{
-		common.UsernameField: {"root"},
-		common.PasswordField: {"test"},
-	}))
+	_, e = service.Authenticate(
+		metadata.NewIncomingContext(context.Background(), basicPassword("root", "test")))
 
 	assert.NotNil(t, e)
 	assert.NotEqual(t, e, missingCredentials)
 
 	_, e = service.Authenticate(context.Background())
 	assert.Equal(t, e, missingCredentials)
+}
+
+func basicPassword(user, password string) map[string][]string {
+	data, _ := (&common.LoginCredentials{
+		Username: user,
+		Password: password,
+	}).GetRequestMetadata(context.Background())
+
+	return map[string][]string{
+		"authorization": {data["authorization"]},
+	}
 }
