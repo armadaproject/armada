ef439639c586106d54c9e8cb6f32afd4f2a35bb2
JamesMurkin <jamesmurkin@hotmail.com>
2019-11-12 17:04:06 +0000

    Adding basic readinessProbe to server
    This just makes sure the server is ready before requests are sent to it
    The check it performs is very basic for now and may want to be extended to:
     - Check gRPC server is up
     - Check redis is up
    Not doing this for now as likely it is overkill and won't be needed. Mostly we are just checking if the server completely fails to start or not

--- internal/armada/gateway.go
+++ internal/armada/gateway.go
@@ -4,25 +4,24 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/go-openapi/runtime/middleware"
 	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
-	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 
 	"github.com/G-Research/armada/internal/armada/api"
 	protoutil "github.com/G-Research/armada/internal/armada/protoutils"
+	"github.com/G-Research/armada/internal/common"
 )
 
 func ServeGateway(port uint16, grpcPort uint16) (shutdown func()) {
 
-	address := fmt.Sprintf(":%d", port)
 	grpcAddress := fmt.Sprintf(":%d", grpcPort)
-
 	connectionCtx, cancelConnectionCtx := context.WithCancel(context.Background())
 
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", health)
 
 	m := new(protoutil.JSONMarshaller)
 	gw := gwruntime.NewServeMux(gwruntime.WithMarshalerOption(gwruntime.MIMEWildcard, m))
@@ -40,26 +39,17 @@ func ServeGateway(port uint16, grpcPort uint16) (shutdown func()) {
 		panic(err)
 	}
 	mux.Handle("/", gw)
-
 	h := middleware.Spec("/", []byte(api.SwaggerJsonTemplate()), mux)
 
-	srv := &http.Server{Addr: address, Handler: h}
+	cancel := common.ServeHttp(port, h)
 
-	go func() {
-		log.Printf("Gateway listening on %d", port)
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
 	return func() {
 		cancelConnectionCtx()
 		conn.Close()
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		log.Print("Stopping gateway server")
-		e := srv.Shutdown(ctx)
-		if e != nil {
-			panic(e)
-		}
+		cancel()
 	}
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
 }
