5d249b39ed1344c0af0217091619e84a5278c6d1
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2019-12-05 15:54:29 +0000

    Implement same interface for events in C# client.

--- client/DotNet/Armada.Client/Client.cs
+++ client/DotNet/Armada.Client/Client.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -6,6 +5,36 @@ using Newtonsoft.Json;
 
 namespace GResearch.Armada.Client
 {
+    public interface IEvent
+    {
+        string JobId { get; }
+        string JobSetId { get;  }
+        string Queue { get;  }
+        System.DateTimeOffset? Created { get; }
+    }
+
+    public partial class ApiEventMessage
+    {
+        public IEvent Event => Cancelled ?? Submitted ?? Queued ?? Leased ?? LeaseReturned ??
+                               LeaseExpired ?? Pending ?? Running ?? UnableToSchedule ??
+                               Failed ?? Succeeded ?? Reprioritized ?? Cancelling ?? Cancelled ?? Terminated as IEvent;
+    }
+
+    public partial class ApiJobSubmittedEvent : IEvent {}
+    public partial class ApiJobQueuedEvent : IEvent {}
+    public partial class ApiJobLeasedEvent : IEvent {}
+    public partial class ApiJobLeaseReturnedEvent : IEvent {}
+    public partial class ApiJobLeaseExpiredEvent : IEvent {}
+    public partial class ApiJobPendingEvent : IEvent {}
+    public partial class ApiJobRunningEvent : IEvent {}
+    public partial class ApiJobUnableToScheduleEvent : IEvent {}
+    public partial class ApiJobFailedEvent : IEvent {}
+    public partial class ApiJobSucceededEvent : IEvent {}
+    public partial class ApiJobReprioritizedEvent  : IEvent {}
+    public partial class ApiJobCancellingEvent  : IEvent {}
+    public partial class ApiJobCancelledEvent  : IEvent {}
+    public partial class ApiJobTerminatedEvent : IEvent {}
+
     public class StreamResponse<T>
     {
         public T Result { get; set; }
