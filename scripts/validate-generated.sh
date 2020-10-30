22b788bc03200cbb65e17236b38a65e6dc1929dd
Carlo Camurri <carlo.camurri98@gmail.com>
2020-10-30 17:39:57 +0000

    Use git status to check generated file mismatches

--- scripts/validate-generated.sh
+++ scripts/validate-generated.sh
@@ -1,42 +1,10 @@
 #!/bin/sh
 
-compare_generated_files() {
-  expected_repo_path="$1"
-  actual_repo_path="$2"
-
-  if [ -z "$expected_repo_path" ] || [ -z "$actual_repo_path" ]; then
-    echo "Please specify paths to compare generated files"
-    exit 1
-  fi
-
-  rm -f generated.txt
-
-  { find pkg/api \( -iname '*.pb.go' -o -iname '*.pb.gw.go' -o -iname '*.swagger.go' \);
-    find client/DotNet/Armada.Client -iname '*Generated.cs'; } >> generated.txt
-
-  result=0
-  while IFS='' read -r relative_path || [ -n "$relative_path" ]; do
-    echo "Comparing: $expected_repo_path/$relative_path and $actual_repo_path/$relative_path"
-
-    if ! diff "$expected_repo_path/$relative_path" "$actual_repo_path/$relative_path"; then
-      result=1
-    fi
-  done < generated.txt
-
-  rm generated.txt
-
-  exit $result
-}
-
-mkdir -p ../expected && cp -R ./ ../expected
-
-cd ../expected || exit 1
 make proto
-cd ../armada || exit 1
 
-compare_generated_files ../expected .
-compare_result="$?"
+dirs_to_check='pkg/api|client/DotNet/Armada.Client' # Pipe separated
 
-rm -rf ../expected
+git status -s -uno | grep -E "$dirs_to_check"
+compare_result="$(git status -s -uno | grep -c -E "$dirs_to_check")"
 
-exit $compare_result
+exit "$compare_result"
