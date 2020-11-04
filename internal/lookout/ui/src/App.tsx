d84c7860c78751356943730ef323c2565d139a77
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2020-11-04 14:17:28 +0000

    Remove commented out code.

--- internal/lookout/ui/src/App.tsx
+++ internal/lookout/ui/src/App.tsx
@@ -32,22 +32,5 @@ export function App(services: { jobService: JobService; }) {
             </Switch>
         </div>
     </Router>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
