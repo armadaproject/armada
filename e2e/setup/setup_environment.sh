9c4a48ebaecc0ffda1d1bb1bcf629a0169331c22
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2020-11-04 17:24:54 +0000

    Debug CI 3.

--- e2e/setup/setup_environment.sh
+++ e2e/setup/setup_environment.sh
@@ -21,6 +21,8 @@ echo "Installing nodejs"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-nvm use node
+
+ls -la /opt/circleci/.nvm/versions/node/
+
 node -v
 npm -v
