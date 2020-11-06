2ee806db4a4d5e6bac61e3e75a7a68f34a0e75cb
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2020-11-06 17:11:54 +0000

    Create Overview table in Lookout UI.

--- internal/lookout/ui/src/Overview.tsx
+++ internal/lookout/ui/src/Overview.tsx
@@ -1,24 +1,47 @@
-import  React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LookoutSystemOverview } from './openapi';
 import { JobService } from './services/jobs';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Container } from '@material-ui/core';
 
-export function Overview ({jobService: jobs}: {jobService: JobService}): JSX.Element {
+export function Overview({ jobService: jobs }: { jobService: JobService }): JSX.Element {
 
     const [overview, setOverview] = useState<LookoutSystemOverview>({});
-    const loadData = async () => {
-        const res = await jobs.getOverview();
-        setOverview(res);
-    };  
 
     useEffect(() => {
-        loadData();
-        return () => {};
-    });
+        const loadData = async () => {
+            const res = await jobs.getOverview();
+            setOverview(res);
+        };
 
-    return  (<div>
-        { (overview.queues || []).map(q => 
-            
-            <p> { q.queue } </p>
-        ) }      
-        </div>);
+        loadData();
+        return () => { };
+    }, [jobs]);
+
+    return (<Container style={{marginTop: "1em"}}>
+    <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Queue</TableCell>
+                    <TableCell align="right">jobs queued</TableCell>
+                    <TableCell align="right">jobs pending</TableCell>
+                    <TableCell align="right">jobs running</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {(overview.queues || []).map(q =>
+                    <TableRow key={q.queue}>
+                        <TableCell component="th" scope="row">
+                            {q.queue}
+                        </TableCell>
+                        <TableCell align="right">{q.jobsQueued ?? 0}</TableCell>
+                        <TableCell align="right">{q.jobsPending ?? 0}</TableCell>
+                        <TableCell align="right">{q.jobsRunning ?? 0}</TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
+    </TableContainer>
+    </Container>
+    );
 }
