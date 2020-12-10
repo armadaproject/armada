cfa795803d0e9dc6f7fdef1b29b826df714bfbd5
Eric Jonsohn <eric@insightsoftmax.com>
2020-12-10 11:27:39 -0800

    Remove unnecessary files, move gh-pages site up

--- e2e/setup/setup_kube_config_ci.sh
+++
@@ -1,6 +0,0 @@
-until curl -s --fail http://localhost:10080/kubernetes-ready; do
-  sleep 1;
-done
-mkdir .kube || true
-rm .kube/config || true
-wget -P .kube http://localhost:10080/config
--- internal/common/util/map.go
+++
@@ -1,9 +0,0 @@
-package util
-
-func GetOrDefault(m map[string]float64, key string, def float64) float64 {
-	v, ok := m[key]
-	if ok {
-		return v
-	}
-	return def
-}
