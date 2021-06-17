38ff21098a58fc96b21f70df9316c03a0989f35d
JamesMurkin <jamesmurkin@hotmail.com>
2021-06-17 16:38:46 +0100

    Fix CancelJobSet ListItemText className

--- internal/lookout/ui/src/components/job-sets/CancelJobSets.tsx
+++ internal/lookout/ui/src/components/job-sets/CancelJobSets.tsx
@@ -21,7 +21,7 @@ export default function CancelJobSets(props: CancelJobSetsProps) {
       <List component={Paper} className="job-sets-action-table-container">
         {props.jobSets.map((jobSet) => (
           <ListItem key={jobSet.jobSetId}>
-            <ListItemText className="cancel-job-sets-wrap">{jobSet.jobSetId}</ListItemText>
+            <ListItemText className="job-sets-action-wrap">{jobSet.jobSetId}</ListItemText>
           </ListItem>
         ))}
       </List>
