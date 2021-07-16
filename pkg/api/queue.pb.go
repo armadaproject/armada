a7d56c1f381c3f5bd37bff97476e19a4cb3a7ad1
Jan Kaspar <2270833+jankaspar@users.noreply.github.com>
2021-07-16 18:22:13 +0100

    Update generated protobuf code.

--- pkg/api/queue.pb.go
+++ pkg/api/queue.pb.go
@@ -5478,10 +5478,7 @@ func (m *ClusterSchedulingInfoReport) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthQueue
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthQueue
 			}
 			if (iNdEx + skippy) > l {
