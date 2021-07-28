5ded21d0d85e86d6f5e8f151fc1d4c2dc67c9159
Carlo Camurri <carlo.camurri98@gmail.com>
2021-07-28 16:58:37 +0100

    Force delete container

--- e2e/setup/setup_cluster_ci.sh
+++ e2e/setup/setup_cluster_ci.sh
@@ -1,4 +1,5 @@
 if curl -s --fail http://127.0.0.1:10080/kubernetes-ready; then
     exit 0
 fi
+docker stop kube && docker rm kube || true
 docker run -d --name kube --privileged -p 8443:8443 -p 10080:10080 bsycorp/kind:v1.17.9 || true
