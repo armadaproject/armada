16def47268a4282af542e44461e8ac46f688dcf6
Carlo Camurri <carlo.camurri98@gmail.com>
2021-07-28 17:17:50 +0100

    actual fix

--- e2e/setup/setup_cluster_ci.sh
+++ e2e/setup/setup_cluster_ci.sh
@@ -1,7 +1,4 @@
-if curl --fail http://127.0.0.1:10080/kubernetes-ready; then
+if curl -s --fail http://127.0.0.1:10080/config; then
     exit 0
 fi
-docker ps -a
-docker stop '/kube' && docker rm '/kube' || true
-docker stop kube && docker rm kube || true
 docker run -d --name kube --privileged -p 8443:8443 -p 10080:10080 bsycorp/kind:v1.17.9 || true
