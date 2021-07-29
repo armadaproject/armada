b80de9b89f10b1ddf2180ce139c4dafb834af138
Carlo Camurri <carlo.camurri98@gmail.com>
2021-07-29 08:30:17 +0100

    sed inplace

--- e2e/setup/setup_kube_config_ci.sh
+++ e2e/setup/setup_kube_config_ci.sh
@@ -4,6 +4,4 @@ done
 mkdir .kube || true
 rm .kube/config || true
 wget -P .kube http://localhost:10080/config
-sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"localhost"/ .kube/config > .kube/config-2
-rm .kube/config
-mv .kube/config-2 .kube/config
+sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"localhost"/ .kube/config
