71eeebbf035a6da8dc61a7b3a44420111b34a5f9
Stefan Novakovic <stefnova@Stefans-MacBook-Pro.local>
2021-10-25 10:54:02 +0200

    [issues-699] Make armadactl queue management more like kubectl
    [Problem]
    
    Make armadactl to look and feel like kubectl commands. All of the Armada
    users will have fimiliarity with kubectl already so it makes sense for
    armada feel like an extension of kubectl.
    
    Existing queue commands `create-queue` and `delete-queue` should be changed
    to be `create queue` and `delete queue`. Additionally create queue command
    should no longer support upsert (update or insert) functionallity.
    
    From security perspective, this is an issue because queues could accidentaly be
    overwritten. More explicit approach should be enforced, where queue creation
    should fail if queue exisits, and separate command for queue update should
    be introduced.
    
    [Solution]
    
    - Add queue package that will support queue commands: (create, delete, info
    and update). All of the commands are now using RunE instead of Run cobra
    function for proper error handling. Queue commands are also using cobra's
    Printf function instead of logrus logger. Add error handling of failed
    flag validation.
    
    - `create-queue` and `delete-queue` commands have a depreicate message
    associated with them.
    
    - `info queue` has been moved to cmd/queue package
    
    Close #699

---
+++ cmd/armadactl/cmd/commands.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"github.com/G-Research/armada/cmd/armadactl/cmd/queue"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(
+		Create(),
+		Delete(),
+		Update(),
+		Info(),
+	)
+}
+
+func Create() *cobra.Command {
+	command := cobra.Command{
+		Use:   "create",
+		Short: "Create Armada resource. Supported: queue",
+	}
+
+	command.AddCommand(
+		queue.Create(),
+	)
+
+	return &command
+}
+
+func Delete() *cobra.Command {
+	command := cobra.Command{
+		Use:   "delete",
+		Short: "Delete Armada resource. Supported: queue",
+	}
+
+	command.AddCommand(
+		queue.Delete(),
+	)
+
+	return &command
+}
+
+func Update() *cobra.Command {
+	command := cobra.Command{
+		Use:   "update",
+		Short: "Update Armada resource. Supported: queue",
+	}
+
+	command.AddCommand(
+		queue.Update(),
+	)
+
+	return &command
+}
+
+func Info() *cobra.Command {
+	command := cobra.Command{
+		Use:   "info",
+		Short: "Retrieve information about armada resource. Supported: queue",
+	}
+
+	command.AddCommand(
+		queue.Info(),
+	)
+
+	return &command
+}
