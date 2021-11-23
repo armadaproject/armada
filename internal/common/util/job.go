696c18b28ddc5d85b1bb81661c46fe254e8f9b12
JamesMurkin <jamesmurkin@hotmail.com>
2021-11-23 16:22:45 +0000

    Initial implementation of Job Requirements Key

---
+++ internal/common/util/job.go
@@ -0,0 +1,302 @@
+package util
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/gogo/protobuf/proto"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	"github.com/G-Research/armada/internal/common"
+	"github.com/G-Research/armada/pkg/api"
+)
+
+const objectSeparator = "--"
+const tolerationFieldSeparator = "+"
+const sectionSeparator = "||"
+const keySeparator = "##"
+const prioritySeparator = "++"
+const noValueString = "NIL"
+
+func GetRequirementKey(job *api.Job) string {
+	existingKeys := map[string]int{}
+
+	if job.PodSpec != nil {
+		specKey := getSpecKey(job.PodSpec)
+		existingKeys[specKey]++
+	}
+
+	for _, spec := range job.PodSpecs {
+		specKey := getSpecKey(spec)
+		existingKeys[specKey]++
+	}
+
+	requestKeyValues := make([]string, 0, len(existingKeys))
+	for key, value := range existingKeys {
+		requestKeyValues = append(requestKeyValues, fmt.Sprintf("%s%s%s", key, sectionSeparator, strconv.Itoa(value)))
+	}
+	sort.Strings(requestKeyValues)
+	// Add quotes and escaping if required
+	return strconv.FormatFloat(job.Priority, 'f', -1, 64) + prioritySeparator + strings.Join(requestKeyValues, keySeparator)
+}
+
+func getSpecKey(podSpec *v1.PodSpec) string {
+	requirementsKey := getRequirementsKey(podSpec)
+	nodeSelectorKey := getNodeSelectorKey(podSpec)
+	tolerationsKey := getTolerationsKey(podSpec.Tolerations)
+	affinityKey := getAffinityKey(podSpec)
+	return requirementsKey + sectionSeparator + nodeSelectorKey + sectionSeparator + tolerationsKey + sectionSeparator + affinityKey
+}
+
+func getRequirementsKey(podSpec *v1.PodSpec) string {
+	totalResourceRequest := common.TotalPodResourceRequest(podSpec)
+
+	requestAsStringsMap := map[string]string{}
+	for resourceName, quantity := range totalResourceRequest {
+		requestAsStringsMap[resourceName] = removeDecimalZeros(quantity.AsDec().String())
+	}
+
+	return convertStringMapToKey(requestAsStringsMap)
+}
+
+func removeDecimalZeros(s string) string {
+	if !strings.Contains(s, ".") {
+		return s
+	}
+	s = strings.TrimRight(s, "0")
+	if strings.HasSuffix(s, ".") {
+		s = s[0 : len(s)-1]
+	}
+	return s
+}
+
+func getRequirementsFromKey(key string) (v1.ResourceRequirements, error) {
+	resourceRequirementsMap, err := convertStringToStringMap(key)
+	if err != nil {
+		return v1.ResourceRequirements{}, fmt.Errorf("unable to process resource requirements key because %s", err)
+	}
+
+	resourceList := v1.ResourceList{}
+
+	for resourceName, quantityString := range resourceRequirementsMap {
+		quantity, err := resource.ParseQuantity(quantityString)
+		if err != nil {
+			return v1.ResourceRequirements{}, fmt.Errorf("unable to process resource value %s because %s", quantityString, err)
+		}
+		resourceList[v1.ResourceName(resourceName)] = quantity
+	}
+
+	return v1.ResourceRequirements{
+		Limits:   resourceList,
+		Requests: resourceList,
+	}, nil
+}
+
+func getTolerationsKey(tolerations []v1.Toleration) string {
+	if len(tolerations) <= 0 {
+		return noValueString
+	}
+	sort.Slice(tolerations, func(i, j int) bool {
+		return tolerations[i].Key < tolerations[j].Key
+	})
+
+	tolerationKeys := make([]string, 0, len(tolerations))
+	for _, toleration := range tolerations {
+		tolerationKeys = append(tolerationKeys, getTolerationKey(toleration))
+	}
+	return strings.Join(tolerationKeys, objectSeparator)
+}
+
+func getTolerationKey(toleration v1.Toleration) string {
+	return fmt.Sprintf("%s%s%s%s%s%s%s",
+		toleration.Key, tolerationFieldSeparator,
+		toleration.Value, tolerationFieldSeparator,
+		toleration.Effect, tolerationFieldSeparator,
+		toleration.Operator)
+}
+
+func getTolerationsFromKey(key string) ([]v1.Toleration, error) {
+	if key == noValueString {
+		return []v1.Toleration{}, nil
+	}
+
+	tolerationKeys := strings.Split(key, objectSeparator)
+	tolerations := make([]v1.Toleration, 0, len(tolerationKeys))
+	for _, tolerationKey := range tolerationKeys {
+		toleration, err := getTolerationFromKey(tolerationKey)
+		if err != nil {
+			return []v1.Toleration{}, err
+		}
+		tolerations = append(tolerations, toleration)
+	}
+	return tolerations, nil
+}
+
+func getTolerationFromKey(key string) (v1.Toleration, error) {
+	tolerationFields := strings.Split(key, tolerationFieldSeparator)
+	if len(tolerationFields) != 4 {
+		return v1.Toleration{}, fmt.Errorf("unable to convert key %s to toleration, due to it being in an unexpected format", key)
+	}
+	return v1.Toleration{
+		Key:      tolerationFields[0],
+		Value:    tolerationFields[1],
+		Effect:   v1.TaintEffect(tolerationFields[2]),
+		Operator: v1.TolerationOperator(tolerationFields[3]),
+	}, nil
+
+}
+
+func getNodeSelectorKey(spec *v1.PodSpec) string {
+	if len(spec.NodeSelector) <= 0 {
+		return noValueString
+	}
+	return convertStringMapToKey(spec.NodeSelector)
+}
+
+func getNodeSelectorFromKey(key string) (map[string]string, error) {
+	if key == noValueString {
+		return map[string]string{}, nil
+	}
+
+	return convertStringToStringMap(key)
+}
+
+// TODO We could do some custom encoding of these object to make them more compact
+func getAffinityKey(spec *v1.PodSpec) string {
+	if spec.Affinity == nil || spec.Affinity.NodeAffinity == nil {
+		return noValueString
+	}
+
+	//var b bytes.Buffer
+	//return proto.MarshalTextString(spec.Affinity.NodeAffinity)
+	b, _ := proto.Marshal(spec.Affinity.NodeAffinity)
+
+	//var b bytes.Buffer
+	//e := msgpack.NewEncoder(&b)
+	//e.SetOmitEmpty(true)
+	//e.SetSortMapKeys(true)
+	//_  = e.Encode(spec.Affinity.NodeAffinity)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//b, _ := json.Marshal(spec.Affinity.NodeAffinity)
+	//output := string(b)
+	//output = strings.ReplaceAll(output, "\"", "!")
+	return string(b)
+}
+
+func getAffinityFromKey(key string) (*v1.NodeAffinity, error) {
+	if key == noValueString {
+		return nil, nil
+	}
+
+	var nodeAffinity v1.NodeAffinity
+	keyBytes := []byte(key)
+	//err := msgpack.Unmarshal(keyBytes, &nodeAffinity)
+	//key = strings.ReplaceAll(key, "!", "\"")
+	err := proto.Unmarshal(keyBytes, &nodeAffinity)
+	//err := json.Unmarshal(keyBytes, &nodeAffinity)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal node affinity %s because %s", key, err)
+	}
+	return &nodeAffinity, err
+}
+
+func GenerateJobRequirementsFromKey(key string) (*api.Job, error) {
+	job := &api.Job{}
+	specKeys := strings.Split(key, keySeparator)
+	for _, specKey := range specKeys {
+		spec, count, err := generatePodSpecFromKey(specKey)
+		if err != nil {
+			return nil, err
+		}
+		for i := 0; i < count; i++ {
+			job.PodSpecs = append(job.PodSpecs, spec)
+		}
+	}
+	return job, nil
+}
+
+func generatePodSpecFromKey(key string) (*v1.PodSpec, int, error) {
+	sections := strings.Split(key, sectionSeparator)
+	if len(sections) != 5 {
+		return nil, 0, fmt.Errorf("Unexpected number of key sections found, found %d, expected  %d", len(sections), 5)
+	}
+
+	spec := &v1.PodSpec{}
+
+	requirementsKey := sections[0]
+	requirements, err := getRequirementsFromKey(requirementsKey)
+	if err != nil {
+		return nil, 0, err
+	}
+	spec.Containers = []v1.Container{
+		{
+			Resources: requirements,
+		},
+	}
+
+	nodeSelectorKey := sections[1]
+	nodeSelector, err := getNodeSelectorFromKey(nodeSelectorKey)
+	if err != nil {
+		return nil, 0, err
+	}
+	spec.NodeSelector = nodeSelector
+
+	tolerationsKey := sections[2]
+	tolerations, err := getTolerationsFromKey(tolerationsKey)
+	if err != nil {
+		return nil, 0, err
+	}
+	spec.Tolerations = tolerations
+
+	affinityKey := sections[3]
+	nodeAffinity, err := getAffinityFromKey(affinityKey)
+	if err != nil {
+		return nil, 0, err
+	}
+	if nodeAffinity != nil {
+		affinity := &v1.Affinity{
+			NodeAffinity: nodeAffinity,
+		}
+		spec.Affinity = affinity
+	}
+
+	count, err := strconv.Atoi(sections[4])
+	if err != nil {
+		return nil, 0, fmt.Errorf("count section contained %s, expected a number", sections[4])
+	}
+
+	return spec, count, nil
+}
+
+func convertStringMapToKey(m map[string]string) string {
+	keys := GetKeys(m)
+	sort.Strings(keys)
+	values := make([]string, 0, len(keys))
+	for _, key := range keys {
+		values = append(values, fmt.Sprintf("%s=%s", key, m[key]))
+	}
+
+	return strings.Join(values, objectSeparator)
+}
+
+func convertStringToStringMap(key string) (map[string]string, error) {
+	keyValueStrings := strings.Split(key, objectSeparator)
+
+	result := make(map[string]string, len(keyValueStrings))
+
+	for _, keyValueString := range keyValueStrings {
+		r := strings.Split(keyValueString, "=")
+		if len(r) != 2 {
+			return map[string]string{}, fmt.Errorf("unable to process keyvalue string %s as it is in an unexpected format", keyValueString)
+		}
+		k, value := r[0], r[1]
+		result[k] = value
+	}
+
+	return result, nil
+}
---
+++ internal/common/util/job_test.go
@@ -0,0 +1,306 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/G-Research/armada/pkg/api"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+func TestGetResourceRequestKey(t *testing.T) {
+	resourceRequirement := createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("700Gi"), "nvidia.com/gpu": resource.MustParse("1")})
+	job := api.Job{
+		Priority: 50,
+		PodSpec: &v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Resources: resourceRequirement,
+				},
+			},
+			NodeSelector: map[string]string{
+				"armada/important": "true",
+			},
+			Tolerations: []v1.Toleration{
+				{
+					Key:      "armada/important",
+					Value:    "true",
+					Operator: v1.TolerationOpEqual,
+					Effect:   v1.TaintEffectNoSchedule,
+				},
+			},
+			Affinity: &v1.Affinity{
+				NodeAffinity: &v1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+						NodeSelectorTerms: []v1.NodeSelectorTerm{
+							{
+								MatchExpressions: []v1.NodeSelectorRequirement{
+									{
+										Key:      "NodeName",
+										Operator: v1.NodeSelectorOpExists,
+										Values: []string{
+											"val1",
+											"val2",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	result := GetRequirementKey(&job)
+
+	assert.Equal(t, result, "50++cpu=1--memory=751619276800--nvidia.com/gpu=1||armada/important=true||armada/important+true+NoSchedule+Equal||\n\"\n \n\u001E\n\bNodeName\u0012\u0006Exists\u001A\u0004val1\u001A\u0004val2||1")
+}
+
+func TestGenerateJobRequirementsFromKey(t *testing.T) {
+	resourceRequirement := createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("1000000000"), "nvidia.com/gpu": resource.MustParse("1")})
+	expected := &api.Job{
+		Priority: 50,
+		PodSpec: &v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Resources: resourceRequirement,
+				},
+			},
+			NodeSelector: map[string]string{
+				"armada/important": "true",
+			},
+			Tolerations: []v1.Toleration{
+				{
+					Key:      "armada/important",
+					Value:    "true",
+					Operator: v1.TolerationOpEqual,
+					Effect:   v1.TaintEffectNoSchedule,
+				},
+			},
+			Affinity: &v1.Affinity{
+				NodeAffinity: &v1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+						NodeSelectorTerms: []v1.NodeSelectorTerm{
+							{
+								MatchExpressions: []v1.NodeSelectorRequirement{
+									{
+										Key:      "NodeName",
+										Operator: v1.NodeSelectorOpExists,
+										Values: []string{
+											"val1",
+											"val2",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	result, err := GenerateJobRequirementsFromKey("50++cpu=1--memory=751619276800--nvidia.com/gpu=1||armada/important=true||armada/important+true+NoSchedule+Equal||\n\"\n \n\u001E\n\bNodeName\u0012\u0006Exists\u001A\u0004val1\u001A\u0004val2||1")
+	assert.NoError(t, err)
+	assert.Equal(t, result, expected)
+}
+
+func TestGetRequirementsKey(t *testing.T) {
+	resourceRequirement := createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("1Gi"), "nvidia.com/gpu": resource.MustParse("1")})
+	spec := &v1.PodSpec{
+		Containers: []v1.Container{
+			{
+				Resources: resourceRequirement,
+			},
+		},
+	}
+
+	key := getRequirementsKey(spec)
+	assert.Equal(t, key, "cpu=1--memory=1073741824--nvidia.com/gpu=1")
+
+	resourceRequirement = createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1000m"), "memory": resource.MustParse("1Gi"), "nvidia.com/gpu": resource.MustParse("1")})
+	spec.Containers[0].Resources = resourceRequirement
+
+	key = getRequirementsKey(spec)
+	assert.Equal(t, key, "cpu=1--memory=1073741824--nvidia.com/gpu=1")
+
+	resourceRequirement = createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("100m"), "memory": resource.MustParse("1Gi"), "nvidia.com/gpu": resource.MustParse("1")})
+	spec.Containers[0].Resources = resourceRequirement
+
+	key = getRequirementsKey(spec)
+	assert.Equal(t, key, "cpu=0.1--memory=1073741824--nvidia.com/gpu=1")
+
+	resourceRequirement = createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("100m"), "memory": resource.MustParse("1G"), "nvidia.com/gpu": resource.MustParse("1")})
+	spec.Containers[0].Resources = resourceRequirement
+
+	key = getRequirementsKey(spec)
+	assert.Equal(t, key, "cpu=0.1--memory=1000000000--nvidia.com/gpu=1")
+}
+
+func TestGetRequirementsKey_WhenPodHasMultipleContainers(t *testing.T) {
+	resourceRequirement := createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("1Gi"), "nvidia.com/gpu": resource.MustParse("1")})
+	spec := &v1.PodSpec{
+		Containers: []v1.Container{
+			{
+				Resources: resourceRequirement,
+			},
+			{
+				Resources: resourceRequirement,
+			},
+		},
+		InitContainers: []v1.Container{
+			{
+				Resources: resourceRequirement,
+			},
+			{
+				Resources: resourceRequirement,
+			},
+		},
+	}
+
+	result := getRequirementsKey(spec)
+	assert.Equal(t, result, "cpu=2--memory=2147483648--nvidia.com/gpu=2")
+}
+
+func TestGetRequirementFromKey(t *testing.T) {
+	resourceRequirement := createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("1073741824"), "nvidia.com/gpu": resource.MustParse("1")})
+	result, err := getRequirementsFromKey("cpu=1--memory=1073741824--nvidia.com/gpu=1")
+	assert.NoError(t, err)
+	assert.Equal(t, resourceRequirement, result)
+
+	resourceRequirement = createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("0.1"), "memory": resource.MustParse("1073741824"), "nvidia.com/gpu": resource.MustParse("1")})
+	result, err = getRequirementsFromKey("cpu=0.1--memory=1073741824--nvidia.com/gpu=1")
+	assert.NoError(t, err)
+	assert.Equal(t, resourceRequirement, result)
+
+	resourceRequirement = createResourceRequirements(v1.ResourceList{"cpu": resource.MustParse("1"), "memory": resource.MustParse("1000000000")})
+	result, err = getRequirementsFromKey("cpu=1--memory=1000000000")
+	assert.NoError(t, err)
+	assert.Equal(t, resourceRequirement, result)
+
+	_, err = getRequirementsFromKey("INVALID_KEY")
+	assert.Error(t, err)
+}
+
+func TestGetTolerationsKey(t *testing.T) {
+	tolerations := []v1.Toleration{
+		{
+			Key:      "armada/special",
+			Operator: v1.TolerationOpEqual,
+			Value:    "true",
+			Effect:   v1.TaintEffectNoSchedule,
+		},
+		{
+			Key:      "armada/important",
+			Operator: v1.TolerationOpEqual,
+			Value:    "true",
+			Effect:   v1.TaintEffectNoSchedule,
+		},
+	}
+
+	result := getTolerationsKey(tolerations)
+	assert.Equal(t, "armada/important+true+NoSchedule+Equal--armada/special+true+NoSchedule+Equal", result)
+
+	wildcardToleration := []v1.Toleration{
+		{
+			Key:   "armada/special",
+			Value: "true",
+		},
+	}
+
+	result = getTolerationsKey(wildcardToleration)
+	assert.Equal(t, "armada/special+true++", result)
+
+	result = getTolerationsKey([]v1.Toleration{})
+	assert.Equal(t, "NIL", result)
+}
+
+func TestGetTolerationsFromKey(t *testing.T) {
+	tolerations := []v1.Toleration{
+		{
+			Key:      "armada/important",
+			Operator: v1.TolerationOpEqual,
+			Value:    "true",
+			Effect:   v1.TaintEffectNoSchedule,
+		},
+		{
+			Key:      "armada/special",
+			Operator: v1.TolerationOpEqual,
+			Value:    "true",
+			Effect:   v1.TaintEffectNoSchedule,
+		},
+	}
+
+	result, err := getTolerationsFromKey("armada/important+true+NoSchedule+Equal--armada/special+true+NoSchedule+Equal")
+	assert.NoError(t, err)
+	assert.Equal(t, result, tolerations)
+
+	wildcardToleration := []v1.Toleration{
+		{
+			Key:   "armada/special",
+			Value: "true",
+		},
+	}
+
+	result, err = getTolerationsFromKey("armada/special+true++")
+	assert.NoError(t, err)
+	assert.Equal(t, result, wildcardToleration)
+
+	result, err = getTolerationsFromKey("NIL")
+	assert.NoError(t, err)
+	assert.Equal(t, []v1.Toleration{}, result)
+
+	result, err = getTolerationsFromKey("INVALID_KEY")
+	assert.Error(t, err)
+}
+
+func TestGetNodeSelectorKey(t *testing.T) {
+	spec := &v1.PodSpec{
+		NodeSelector: map[string]string{
+			"armada/special":   "true",
+			"armada/important": "true",
+		},
+	}
+
+	result := getNodeSelectorKey(spec)
+	assert.Equal(t, result, "armada/important=true--armada/special=true")
+
+	spec = &v1.PodSpec{}
+	result = getNodeSelectorKey(spec)
+	assert.Equal(t, result, "NIL")
+}
+
+func TestGetNodeSelectorFromKey(t *testing.T) {
+	expected := map[string]string{
+		"armada/special":   "true",
+		"armada/important": "true",
+	}
+
+	result, err := getNodeSelectorFromKey("armada/important=true--armada/special=true")
+	assert.NoError(t, err)
+	assert.Equal(t, result, expected)
+
+	result, err = getNodeSelectorFromKey("NIL")
+	assert.NoError(t, err)
+	assert.Equal(t, result, map[string]string{})
+
+	result, err = getNodeSelectorFromKey("UNKNOWN_FORMAT")
+	assert.Error(t, err)
+}
+
+func TestGetNodeAffinityKey(t *testing.T) {
+
+}
+
+func TestGetNodeAffinityFromKey(t *testing.T) {
+
+}
+
+func createResourceRequirements(resource v1.ResourceList) v1.ResourceRequirements {
+	return v1.ResourceRequirements{
+		Limits:   resource,
+		Requests: resource,
+	}
+}
