c9cef8820a72261fabf34fca5e2e3403023d4f23
JamesMurkin <jamesmurkin@hotmail.com>
2021-11-25 14:13:52 +0000

    Update executor config validation tests

--- internal/executor/application_test.go
+++ internal/executor/application_test.go
@@ -10,21 +10,44 @@ import (
 
 func Test_ValidateConfig_When_AvoidNodeLabelsOnRetry_MissingFrom_TrackedNodeLabels_Fails(t *testing.T) {
 	var config configuration.ExecutorConfiguration
+	config.Application.SubmissionThreadCount = 1
+
 	config.Kubernetes.TrackedNodeLabels = []string{"host"}
 	config.Kubernetes.AvoidNodeLabelsOnRetry = []string{"host", "cluster"}
-	assert.NotNil(t, validateConfig(config))
+	assert.Error(t, validateConfig(config))
 }
 
 func Test_ValidateConfig_When_AvoidNodeLabelsOnRetry_And_TrackedNodeLabels_Match_Succeeds(t *testing.T) {
 	var config configuration.ExecutorConfiguration
+	config.Application.SubmissionThreadCount = 1
+
 	config.Kubernetes.TrackedNodeLabels = []string{"host", "cluster"}
 	config.Kubernetes.AvoidNodeLabelsOnRetry = []string{"host", "cluster"}
-	assert.Nil(t, validateConfig(config))
+	assert.NoError(t, validateConfig(config))
 }
 
 func Test_ValidateConfig_When_TrackedNodeLabels_MissingFrom_AvoidNodeLabelsOnRetry_Succeeds(t *testing.T) {
 	var config configuration.ExecutorConfiguration
+	config.Application.SubmissionThreadCount = 1
+
 	config.Kubernetes.TrackedNodeLabels = []string{"host", "cluster"}
 	config.Kubernetes.AvoidNodeLabelsOnRetry = []string{"host"}
-	assert.Nil(t, validateConfig(config))
+	assert.NoError(t, validateConfig(config))
+}
+func Test_ValidateConfig_When_SubmissionThreadCount_GreaterOrEqualToOne(t *testing.T) {
+	var config configuration.ExecutorConfiguration
+	config.Application.SubmissionThreadCount = 1
+	assert.NoError(t, validateConfig(config))
+
+	config.Application.SubmissionThreadCount = 5
+	assert.NoError(t, validateConfig(config))
+}
+
+func Test_ValidateConfig_When_SubmissionThreadCount_LessThanOrEqualToZero(t *testing.T) {
+	var config configuration.ExecutorConfiguration
+	config.Application.SubmissionThreadCount = 0
+	assert.Error(t, validateConfig(config))
+
+	config.Application.SubmissionThreadCount = -5
+	assert.Error(t, validateConfig(config))
 }
