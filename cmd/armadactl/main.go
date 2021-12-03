50f1f17ba059a8ccd683a02a611c6fc2abc6848a
Albin Severinson <albin@severinson.org>
2021-12-03 11:54:30 +0000

    Removed commented-out code

--- cmd/armadactl/main.go
+++ cmd/armadactl/main.go
@@ -10,20 +10,3 @@ func main() {
 	common.ConfigureCommandLineLogging() // TODO: can be removed once all of armadactl prints to its internal io.Writer
 	cmd.Execute()
 }
-
-// package main
-
-// import (
-// 	log "github.com/sirupsen/logrus"
-
-// 	"github.com/G-Research/armada/cmd/armadactl/cmd"
-// 	"github.com/G-Research/armada/internal/common"
-// )
-
-// func main() {
-// 	common.ConfigureCommandLineLogging()
-// 	root := cmd.RootCmd()
-// 	if err := root.Execute(); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
