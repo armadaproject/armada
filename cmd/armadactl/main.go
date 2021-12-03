5b61bf5ec3fc9978430365519714b00c6f60743e
Albin Severinson <albin@severinson.org>
2021-12-03 11:56:12 +0000

    Removed unnecessary logging setup

--- cmd/armadactl/main.go
+++ cmd/armadactl/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/G-Research/armada/cmd/armadactl/cmd"
-	"github.com/G-Research/armada/internal/common"
 )
 
 // Config is handled by cmd/params.go
 func main() {
-	common.ConfigureCommandLineLogging() // TODO: can be removed once all of armadactl prints to its internal io.Writer
 	cmd.Execute()
 }
