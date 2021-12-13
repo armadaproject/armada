9d2b39bc1e638d79d71b2be446f9f374f386ff90
Daniel Rastelli <rastellidani@gmail.com>
2021-12-13 11:54:34 +0000

    Update deserialize

--- pkg/api/deserialize.go
+++ pkg/api/deserialize.go
@@ -24,3 +24,23 @@ func (x *IngressType) UnmarshalJSON(data []byte) error {
 	*x = IngressType(value)
 	return nil
 }
+
+func (x *ServiceType) UnmarshalJSON(data []byte) error {
+	var s int32
+	e := json.Unmarshal(data, &s)
+	if e == nil {
+		*x = ServiceType(s)
+		return nil
+	}
+	var t string
+	e = json.Unmarshal(data, &t)
+	if e != nil {
+		return e
+	}
+	value, present := ServiceType_value[t]
+	if !present {
+		return fmt.Errorf("no ServiceType of type %s", t)
+	}
+	*x = ServiceType(value)
+	return nil
+}
