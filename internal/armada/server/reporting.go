cb1c0e4914fe6e06955d0b49adee2ed68eea885f
Albin Severinson <albin@severinson.org>
2021-12-20 15:43:26 +0000

    Error handling and messages

--- internal/armada/server/reporting.go
+++ internal/armada/server/reporting.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -13,82 +14,101 @@ func reportQueued(repository repository.EventStore, jobs []*api.Job) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobQueuedEvent{
+		event, err := api.Wrap(&api.JobQueuedEvent{
 			JobId:    job.Id,
 			Queue:    job.Queue,
 			JobSetId: job.JobSetId,
 			Created:  now,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportQueued] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportQueued] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportDuplicateDetected(repository repository.EventStore, results []*repository.SubmitJobResult) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, result := range results {
-		event, e := api.Wrap(&api.JobDuplicateFoundEvent{
+		event, err := api.Wrap(&api.JobDuplicateFoundEvent{
 			JobId:         result.SubmittedJob.Id,
 			Queue:         result.SubmittedJob.Queue,
 			JobSetId:      result.SubmittedJob.JobSetId,
 			Created:       now,
 			OriginalJobId: result.JobId,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportDuplicateDetected] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportDuplicateDetected] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportSubmitted(repository repository.EventStore, jobs []*api.Job) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobSubmittedEvent{
+		event, err := api.Wrap(&api.JobSubmittedEvent{
 			JobId:    job.Id,
 			Queue:    job.Queue,
 			JobSetId: job.JobSetId,
 			Created:  now,
 			Job:      *job,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportSubmitted] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
 
-	e := repository.ReportEvents(events)
-	return e
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportSubmitted] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
+// TODO This function behaves differently from the rest in this file.
+// We should consolidate so that they all behave in the same way.
 func reportJobsLeased(repository repository.EventStore, jobs []*api.Job, clusterId string) {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobLeasedEvent{
+		event, err := api.Wrap(&api.JobLeasedEvent{
 			JobId:     job.Id,
 			Queue:     job.Queue,
 			JobSetId:  job.JobSetId,
 			Created:   now,
 			ClusterId: clusterId,
 		})
-		if e != nil {
-			log.Error(e)
+		if err != nil {
+			err = fmt.Errorf("[reportJobsLeased] error wrapping event: %w", err)
+			log.Error(err)
 		} else {
 			events = append(events, event)
 		}
 	}
-	e := repository.ReportEvents(events)
-	if e != nil {
-		log.Error(e)
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		err = fmt.Errorf("[reportJobsLeased] error reporting events: %w", err)
+		log.Error(err)
 	}
 }
 
@@ -96,27 +116,32 @@ func reportJobsCancelling(repository repository.EventStore, requestorName string
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobCancellingEvent{
+		event, err := api.Wrap(&api.JobCancellingEvent{
 			JobId:     job.Id,
 			Queue:     job.Queue,
 			JobSetId:  job.JobSetId,
 			Created:   now,
 			Requestor: requestorName,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportJobsCancelling] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportJobsCancelling] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportJobsReprioritizing(repository repository.EventStore, requestorName string, jobs []*api.Job, newPriority float64) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobReprioritizingEvent{
+		event, err := api.Wrap(&api.JobReprioritizingEvent{
 			JobId:       job.Id,
 			Queue:       job.Queue,
 			JobSetId:    job.JobSetId,
@@ -124,20 +149,25 @@ func reportJobsReprioritizing(repository repository.EventStore, requestorName st
 			NewPriority: newPriority,
 			Requestor:   requestorName,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportJobsReprioritizing] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportJobsReprioritizing] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportJobsReprioritized(repository repository.EventStore, requestorName string, jobs []*api.Job, newPriority float64) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobReprioritizedEvent{
+		event, err := api.Wrap(&api.JobReprioritizedEvent{
 			JobId:       job.Id,
 			Queue:       job.Queue,
 			JobSetId:    job.JobSetId,
@@ -145,20 +175,25 @@ func reportJobsReprioritized(repository repository.EventStore, requestorName str
 			NewPriority: newPriority,
 			Requestor:   requestorName,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportJobsReprioritized] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportJobsReprioritized] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportJobsUpdated(repository repository.EventStore, requestorName string, jobs []*api.Job) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobUpdatedEvent{
+		event, err := api.Wrap(&api.JobUpdatedEvent{
 			JobId:     job.Id,
 			Queue:     job.Queue,
 			JobSetId:  job.JobSetId,
@@ -166,33 +201,43 @@ func reportJobsUpdated(repository repository.EventStore, requestorName string, j
 			Requestor: requestorName,
 			Job:       *job,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportJobsUpdated] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportJobsUpdated] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 func reportJobsCancelled(repository repository.EventStore, requestorName string, jobs []*api.Job) error {
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, job := range jobs {
-		event, e := api.Wrap(&api.JobCancelledEvent{
+		event, err := api.Wrap(&api.JobCancelledEvent{
 			JobId:     job.Id,
 			Queue:     job.Queue,
 			JobSetId:  job.JobSetId,
 			Created:   now,
 			Requestor: requestorName,
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportJobsCancelled] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportJobsCancelled] error reporting events: %w", err)
+	}
+
+	return nil
 }
 
 type jobFailure struct {
@@ -204,7 +249,7 @@ func reportFailed(repository repository.EventStore, clusterId string, jobFailure
 	events := []*api.EventMessage{}
 	now := time.Now()
 	for _, jobFailure := range jobFailures {
-		event, e := api.Wrap(&api.JobFailedEvent{
+		event, err := api.Wrap(&api.JobFailedEvent{
 			JobId:        jobFailure.job.Id,
 			JobSetId:     jobFailure.job.JobSetId,
 			Queue:        jobFailure.job.Queue,
@@ -215,11 +260,16 @@ func reportFailed(repository repository.EventStore, clusterId string, jobFailure
 			KubernetesId: "",
 			NodeName:     "",
 		})
-		if e != nil {
-			return e
+		if err != nil {
+			return fmt.Errorf("[reportFailed] error wrapping event: %w", err)
 		}
 		events = append(events, event)
 	}
-	e := repository.ReportEvents(events)
-	return e
+
+	err := repository.ReportEvents(events)
+	if err != nil {
+		return fmt.Errorf("[reportFailed] error reporting events: %w", err)
+	}
+
+	return nil
 }
