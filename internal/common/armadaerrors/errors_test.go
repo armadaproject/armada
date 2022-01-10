2cfaa748df1ff82d85029f9e77086a522d183eed
Albin Severinson <albin@severinson.org>
2022-01-10 16:44:37 +0000

    Added test case for nil error

--- internal/common/armadaerrors/errors_test.go
+++ internal/common/armadaerrors/errors_test.go
@@ -27,6 +27,7 @@ func TestCodeFromError(t *testing.T) {
 		"pkg.Error => ErrNotFound":        {errors.WithMessage(&ErrNotFound{}, "foo"), codes.NotFound},
 		"pkg.Error => ErrInvalidArgument": {errors.WithMessage(&ErrInvalidArgument{}, "foo"), codes.InvalidArgument},
 		"pkg.Error":                       {errors.New("foo"), codes.Unknown},
+		"nil":                             {nil, codes.OK},
 	}
 
 	for name, tc := range tests {
