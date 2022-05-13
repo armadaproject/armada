c043e50a8046c24ef76e0b20083b3d633cc7ce6c
Daniel Rastelli <rastellidani@gmail.com>
2022-05-13 15:53:06 +0100

    Add refreshing token source

--- pkg/client/auth/oidc/kubernetes.go
+++ pkg/client/auth/oidc/kubernetes.go
@@ -1,16 +1,13 @@
 package oidc
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
 
-	openId "github.com/coreos/go-oidc"
 	"golang.org/x/oauth2"
 )
 
@@ -21,63 +18,48 @@ type KubernetesDetails struct {
 }
 
 func AuthenticateKubernetes(config KubernetesDetails) (*TokenCredentials, error) {
-	ctx := context.Background()
-	provider, err := openId.NewProvider(ctx, config.ProviderUrl)
-	if err != nil {
-		fmt.Println("#################################")
-		fmt.Println("Failed to create provider!!!")
-		fmt.Println("#################################")
-		return nil, err
+	tokenSource := functionTokenSource{
+		getToken: func() (*oauth2.Token, error) {
+			kubernetesToken, err := getKubernetesToken()
+			if err != nil {
+				return nil, err
+			}
+
+			c := &http.Client{}
+			resp, err := c.PostForm(config.ProviderUrl+"/connect/token",
+				url.Values{
+					"client_id":        {config.ClientId},
+					"scope":            {strings.Join(config.Scopes, " ")},
+					"grant_type":       {"kubernetes"},
+					"client_assertion": {kubernetesToken},
+				})
+
+			if err != nil {
+				return nil, err
+			}
+
+			if resp.StatusCode == 400 {
+				var errResp oauthErrorResponse
+				err = json.NewDecoder(resp.Body).Decode(&errResp)
+				if err != nil {
+					return nil, err
+				}
+				return nil, errors.New(errResp.Error)
+			} else if resp.StatusCode != 200 {
+				return nil, makeErrorForHTTPResponse(resp)
+			}
+
+			var token oauth2.Token
+			err = json.NewDecoder(resp.Body).Decode(&token)
+			if err != nil {
+				return nil, err
+			}
+
+			return &token, nil
+		},
 	}
 
-	oauth := oauth2.Config{
-		ClientID: config.ClientId,
-		Endpoint: provider.Endpoint(),
-		Scopes:   append(config.Scopes, openId.ScopeOpenID),
-	}
-
-	kubernetesToken, err := getKubernetesToken()
-	if err != nil {
-		fmt.Println("#################################")
-		fmt.Println("Failed to get Kubernetes Token!!!")
-		fmt.Println("#################################")
-		return nil, err
-	}
-
-	c := &http.Client{}
-	resp, err := c.PostForm(config.ProviderUrl+"/connect/token",
-		url.Values{
-			"client_id":        {config.ClientId},
-			"scope":            {strings.Join(config.Scopes, " ")},
-			"grant_type":       {"kubernetes"},
-			"client_assertion": {kubernetesToken},
-		})
-
-	if err != nil {
-		fmt.Println("#################################")
-		fmt.Println("Failed to make post request!!!")
-		fmt.Println("#################################")
-		return nil, err
-	}
-
-	if resp.StatusCode == 400 {
-		var errResp oauthErrorResponse
-		err = json.NewDecoder(resp.Body).Decode(&errResp)
-		if err != nil {
-			return nil, err
-		}
-		return nil, errors.New(errResp.Error)
-	} else if resp.StatusCode != 200 {
-		return nil, makeErrorForHTTPResponse(resp)
-	}
-
-	var token oauth2.Token
-	err = json.NewDecoder(resp.Body).Decode(&token)
-	if err != nil {
-		return nil, err
-	}
-
-	return &TokenCredentials{tokenSource: oauth.TokenSource(ctx, &token)}, nil
+	return &TokenCredentials{tokenSource: &tokenSource}, nil
 }
 
 func getKubernetesToken() (string, error) {
