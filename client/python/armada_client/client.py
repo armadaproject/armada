e9c812204300cff27402d0b522371c3cdcae52d8
Kevin Hannon <kevinhannon@Kevins-MacBook-Pro.local>
2022-05-18 21:40:40 +0200

    client stuff

--- client/python/armada_client/client.py
+++ client/python/armada_client/client.py
@@ -23,18 +23,21 @@ from armada_client.k8s.io.apimachinery.pkg.api.resource import (
     generated_pb2 as api_resource,
 )
 
+
 class AuthMethod(Enum):
-        Anonymous = auto()
-        Basic = auto()
-        OpenId = auto()
-        Kerberos = auto()
+    Anonymous = auto()
+    Basic = auto()
+    OpenId = auto()
+    Kerberos = auto()
+
 
 class AuthData:
     def __init__(self, method: AuthMethod = AuthMethod.Anonymous,
-        username: Optional[str] = None, password: Optional[str] = None, # for BasicAuth
-        oidc_token: Optional[str] = None,
-        # TODO add options for kerberos authentication
-    ):
+                 # for BasicAuth
+                 username: Optional[str] = None, password: Optional[str] = None,
+                 oidc_token: Optional[str] = None,
+                 # TODO add options for kerberos authentication
+                 ):
 
         self.method = method
 
@@ -53,6 +56,8 @@ class AuthData:
 
 # The python GRPC library requires authentication data to be provided as an AuthMetadataPlugin
 # The username/password are colon-delimted and base64 encoded as per RFC 2617
+
+
 class GrpcBasicAuth(grpc.AuthMetadataPlugin):
     def __init__(self, username: str, password: str):
         self._username = username
@@ -67,16 +72,17 @@ class GrpcBasicAuth(grpc.AuthMetadataPlugin):
             None
         )
 
+
 class ArmadaClient:
     def __init__(self, host: str, port: int,
-        auth_data: AuthData = AuthData(),
-        disable_ssl: bool = False,
-        max_workers = os.cpu_count()):
+                 auth_data: AuthData = AuthData(),
+                 disable_ssl: bool = False,
+                 max_workers=os.cpu_count()):
 
         self.host = host
         self.port = port
         self.disable_ssl = disable_ssl
-        self.executor = ThreadPoolExecutor(max_workers = max_workers or 1)
+        self.executor = ThreadPoolExecutor(max_workers=max_workers or 1)
 
         if auth_data.method == AuthMethod.Anonymous:
             self.channel = grpc.insecure_channel(f'{host}:{port}')
@@ -88,13 +94,14 @@ class ArmadaClient:
                 channel_credentials = grpc.ssl_channel_credentials()
 
             self.channel = grpc.secure_channel(f'{host}:{port}',
-                grpc.composite_channel_credentials(
-                    channel_credentials,
-                    grpc.metadata_call_credentials(
-                        GrpcBasicAuth(auth_data.username, auth_data.password)
-                    )
-                )
-            )
+                                               grpc.composite_channel_credentials(
+                                                   channel_credentials,
+                                                   grpc.metadata_call_credentials(
+                                                       GrpcBasicAuth(
+                                                           auth_data.username, auth_data.password)
+                                                   )
+                                               )
+                                               )
         elif auth_data.method == AuthMethod.OpenId:
             pass
         elif auth_data.method == AuthMethod.Kerberos:
@@ -105,7 +112,8 @@ class ArmadaClient:
         self.usage_stub = usage_pb2_grpc.UsageStub(self.channel)
 
     def get_job_events_stream(self, queue, job_set_id, from_message_id=None, watch=False):
-        jsr = event_pb2.JobSetRequest(queue=queue, from_message_id=from_message_id, watch=False)
+        jsr = event_pb2.JobSetRequest(
+            queue=queue, from_message_id=from_message_id, watch=False)
         jse = self.event_stub.GetJobSetEvents(queue, job_set_id, jsr)
 
     def submit_jobs(self, queue, job_set_id, job_request_items):
@@ -158,8 +166,10 @@ class ArmadaClient:
         return response
 
     def watch_events(self, on_event, queue, job_set_id, from_message_id=None):
-        jsr = event_pb2.JobSetRequest(queue=queue, id=job_set_id, from_message_id=from_message_id, watch=True, errorIfMissing=True)
+        jsr = event_pb2.JobSetRequest(
+            queue=queue, id=job_set_id, from_message_id=from_message_id, watch=True, errorIfMissing=True)
         event_stream = self.event_stub.GetJobSetEvents(jsr)
+
         def event_counter():
             try:
                 nonlocal event_stream
@@ -180,109 +190,3 @@ class ArmadaClient:
 
     def unwatch_events(self, event_stream):
         event_stream.cancel()
-
-class ArmadaClientTest:
-    def __init__(self, host, port):
-        # TODO: generalize this so tests can be run with a variety of auth schemas
-
-        basic_auth_data = AuthData(AuthMethod.Basic, username='testuser', password='asdfasdf')
-        self.client = ArmadaClient(host, port, basic_auth_data, disable_ssl=True)
-
-    def job_submit_request_items_for_test(self, queue, job_set_id):
-        pod = core_v1.PodSpec(
-            volumes=[
-                core_v1.Volume(
-                    name="root-dir",
-                    volumeSource=
-                        core_v1.VolumeSource(flexVolume=
-                            core_v1.FlexVolumeSource(
-                                driver="gr/cifs",
-                                fsType="cifs",
-                                secretRef=core_v1.LocalObjectReference(name="secret-name"),
-                                options={'networkPath': ""}
-                            )
-                        )
-                )
-            ],
-            containers=[
-                core_v1.Container(
-                    name="Container1",
-                    image="index.docker.io/library/ubuntu:latest",
-                    args=["sleep", "10s"],
-                    securityContext=core_v1.SecurityContext(runAsUser=1000),
-                    resources=core_v1.ResourceRequirements(
-                        requests = {
-                            "cpu": api_resource.Quantity(string="120m"),
-                            "memory": api_resource.Quantity(string="510Mi")
-                        },
-                        limits = {
-                            "cpu": api_resource.Quantity(string="120m"),
-                            "memory": api_resource.Quantity(string="510Mi")
-                        }
-                    )
-                )
-            ]
-        )
-
-        return [
-                submit_pb2.JobSubmitRequestItem(
-                    priority = 1,
-                    pod_spec = pod
-                )
-            ]
-
-    def submit_test_job(self, queue, job_set_id):
-        jsr_items = self.job_submit_request_items_for_test(queue, job_set_id)
-        request = submit_pb2.JobSubmitRequest(
-            queue = "test",
-            job_set_id = job_set_id,
-            job_request_items = jsr_items
-        )
-        self.client.submit_jobs(queue, job_set_id, jsr_items)
-
-    def test_watch_events(self):
-        queue_name = "test"
-        job_set_id = f"set-{uuid.uuid1()}"
-
-        self.client.delete_queue(name=queue_name)
-        self.client.create_queue(name=queue_name, priority_factor=200)
-        self.submit_test_job(queue=queue_name, job_set_id=job_set_id)
-        self.client.cancel_jobs(queue=queue_name, job_set_id=job_set_id)
-
-        count = 0
-        def event_counter(e):
-            nonlocal count
-            count += 1
-
-        event_stream = self.client.watch_events(
-            on_event=event_counter,
-            queue=queue_name,
-            job_set_id=job_set_id
-        )
-        time.sleep(1)
-
-        print(count)
-        self.client.unwatch_events(event_stream)
-
-    def test_simple_job_submit_flow(self):
-        queue_name = "test"
-        job_set_id = f"set-{uuid.uuid1()}"
-
-        self.client.create_queue(name=queue_name, priority_factor=200)
-
-        jsr = self.job_submit_request_items_for_test(
-            queue=queue_name, job_set_id=job_set_id
-        )
-
-        self.client.submit_jobs(jsr)
-        cancel_response =  self.client.cancel_jobs(
-            queue=queue_name, job_set_id=job_set_id
-        )
-
-
-    def test_processing_unknown_events(self):
-        pass
-
-
-tester = ArmadaClientTest("127.0.0.1", 50051)
-tester.test_watch_events()
