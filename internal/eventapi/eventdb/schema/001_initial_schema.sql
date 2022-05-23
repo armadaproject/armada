fa9079c5f07c2c3b7867e389ca727f4d013ec3b2
awsdesktop\chrisma <council_tax@hotmail.com>
2022-05-23 09:17:23 +0100

    added indexes

--- internal/eventapi/eventdb/schema/001_initial_schema.sql
+++ internal/eventapi/eventdb/schema/001_initial_schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE jobset
     created   timestamp,
     UNIQUE (queue, jobset)
 );
+CREATE INDEX idx_jobset_created ON jobset(created);
 
 CREATE TABLE latest_seqno
 (
@@ -13,6 +14,7 @@ CREATE TABLE latest_seqno
     seqno       bigint NOT NULL,
     update_time timestamp
 );
+CREATE INDEX latest_seqno_update_time ON latest_seqno(update_time);
 
 CREATE TABLE event
 (
