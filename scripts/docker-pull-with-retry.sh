3992212f094e3fd6472ba14d89d83abff6e2b25b
JamesMurkin <jamesmurkin@hotmail.com>
2022-05-25 21:58:26 +0100

    Fix comment

--- scripts/docker-pull-with-retry.sh
+++ scripts/docker-pull-with-retry.sh
@@ -1,10 +1,10 @@
 IMAGE=$1
 RETRY_COUNT=${2:-5}
 
-# Try to pull image, return success on pull succeed
 for ((i=1; i<=$RETRY_COUNT; i++))
 do
   if docker pull $IMAGE ; then
+    # Return 0 on pull success
     exit 0
   fi
   echo Failed to pull $IMAGE - attempt $i
