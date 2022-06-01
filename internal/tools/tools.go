14110fef92013431650010249c41e2bd7ea27c62
Kevin Hannon <kevinhannon@kevins-mbp.myfiosgateway.com>
2022-06-01 14:03:10 -0400

    whack a mole on CI issue

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -5,7 +5,6 @@ package tools
 
 import (
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/gogo/protobuf/protoc-gen-gogofaster"
 	_ "github.com/gordonklaus/ineffassign"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
