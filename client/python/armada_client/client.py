e7b13d21eda9bb83d7a49f4ae79c412c6042e600
Kevin Hannon <kevinhannon@kevins-mbp.myfiosgateway.com>
2022-06-13 13:52:45 -0400

    Python doc strings

--- client/python/armada_client/client.py
+++ client/python/armada_client/client.py
@@ -4,6 +4,7 @@ Armada Python GRPC Client
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 import os
+from typing import Callable, List, Optional
 import grpc
 from armada_client.armada import (
     event_pb2,
@@ -34,15 +35,6 @@ class ArmadaClient:
         self.event_stub = event_pb2_grpc.EventStub(channel)
         self.usage_stub = usage_pb2_grpc.UsageStub(channel)
 
-    def get_job_events_stream(
-        self, queue: str, job_set_id: str, from_message_id=None, watch=False
-    ):
-        """Implementation of GetJobSetEvents rpc function"""
-        jsr = event_pb2.JobSetRequest(
-            queue=queue, from_message_id=from_message_id, watch=watch
-        )
-        self.event_stub.GetJobSetEvents(queue, job_set_id, jsr)
-
     def submit_jobs(self, queue: str, job_set_id: str, job_request_items):
         """Submit a armada job.
         :param queue: str
@@ -62,8 +54,20 @@ class ArmadaClient:
         response = self.submit_stub.SubmitJobs(request)
         return response
 
-    def cancel_jobs(self, queue=None, job_id=None, job_set_id=None):
-        """Implementation of CancelJobs rpc function"""
+    def cancel_jobs(self, queue=Optional[str], job_id=Optional[str], job_set_id=Optional[str]):
+        """CancelJobs rpc call.  Cancel jobs in a given queue, job_set_id, job_id
+        :param queue: str
+            The name of the queue
+        :param job_id: str
+            The name of the job id 
+        :param job_set_id: List[JobSubmitRequestItem]
+            An array of JobSubmitRequestItems.
+            See the api definition in submit.proto for a definition.
+
+        This calls the SubmitJob rpc call.
+        :return: A JobSubmitResponse object.
+        """
+
         request = submit_pb2.JobCancelRequest(
             queue=queue, job_id=job_id, job_set_id=job_set_id
         )
@@ -71,10 +75,21 @@ class ArmadaClient:
         return response
 
     def reprioritize_jobs(
-        self, new_priority, job_ids=None, job_set_id=None, queue=None
+        self, new_priority: float, job_ids=List[str], job_set_id = Optional[str], queue = Optional[str]
     ):
-        """Implementation of ReprioritizeJobs rpc function"""
+        """Reprioritize jobs with new_priority value.  
+        Can be applied all jobs in a job_set_id or 
+        applied to a list of jobs
+        param: new_priority: float
+            The new priority value for the jobs
+        param: job_ids: A list of job ids
+            Apply new_priority to list of jobs 
+        param: job_set_id: str A job set id
+        param: queue: str The queue 
 
+        This calls the ReprioritizeJobs rpc call.
+        :return: ReprioritizeJobsResponse object.  It is a map of strings.
+        """
         request = submit_pb2.JobReprioritizeRequest(
             job_ids=job_ids,
             job_set_id=job_set_id,
@@ -85,16 +100,31 @@ class ArmadaClient:
         return response
 
     def create_queue(self, name: str, **queue_params):
-        """Implementation of CreateQueue rpc function"""
+        """Create the queue by name.
+        param: name: str
+            The name of the queue
+        param: queue_params: Queue Object
+            The params is a queue object
+
+        This calls the GetQueue rpc call.
+        :return: A queue object.  See the api definition.
+        """
         request = submit_pb2.Queue(name=name, **queue_params)
         response = self.submit_stub.CreateQueue(request)
         return response
 
-    def update_queue(self, name: str, **queue_params):
-        """Implementation of UpdateQueue rpc function"""
+    def update_queue(self, name: str, **queue_params) -> None:
+        """Update the queue of name with values in queue_params
+        param: name: str
+            The name of the queue
+        param: queue_params: Queue Object
+            The params is a queue object
+
+        This calls the UpdateQueue rpc call.
+        :return: An empty object
+        """
         request = submit_pb2.Queue(name=name, **queue_params)
-        response = self.submit_stub.UpdateQueue(request)
-        return response
+        self.submit_stub.UpdateQueue(request)
 
     def delete_queue(self, name: str) -> None:
         """Delete a queue by name.
@@ -106,7 +136,6 @@ class ArmadaClient:
         """
         request = submit_pb2.QueueDeleteRequest(name=name)
         response = self.submit_stub.DeleteQueue(request)
-        return response
 
     def get_queue(self, name: str):
         """Get the queue by name.
@@ -121,13 +150,31 @@ class ArmadaClient:
         return response
 
     def get_queue_info(self, name: str):
-        """Impl of GetQueueInfo"""
+        """Get the queue info by name.
+        param: name: str
+            The name of the queue
+
+        This calls the GetQueueInfo rpc call.
+        : return: A queue info object.  See the api definition.
+        """
         request = submit_pb2.QueueInfoRequest(name=name)
         response = self.submit_stub.GetQueueInfo(request)
         return response
 
-    def watch_events(self, on_event, queue, job_set_id, from_message_id=None):
-        """Impl of WatchEvents"""
+    def watch_events(self, on_event:Callable, queue:str, job_set_id:str, from_message_id=Optional[str]):
+        """Watch events .
+        param: queue: str
+            The name of the queue
+        param: job_set_id: str
+            The name of the job_set_id to watch
+        param: on_event: Callable
+            A function for handling streams 
+        param: from_message_id: str
+            TBD: What is this
+
+        This calls the Watchevents rpc call.
+        : return: A stream of potential messages from events.  See events.proto for a comprehensive list.
+        """
         jsr = event_pb2.JobSetRequest(
             queue=queue,
             id=job_set_id,
