049a1a4296be03bf9aa0b0d418c41187fa120516
Kevin Hannon <kevinhannon@kevins-mbp.myfiosgateway.com>
2022-06-14 17:51:09 -0400

    integration test complete

--- client/python/tests/integration/test_no_auth.py
+++ client/python/tests/integration/test_no_auth.py
@@ -8,15 +8,14 @@ from armada_client.k8s.io.apimachinery.pkg.api.resource import (
     generated_pb2 as api_resource,
 )
 import grpc
-import pytest
 import time
 
 no_auth_client = ArmadaClient(channel=grpc.insecure_channel(target="127.0.0.1:50051"))
 
 
 def test_submit_job():
-    queue_name = f'queue-{uuid.uuid1()}'
-    job_set_name = f'set-{uuid.uuid1()}'
+    queue_name = f"queue-{uuid.uuid1()}"
+    job_set_name = f"set-{uuid.uuid1()}"
     no_auth_client.create_queue(name=queue_name, priority_factor=200)
     no_auth_client.submit_jobs(
         queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
@@ -28,8 +27,8 @@ def test_submit_job():
 
 
 def test_submit_job_and_cancel_by_id():
-    queue_name = f'queue-{uuid.uuid1()}'
-    job_set_name = f'set-{uuid.uuid1()}'
+    queue_name = f"queue-{uuid.uuid1()}"
+    job_set_name = f"set-{uuid.uuid1()}"
     no_auth_client.create_queue(name=queue_name, priority_factor=200)
     jobs = no_auth_client.submit_jobs(
         queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
@@ -39,9 +38,20 @@ def test_submit_job_and_cancel_by_id():
     no_auth_client.delete_queue(name=queue_name)
 
 
+def test_submit_job_and_cancel_by_queue_job_id():
+    queue_name = f"queue-{uuid.uuid1()}"
+    job_set_name = f"set-{uuid.uuid1()}"
+    no_auth_client.create_queue(name=queue_name, priority_factor=200)
+    no_auth_client.submit_jobs(
+        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    )
+    # Jobs can must be finished before deleting queue
+    no_auth_client.cancel_jobs(queue=queue_name, job_set_id=job_set_name)
+    no_auth_client.delete_queue(name=queue_name)
+
 
 def test_get_queue():
-    queue_name = f'queue-{uuid.uuid1()}'
+    queue_name = f"queue-{uuid.uuid1()}"
     no_auth_client.create_queue(name=queue_name, priority_factor=200)
 
     queue = no_auth_client.get_queue(name=queue_name)
@@ -49,10 +59,8 @@ def test_get_queue():
     no_auth_client.delete_queue(name=queue_name)
 
 
-
 def test_get_queue_info():
-    queue_name = f'queue-{uuid.uuid1()}'
-    job_set_name = f'set-{uuid.uuid1()}'
+    queue_name = f"queue-{uuid.uuid1()}"
     no_auth_client.create_queue(name=queue_name, priority_factor=200)
 
     queue = no_auth_client.get_queue_info(name=queue_name)
@@ -60,20 +68,23 @@ def test_get_queue_info():
     assert not queue.active_job_sets
     no_auth_client.delete_queue(name=queue_name)
 
+
 def test_get_job_events_stream():
-    queue_name = f'queue-{uuid.uuid1()}'
-    job_set_name = f'set-{uuid.uuid1()}'
+    queue_name = f"queue-{uuid.uuid1()}"
+    job_set_name = f"set-{uuid.uuid1()}"
+    no_auth_client.delete_queue(name=queue_name)
     no_auth_client.create_queue(name=queue_name, priority_factor=1.0)
     jobs = no_auth_client.submit_jobs(
         queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
     )
+    time.sleep(2)
     # Jobs can must be finished before deleting queue
     job_id = jobs.job_response_items[0].job_id
-    event_stream = no_auth_client.get_job_events_stream(queue=queue_name, job_set_id=job_set_name)
-    print(job_id)
+    event_stream = no_auth_client.get_job_events_stream(
+        queue=queue_name, job_set_id=job_set_name
+    )
     found_successful_job = False
     for event in event_stream:
-        print(event.message)
         if job_id == event.message.succeeded.job_id:
             found_successful_job = True
             break
