97d051f7d96a003d56967fa9e6e84f49535dede1
EC2 Default User <ec2-user@ip-172-31-93-184.ec2.internal>
2022-06-16 19:37:15 +0000

    remove f strings

--- client/python/tests/unit/test_search_for_job_complete.py
+++ client/python/tests/unit/test_search_for_job_complete.py
@@ -1,10 +1,11 @@
 from armada_client.armada import event_pb2
 from armada_client.client import search_for_job_complete
 
+
 def test_succeded_event():
     event_stream_message = event_pb2.EventStreamMessage()
     event_message = event_pb2.EventMessage()
-    event_stream_message.id = 'test'
+    event_stream_message.id = "test"
 
     successful_event = event_pb2.JobSucceededEvent(job_id="id")
     event_message.succeeded.CopyFrom(successful_event)
@@ -13,12 +14,13 @@ def test_succeded_event():
     events = [event_stream_message]
     job_complete = search_for_job_complete(events, job_name="test", job_id="id")
     assert job_complete[0] == "successful"
-    assert job_complete[1] == f"Armada job:id test:id succeeded"
+    assert job_complete[1] == "Armada job:id test:id succeeded"
+
 
 def test_failed_event():
     event_stream_message = event_pb2.EventStreamMessage()
     event_message = event_pb2.EventMessage()
-    event_stream_message.id = 'test'
+    event_stream_message.id = "test"
 
     failed_event = event_pb2.JobFailedEvent(job_id="id", reason="test fail")
     event_message.failed.CopyFrom(failed_event)
@@ -27,12 +29,15 @@ def test_failed_event():
     events = [event_stream_message]
     job_complete = search_for_job_complete(events, job_name="test", job_id="id")
     assert job_complete[0] == "failed"
-    assert job_complete[1] == f"Armada job:id test:id failed\nfailed with reason test fail"
+    assert (
+        job_complete[1] == "Armada job:id test:id failed\nfailed with reason test fail"
+    )
+
 
 def test_cancelling_event():
     event_stream_message = event_pb2.EventStreamMessage()
     event_message = event_pb2.EventMessage()
-    event_stream_message.id = 'test'
+    event_stream_message.id = "test"
 
     cancelling_event = event_pb2.JobCancellingEvent(job_id="id")
     event_message.cancelling.CopyFrom(cancelling_event)
@@ -41,12 +46,13 @@ def test_cancelling_event():
     events = [event_stream_message]
     job_complete = search_for_job_complete(events, job_name="test", job_id="id")
     assert job_complete[0] == "cancelling"
-    assert job_complete[1] == f"Armada job:id test:id cancelling"
+    assert job_complete[1] == "Armada job:id test:id cancelling"
+
 
 def test_cancelled_event():
     event_stream_message = event_pb2.EventStreamMessage()
     event_message = event_pb2.EventMessage()
-    event_stream_message.id = 'test'
+    event_stream_message.id = "test"
 
     cancelled_event = event_pb2.JobCancelledEvent(job_id="id")
     event_message.cancelled.CopyFrom(cancelled_event)
@@ -55,12 +61,13 @@ def test_cancelled_event():
     events = [event_stream_message]
     job_complete = search_for_job_complete(events, job_name="test", job_id="id")
     assert job_complete[0] == "cancelled"
-    assert job_complete[1] == f"Armada job:id test:id cancelled"
+    assert job_complete[1] == "Armada job:id test:id cancelled"
+
 
 def test_terminated_event():
     event_stream_message = event_pb2.EventStreamMessage()
     event_message = event_pb2.EventMessage()
-    event_stream_message.id = 'test'
+    event_stream_message.id = "test"
 
     terminated_event = event_pb2.JobTerminatedEvent(job_id="id")
     event_message.terminated.CopyFrom(terminated_event)
@@ -69,4 +76,4 @@ def test_terminated_event():
     events = [event_stream_message]
     job_complete = search_for_job_complete(events, job_name="test", job_id="id")
     assert job_complete[0] == "terminated"
-    assert job_complete[1] == f"Armada job:id test:id terminated"
+    assert job_complete[1] == "Armada job:id test:id terminated"
