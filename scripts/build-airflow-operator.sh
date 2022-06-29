536390597abf8c5e5fa526731008aca977251613
Kevin Hannon <kannon1992@gmail.com>
2022-06-29 20:57:09 +0000

    script sed change

--- scripts/build-airflow-operator.sh
+++ scripts/build-airflow-operator.sh
@@ -2,7 +2,7 @@
 # This script is intended to be run under the docker container at $ARMADADIR/build/python-api-client/
 
 # make the python package armada.client, not pkg.api
-mkdir -p proto-airflow/armada
+mkdir -p proto-airflow
 cp pkg/api/jobservice/jobservice.proto proto-airflow
 sed -i 's/\([^\/]\)pkg\/api/\1jobservice/g' proto-airflow/*.proto
 
@@ -15,4 +15,4 @@ cd ..
 # This hideous code is because we can't use python package option in grpc.
 # See https://github.com/protocolbuffers/protobuf/issues/7061 for an explanation.
 # We need to import these packages as a module.
-sed -i 's/from armada/from armada.jobservice/g' third_party/airflow/armada/jobservice/*.py
+sed -i 's/import jobservice_pb2 as jobservice__pb2/from armada.jobservice import jobservice_pb2 as jobservice__pb2/g' third_party/airflow/armada/jobservice/*.py
