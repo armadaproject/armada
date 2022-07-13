14e55076bf43f09be3c49df733655d41160ff15a
JamesMurkin <jamesmurkin@hotmail.com>
2022-07-13 15:30:18 +0100

    Sent JobLeaseReturned event server side
    On max lease retries we current get events like:
    
    JobPendingEvent
    JobUnableToScheduleEvent
    JobFailedEvent
    JobLeaseReturnedEvent
    
    This is bad because:
     - The lease was really returned before the job was failed
     - It is hard to process by downstream systems (such as Lookout) which can't work out what state the job is currently in
    
    I have now moved the JobLeaseReturned event to server side so events now look like:
    
    JobPendingEvent
    JobUnableToScheduleEvent
    JobLeaseReturnedEvent
    JobFailedEvent
    
    Which now reflects the real order these events occurred and also fixes Lookout ingestion. It also means the executor no longer needs to worry about this issue

--- pkg/api/submit.pb.go
+++ pkg/api/submit.pb.go
@@ -7,12 +7,6 @@ import (
 	context "context"
 	encoding_binary "encoding/binary"
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-	reflect "reflect"
-	strings "strings"
-
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	github_com_gogo_protobuf_sortkeys "github.com/gogo/protobuf/sortkeys"
@@ -21,7 +15,12 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	io "io"
 	v1 "k8s.io/api/core/v1"
+	math "math"
+	math_bits "math/bits"
+	reflect "reflect"
+	strings "strings"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
