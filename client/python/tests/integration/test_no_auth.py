46f1d89d357aa033b19a87b2bbf89cd8d21468d7
Sharpz7 <adam.mcarthur62@gmail.com>
2022-07-19 19:59:31 +0000

    Trying to make a wait_for function

--- client/python/tests/integration/test_no_auth.py
+++ client/python/tests/integration/test_no_auth.py
@@ -1,7 +1,8 @@
-import time
 import uuid
+import time
 import os
 import grpc
+
 import pytest
 
 
@@ -13,90 +14,15 @@ from armada_client.k8s.io.apimachinery.pkg.api.resource import (
 
 
 def client() -> ArmadaClient:
-    server_name, server_port = "localhost", "50051"
-    if os.environ.get("ARMADA_SERVER"):
-        server_name = os.environ.get("ARMADA_SERVER")
+    server_name = os.environ.get("ARMADA_SERVER", "localhost")
+    server_port = os.environ.get("ARMADA_PORT", "50051")
 
-    if os.environ.get("ARMADA_PORT"):
-        server_port = os.environ.get("ARMADA_PORT")
-
-    return ArmadaClient(
-        channel=grpc.insecure_channel(target=f"{server_name}:{server_port}")
-    )
+    return ArmadaClient(channel=grpc.insecure_channel(f"{server_name}:{server_port}"))
 
 
 no_auth_client = client()
 
-
-def sleep(sleep_time):
-    time.sleep(sleep_time)
-
-
 queue_name = f"queue-{uuid.uuid1()}"
-
-
-@pytest.fixture(scope="session", autouse=True)
-def queue():
-
-    no_auth_client.create_queue(name=queue_name, priority_factor=1)
-    sleep(3)
-
-
-def test_get_queue():
-    queue = no_auth_client.get_queue(name=queue_name)
-    assert queue.name == queue_name
-
-
-def test_get_queue_info():
-    queue = no_auth_client.get_queue_info(name=queue_name)
-    assert queue.name == queue_name
-    assert not queue.active_job_sets
-
-
-def test_submit_job_and_cancel_by_id():
-    job_set_name = f"set-{uuid.uuid1()}"
-    jobs = no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
-    )
-    sleep(5)
-    # Jobs can must be finished before deleting queue
-    cancelled_message = no_auth_client.cancel_jobs(
-        job_id=jobs.job_response_items[0].job_id
-    )
-    assert cancelled_message.cancelled_ids[0] == jobs.job_response_items[0].job_id
-
-
-def test_submit_job_and_cancel_by_queue_job_set():
-    job_set_name = f"set-{uuid.uuid1()}"
-    no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
-    )
-    sleep(5)
-    # Jobs can must be finished before deleting queue
-    cancelled_message = no_auth_client.cancel_jobs(
-        queue=queue_name, job_set_id=job_set_name
-    )
-    expected = f"all jobs in job set {job_set_name}"
-    assert expected == cancelled_message.cancelled_ids[0]
-
-
-def test_get_job_events_stream():
-    job_set_name = f"set-{uuid.uuid1()}"
-    jobs = no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
-    )
-    sleep(5)
-    # Jobs can must be finished before deleting queue
-    jobs.job_response_items[0].job_id
-    event_stream = no_auth_client.get_job_events_stream(
-        queue=queue_name, job_set_id=job_set_name
-    )
-    # Avoiding fickle tests, we will just pass this test as long as we find an event.
-    found_event = False
-    for event in event_stream:
-        found_event = True
-        break
-    assert found_event
 
 
 def submit_sleep_job():
@@ -121,3 +47,104 @@ def submit_sleep_job():
     )
 
     return [no_auth_client.create_job_request_item(priority=0, pod_spec=pod)]
+
+
+def wait_for(queue=None, job_set_id=None):
+
+    timeout = 20
+
+    while True:
+        try:
+            if queue:
+                no_auth_client.get_queue(name=queue)
+                return True
+
+            if job_set_id:
+                events = no_auth_client.get_job_events_stream(
+                    queue=queue_name, job_set_id=job_set_id
+                )
+                for _ in events:
+                    break
+
+                return True
+
+        except grpc.RpcError as e:
+            code = e.code()
+            if code != grpc.StatusCode.NOT_FOUND:
+                raise e
+
+        timeout -= 1
+
+        time.sleep(1)
+
+        if timeout <= 0:
+            raise Exception("Timeout")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_queue():
+
+    no_auth_client.create_queue(name=queue_name, priority_factor=1)
+    wait_for(queue=queue_name)
+
+
+def test_get_queue():
+    queue = no_auth_client.get_queue(name=queue_name)
+    assert queue.name == queue_name
+
+
+def test_get_queue_info():
+    queue = no_auth_client.get_queue_info(name=queue_name)
+    assert queue.name == queue_name
+    assert not queue.active_job_sets
+
+
+def test_submit_job_and_cancel_by_id():
+    job_set_name = f"set-{uuid.uuid1()}"
+    jobs = no_auth_client.submit_jobs(
+        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    )
+
+    wait_for(job_set_id=job_set_name)
+
+    cancelled_message = no_auth_client.cancel_jobs(
+        job_id=jobs.job_response_items[0].job_id
+    )
+
+    assert cancelled_message.cancelled_ids[0] == jobs.job_response_items[0].job_id
+
+
+def test_submit_job_and_cancel_by_queue_job_set():
+    job_set_name = f"set-{uuid.uuid1()}"
+    no_auth_client.submit_jobs(
+        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    )
+
+    wait_for(job_set_id=job_set_name)
+
+    cancelled_message = no_auth_client.cancel_jobs(
+        queue=queue_name, job_set_id=job_set_name
+    )
+
+    expected = f"all jobs in job set {job_set_name}"
+    assert expected == cancelled_message.cancelled_ids[0]
+
+
+def test_get_job_events_stream():
+    job_set_name = f"set-{uuid.uuid1()}"
+    no_auth_client.submit_jobs(
+        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    )
+
+    wait_for(job_set_id=job_set_name)
+
+    event_stream = no_auth_client.get_job_events_stream(
+        queue=queue_name, job_set_id=job_set_name
+    )
+
+    # Avoiding fickle tests, we will just pass this test as long as we find an event.
+    found_event = False
+    for _ in event_stream:
+        found_event = True
+        break
+    assert found_event
