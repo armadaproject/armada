918ba3fddef87293c2769dbfea8b28cb512d22f4
Sharpz7 <adam.mcarthur62@gmail.com>
2022-07-21 16:48:08 +0000

    Added final changes to event_typings.py

--- client/python/armada_client/gen/event_typings.py
+++ client/python/armada_client/gen/event_typings.py
@@ -7,35 +7,40 @@ def get_event_states(module):
 
 
 def get_all_job_event_classes(module):
-    return [
-        getattr(module, x)
-        for x in module.__dict__
-        if hasattr(getattr(module, x), "job_id")
-    ]
+    return [x for x in module.__dict__ if hasattr(getattr(module, x), "job_id")]
 
 
-def place_event_states(states):
+def gen_file(states, classes):
     enum_options = "\n".join(f'    {x} = "{x}"' for x in states)
+    classes = ",\n".join(f"    {x}" for x in classes)
 
-    docstring = '\n    """\n' + "    Enum for the event states." + '\n    """\n'
+    states_docstring = '\n    """\n' + "    Enum for the event states." + '\n    """\n'
 
     states_text = (
-        f"from enum import Enum\n\n\nclass EventType(Enum):{docstring}\n"
+        f"from enum import Enum\nfrom typing import Union\n\nfrom armada_client.armada.event_pb2 import (\n{classes}\n)\n\n\nclass EventType(Enum):{states_docstring}\n"
         f"{enum_options}\n"
     )
 
+    union_docstring = "# Union for the Job Event Types.\n"
+
+    union_text = f"{union_docstring}OneOfJobEvent = Union[\n{classes}\n]\n"
+
     with open(f"{root}/armada_client/typings.py", "w", encoding="utf-8") as f:
         f.write(states_text)
+        f.write("\n")
+        f.write(union_text)
 
 
 def main():
     event_module = import_module("armada_client.armada.event_pb2")
 
     states = get_event_states(event_module)
-    place_event_states(states)
     print("Done creating EventStates")
 
-    print(get_all_job_event_classes(event_module))
+    classes = get_all_job_event_classes(event_module)
+    print("Done creating JobEvent classes")
+
+    gen_file(states, classes)
 
 
 if __name__ == "__main__":
