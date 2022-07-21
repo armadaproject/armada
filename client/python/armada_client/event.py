080532e8bd903f0fbc3323089ab1e1d3ae825f1d
Sharpz7 <adam.mcarthur62@gmail.com>
2022-07-21 21:54:28 +0000

    Prep for typings.py

--- client/python/armada_client/event.py
+++ client/python/armada_client/event.py
@@ -1,72 +1,7 @@
-import enum
 import typing
-from dataclasses import dataclass
 
-import google.protobuf.timestamp_pb2 as timestamp_pb2
-
-from armada_client.armada import event_pb2, queue_pb2
-from armada_client.k8s.io.apimachinery.pkg.api.resource import (
-    generated_pb2 as resource_pb2,
-)
-
-
-class EventType(enum.Enum):
-    """
-    Enum for the event states.
-    """
-
-    submitted = "submitted"
-    queued = "queued"
-    duplicate_found = "duplicate_found"
-    leased = "leased"
-    lease_returned = "lease_returned"
-    pending = "pending"
-    running = "running"
-    unable_to_schedule = "unable_to_schedule"
-    failed = "failed"
-    succeeded = "succeeded"
-    reprioritized = "reprioritized"
-    cancelling = "cancelling"
-    cancelled = "cancelled"
-    terminated = "terminated"
-    utilisation = "utilisation"
-    ingress_info = "ingress_info"
-    reprioritizing = "reprioritizing"
-    updated = "updated"
-
-
-@dataclass
-class EventMessage:
-    """
-    EventMessage is the message type for the event stream.
-
-    Based on event_pb2.EventMessage
-    """
-
-    job: queue_pb2.Job
-
-    job_id: str
-    job_set_id: str
-    queue: str
-    kubernetes_id: str
-    pod_name: str
-    pod_namespace: str
-    cluster_id: str
-    node_name: str
-    reason: str
-    requestor: str
-
-    created: timestamp_pb2.Timestamp
-
-    pod_number: int
-    new_priority: float
-
-    container_statuses: typing.Iterable[event_pb2.ContainerStatus]
-
-    exit_codes: typing.Mapping[str, int]
-    ingress_addresses: typing.Mapping[int, str]
-    MaxResourcesForPeriod: typing.Mapping[str, resource_pb2.Quantity]
-    total_cumulative_usage: typing.Mapping[str, resource_pb2.Quantity]
+from armada_client.typings import EventType, OneOfJobEvent
+from armada_client.armada import event_pb2
 
 
 class Event:
@@ -87,7 +22,7 @@ class Event:
         message = getattr(event.message, msg_type)
 
         self.type: EventType = EventType(msg_type)
-        self.message: EventMessage = typing.cast(EventMessage, message)
+        self.message: OneOfJobEvent = typing.cast(OneOfJobEvent, message)
         self.id: int = event.id
 
     def __repr__(self):
