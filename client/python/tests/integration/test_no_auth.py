c6000f1782f47e2bc96a117fe24370e5c17f22df
Sharpz7 <adam.mcarthur62@gmail.com>
2022-07-21 21:33:23 +0000

    Added fixtures

--- client/python/tests/integration/test_no_auth.py
+++ client/python/tests/integration/test_no_auth.py
@@ -11,30 +11,7 @@ from armada_client.k8s.io.apimachinery.pkg.api.resource import (
 )
 
 
-def client() -> ArmadaClient:
-    server_name = os.environ.get("ARMADA_SERVER", "localhost")
-    server_port = os.environ.get("ARMADA_PORT", "50051")
-    server_ssl = os.environ.get("ARMADA_SSL", "false")
-
-    if server_ssl.lower() == "true":
-        channel_credentials = grpc.ssl_channel_credentials()
-        return ArmadaClient(
-            channel=grpc.secure_channel(
-                f"{server_name}:{server_port}", channel_credentials
-            )
-        )
-
-    else:
-        return ArmadaClient(
-            channel=grpc.insecure_channel(f"{server_name}:{server_port}")
-        )
-
-
-no_auth_client = client()
-queue_name = f"queue-{uuid.uuid1()}"
-
-
-def submit_sleep_job():
+def submit_sleep_job(client):
     pod = core_v1.PodSpec(
         containers=[
             core_v1.Container(
@@ -55,10 +32,10 @@ def submit_sleep_job():
         ],
     )
 
-    return [no_auth_client.create_job_request_item(priority=0, pod_spec=pod)]
+    return [client.create_job_request_item(priority=0, pod_spec=pod)]
 
 
-def wait_for(queue=None, job_set_id=None):
+def wait_for(client: ArmadaClient, queue, job_set_id=None):
     """
     Waits for a queue and/or a job_set_id to be active.
 
@@ -70,12 +47,12 @@ def wait_for(queue=None, job_set_id=None):
     while True:
         try:
             if queue:
-                no_auth_client.get_queue(name=queue)
+                client.get_queue(name=queue)
                 return True
 
-            if job_set_id:
-                events = no_auth_client.get_job_events_stream(
-                    queue=queue_name, job_set_id=job_set_id
+            if job_set_id and queue:
+                events = client.get_job_events_stream(
+                    queue=queue, job_set_id=job_set_id
                 )
                 for _ in events:
                     break
@@ -95,64 +72,91 @@ def wait_for(queue=None, job_set_id=None):
             raise Exception("Timeout")
 
 
+@pytest.fixture(scope="session", name="client")
+def no_auth_client() -> ArmadaClient:
+    server_name = os.environ.get("ARMADA_SERVER", "localhost")
+    server_port = os.environ.get("ARMADA_PORT", "50051")
+    server_ssl = os.environ.get("ARMADA_SSL", "false")
+
+    if server_ssl.lower() == "true":
+        channel_credentials = grpc.ssl_channel_credentials()
+        return ArmadaClient(
+            channel=grpc.secure_channel(
+                f"{server_name}:{server_port}", channel_credentials
+            )
+        )
+
+    else:
+        return ArmadaClient(
+            channel=grpc.insecure_channel(f"{server_name}:{server_port}")
+        )
+
+
+@pytest.fixture(scope="session", name="queue_name")
+def get_queue():
+    return f"queue-{uuid.uuid1()}"
+
+
 @pytest.fixture(scope="session", autouse=True)
-def create_queue():
+def create_queue(client: ArmadaClient, queue_name):
 
-    no_auth_client.create_queue(name=queue_name, priority_factor=1)
-    wait_for(queue=queue_name)
+    client.create_queue(name=queue_name, priority_factor=1)
+    wait_for(client, queue=queue_name)
 
 
-def test_get_queue():
-    queue = no_auth_client.get_queue(name=queue_name)
+def test_get_queue(client: ArmadaClient, queue_name):
+    queue = client.get_queue(name=queue_name)
     assert queue.name == queue_name
 
 
-def test_get_queue_info():
-    queue = no_auth_client.get_queue_info(name=queue_name)
+def test_get_queue_info(client: ArmadaClient, queue_name):
+    queue = client.get_queue_info(name=queue_name)
     assert queue.name == queue_name
     assert not queue.active_job_sets
 
 
-def test_submit_job_and_cancel_by_id():
+def test_submit_job_and_cancel_by_id(client: ArmadaClient, queue_name):
     job_set_name = f"set-{uuid.uuid1()}"
-    jobs = no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    jobs = client.submit_jobs(
+        queue=queue_name,
+        job_set_id=job_set_name,
+        job_request_items=submit_sleep_job(client),
     )
 
-    wait_for(job_set_id=job_set_name)
+    wait_for(client, queue=queue_name, job_set_id=job_set_name)
 
-    cancelled_message = no_auth_client.cancel_jobs(
-        job_id=jobs.job_response_items[0].job_id
-    )
+    cancelled_message = client.cancel_jobs(job_id=jobs.job_response_items[0].job_id)
 
     assert cancelled_message.cancelled_ids[0] == jobs.job_response_items[0].job_id
 
 
-def test_submit_job_and_cancel_by_queue_job_set():
+def test_submit_job_and_cancel_by_queue_job_set(client: ArmadaClient, queue_name):
     job_set_name = f"set-{uuid.uuid1()}"
-    no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    client.submit_jobs(
+        queue=queue_name,
+        job_set_id=job_set_name,
+        job_request_items=submit_sleep_job(client),
     )
 
-    wait_for(job_set_id=job_set_name)
+    wait_for(client, queue=queue_name, job_set_id=job_set_name)
 
-    cancelled_message = no_auth_client.cancel_jobs(
-        queue=queue_name, job_set_id=job_set_name
-    )
+    cancelled_message = client.cancel_jobs(queue=queue_name, job_set_id=job_set_name)
 
     expected = f"all jobs in job set {job_set_name}"
     assert expected == cancelled_message.cancelled_ids[0]
 
 
-def test_get_job_events_stream():
+def test_get_job_events_stream(client: ArmadaClient, queue_name):
     job_set_name = f"set-{uuid.uuid1()}"
-    no_auth_client.submit_jobs(
-        queue=queue_name, job_set_id=job_set_name, job_request_items=submit_sleep_job()
+    client.submit_jobs(
+        queue=queue_name,
+        job_set_id=job_set_name,
+        job_request_items=submit_sleep_job(client),
     )
 
-    wait_for(job_set_id=job_set_name)
+    wait_for(client, queue=queue_name, job_set_id=job_set_name)
 
-    event_stream = no_auth_client.get_job_events_stream(
+    event_stream = client.get_job_events_stream(
         queue=queue_name, job_set_id=job_set_name
     )
 
