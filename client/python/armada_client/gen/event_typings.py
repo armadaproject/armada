631b5a9ae9a766a18dbaaf82788bc5c4c6cf0642
Sharpz7 <adam.mcarthur62@gmail.com>
2022-07-21 20:19:03 +0000

    Finished unit tests

--- client/python/armada_client/gen/event_typings.py
+++ client/python/armada_client/gen/event_typings.py
@@ -13,25 +13,29 @@ def get_all_job_event_classes():
     return [x for x in event_pb2.__dict__ if hasattr(getattr(event_pb2, x), "job_id")]
 
 
-def gen_file(states, classes, file=None):
+def gen_file(states, classes):
     enum_options = "\n".join(f'    {x} = "{x}"' for x in states)
     classes = ",\n".join(f"    {x}" for x in classes)
 
     states_docstring = '\n    """\n' + "    Enum for the event states." + '\n    """\n'
 
+    import_text = f"from enum import Enum\nfrom typing import Union\n\nfrom armada_client.armada.event_pb2 import (\n{classes}\n)"
+
     states_text = (
-        f"from enum import Enum\nfrom typing import Union\n\nfrom armada_client.armada.event_pb2 import (\n{classes}\n)"
-        f"\n\n\nclass EventType(Enum):{states_docstring}\n"
-        f"{enum_options}\n"
+        f"\n\n\nclass EventType(Enum):{states_docstring}\n" f"{enum_options}\n\n"
     )
 
     union_docstring = "# Union for the Job Event Types.\n"
 
     union_text = f"{union_docstring}OneOfJobEvent = Union[\n{classes}\n]\n"
 
+    return import_text, states_text, union_text
+
+
+def write_file(import_text, states_text, union_text, file):
     with open(f"{file}", "w", encoding="utf-8") as f:
+        f.write(import_text)
         f.write(states_text)
-        f.write("\n")
         f.write(union_text)
 
 
@@ -42,7 +46,8 @@ def main():
     classes = get_all_job_event_classes()
     print("Done creating JobEvent classes")
 
-    gen_file(states, classes, typings_file)
+    import_text, states_text, union_text = gen_file(states, classes)
+    write_file(import_text, states_text, union_text, typings_file)
 
 
 if __name__ == "__main__":
