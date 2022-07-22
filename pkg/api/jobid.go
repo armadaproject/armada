92af38b8e3e2bb68c6fea71aa5974a4bfe7a7f43
dejanzele <pejcev.dejan@gmail.com>
2022-07-22 12:26:34 +0200

    minor cleanup

--- pkg/api/jobid.go
+++ pkg/api/jobid.go
@@ -1,7 +1,5 @@
 package api
 
-import "time"
-
 func JobIdFromApiEvent(msg *EventMessage) string {
 	switch e := msg.Events.(type) {
 	case *EventMessage_Submitted:
@@ -44,48 +42,4 @@ func JobIdFromApiEvent(msg *EventMessage) string {
 		return e.Updated.JobId
 	}
 	return ""
-}
-
-func CreatedFromApiEvent(msg *EventMessage) time.Time {
-	switch e := msg.Events.(type) {
-	case *EventMessage_Submitted:
-		return e.Submitted.Created
-	case *EventMessage_Queued:
-		return e.Queued.Created
-	case *EventMessage_DuplicateFound:
-		return e.DuplicateFound.Created
-	case *EventMessage_Leased:
-		return e.Leased.Created
-	case *EventMessage_LeaseReturned:
-		return e.LeaseReturned.Created
-	case *EventMessage_LeaseExpired:
-		return e.LeaseExpired.Created
-	case *EventMessage_Pending:
-		return e.Pending.Created
-	case *EventMessage_Running:
-		return e.Running.Created
-	case *EventMessage_UnableToSchedule:
-		return e.UnableToSchedule.Created
-	case *EventMessage_Failed:
-		return e.Failed.Created
-	case *EventMessage_Succeeded:
-		return e.Succeeded.Created
-	case *EventMessage_Reprioritized:
-		return e.Reprioritized.Created
-	case *EventMessage_Cancelling:
-		return e.Cancelling.Created
-	case *EventMessage_Cancelled:
-		return e.Cancelled.Created
-	case *EventMessage_Terminated:
-		return e.Terminated.Created
-	case *EventMessage_Utilisation:
-		return e.Utilisation.Created
-	case *EventMessage_IngressInfo:
-		return e.IngressInfo.Created
-	case *EventMessage_Reprioritizing:
-		return e.Reprioritizing.Created
-	case *EventMessage_Updated:
-		return e.Updated.Created
-	}
-	return time.Time{}
 }
