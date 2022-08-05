7f8ae1a3c1f561549eaa79d4d3558248fad9b6d2
Sharpz7 <adam.mcarthur62@gmail.com>
2022-08-05 15:10:40 +0000

    Completed suggested changes

--- client/python/docs/source/conf.py
+++ client/python/docs/source/conf.py
@@ -39,15 +39,10 @@ extensions = [
 # better in our gh-pages template that renders the generated markdown.
 autodoc_typehints = "description"
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = []
 
+# This setting makes sure that all types are fully documented with their correct paths
+# i.e instead of just "Permissions" we get "armada_client.permissions.Permissions"
 autodoc_typehints_format = "fully-qualified"
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -56,8 +51,3 @@ autodoc_typehints_format = "fully-qualified"
 # a list of builtin themes.
 #
 html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = []
