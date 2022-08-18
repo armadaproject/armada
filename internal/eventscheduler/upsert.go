f4f5d7d16c27747afd884882007cb83b0a8ecff5
Albin Severinson <albin@severinson.org>
2022-08-18 08:18:27 +0000

    Refactor

--- internal/eventscheduler/upsert.go
+++ internal/eventscheduler/upsert.go
@@ -92,112 +92,131 @@ func UpsertRecords(ctx context.Context, db *pgx.Conn, topicName string, writeMes
 		DeferrableMode: pgx.Deferrable,
 	}, func(tx pgx.Tx) error {
 
-		// Load the message ids stored in postgres.
-		queries := New(tx)
-		sqlWriteMessageIds, err := queries.GetTopicMessageIds(ctx, topicName)
-		if err != pgx.ErrNoRows && err != nil {
-			return errors.WithStack(err)
-		}
-
-		// Check if the data stored in postgres is more recent than what we're trying to write.
-		staleWrites := make([]StaleWrite, 0)
-		for _, sqlWriteMessageId := range sqlWriteMessageIds {
-
-			// Convert from the SQL-specific representation of message ids to one that can be used for comparison.
-			dbMessageId := pulsarMessageIdFromPulsarRecord(sqlWriteMessageId)
-
-			// If the id loaded from the database is more recent than the one provided, abort the transaction.
-			// Since the data we're trying to write is stale.
-			if writeMessageId, ok := writeMessageIds[dbMessageId.PartitionIdx()]; ok {
-				isGreater, err := dbMessageId.Greater(writeMessageId)
-				if err != nil {
-					return err
-				}
-				if isGreater {
-					staleWrites = append(staleWrites, StaleWrite{
-						WriteMessageId: writeMessageId,
-						DbMessageId:    dbMessageId,
-					})
-				}
-			}
-		}
-		if len(staleWrites) > 0 {
-			return &ErrStaleWrite{
-				Topic:       topicName,
-				StaleWrites: staleWrites,
-			}
-		}
-
-		// Otherwise, we have more recent data than what is already stored in the db that we should write.
-		// Update the message id in postgres to reflect the data to be written.
-		//
-		// TODO: Add a call to insert these in a single call.
-		for _, writeMessageId := range writeMessageIds {
-			err = queries.UpsertMessageId(ctx, UpsertMessageIdParams{
-				Topic:        topicName,
-				Ledgerid:     writeMessageId.LedgerID(),
-				Entryid:      writeMessageId.EntryID(),
-				Batchidx:     writeMessageId.BatchIdx(),
-				Partitionidx: writeMessageId.PartitionIdx(),
-			})
-			if err != nil {
-				return errors.WithStack(err)
-			}
-		}
-
-		// Write records into postgres.
-		// First, create a temporary table for loading data in bulk using the copy protocol.
-		// The table is created with the provided schema.
-		tempTableName := "insert" // TODO: Is this name unique per transaction?
-		_, err = tx.Exec(ctx, fmt.Sprintf("CREATE TEMPORARY TABLE %s %s ON COMMIT DROP;", tempTableName, schema))
+		err := IdempotencyCheck(ctx, tx, topicName, writeMessageIds)
 		if err != nil {
-			return errors.WithStack(err)
+			return err
 		}
 
-		// Use the postgres-specific COPY wire protocol to load data into the new table in a single operation.
-		// The COPY protocol can be faster than repeated inserts for as little as 5 rows; see
-		// https://www.postgresql.org/docs/current/populate.html
-		// https://pkg.go.dev/github.com/jackc/pgx/v4#hdr-Copy_Protocol
-		//
-		// We're guaranteed there is at least one record.
-		names, _ := NamesValuesFromRecord(records[0])
-		if len(names) < 2 {
-			return errors.Errorf("Names() must return at least 2 elements, but got %v", names)
-		}
-		n, err := tx.CopyFrom(ctx,
-			pgx.Identifier{tempTableName},
-			names,
-			pgx.CopyFromSlice(len(records), func(i int) ([]interface{}, error) {
-				// TODO: Are we guaranteed that values always come in the order listed in the record? Otherwise we need to control the order.
-				_, values := NamesValuesFromRecord(records[i])
-				return values, nil
-			}),
-		)
+		err = CopyProtocolUpsert(ctx, tx, topicName, tableName, schema, records)
 		if err != nil {
-			return errors.WithStack(err)
-		}
-		if n != int64(len(records)) {
-			return errors.Errorf("only %d out of %d rows were inserted", n, len(records))
-		}
-
-		// Move those rows into the main table, using ON CONFLICT rules to over-write existing rows.
-		var b strings.Builder
-		fmt.Fprintf(&b, "INSERT INTO %s SELECT * from %s ", tableName, tempTableName)
-		fmt.Fprintf(&b, "ON CONFLICT (%s) DO UPDATE SET ", names[0])
-		for i, name := range names[1:] {
-			fmt.Fprintf(&b, "%s = EXCLUDED.%s", name, name)
-			if i != len(names)-2 {
-				fmt.Fprintf(&b, ", ")
-			}
-		}
-		fmt.Fprint(&b, ";")
-		_, err = tx.Exec(ctx, b.String())
-		if err != nil {
-			return errors.WithStack(err)
+			return err
 		}
 
 		return nil
 	})
+}
+
+func IdempotencyCheck(ctx context.Context, tx DBTX, topicName string, writeMessageIds map[int32]pulsar.MessageID) error {
+
+	// Load the message ids stored in postgres.
+	queries := New(tx)
+	sqlWriteMessageIds, err := queries.GetTopicMessageIds(ctx, topicName)
+	if err != pgx.ErrNoRows && err != nil {
+		return errors.WithStack(err)
+	}
+
+	// Check if the data stored in postgres is more recent than what we're trying to write.
+	staleWrites := make([]StaleWrite, 0)
+	for _, sqlWriteMessageId := range sqlWriteMessageIds {
+
+		// Convert from the SQL-specific representation of message ids to one that can be used for comparison.
+		dbMessageId := pulsarMessageIdFromPulsarRecord(sqlWriteMessageId)
+
+		// If the id loaded from the database is more recent than the one provided, abort the transaction.
+		// Since the data we're trying to write is stale.
+		if writeMessageId, ok := writeMessageIds[dbMessageId.PartitionIdx()]; ok {
+			isGreater, err := dbMessageId.Greater(writeMessageId)
+			if err != nil {
+				return err
+			}
+			if isGreater {
+				staleWrites = append(staleWrites, StaleWrite{
+					WriteMessageId: writeMessageId,
+					DbMessageId:    dbMessageId,
+				})
+			}
+		}
+	}
+	if len(staleWrites) > 0 {
+		return &ErrStaleWrite{
+			Topic:       topicName,
+			StaleWrites: staleWrites,
+		}
+	}
+
+	// Otherwise, we have more recent data than what is already stored in the db that we should write.
+	// Update the message id in postgres to reflect the data to be written.
+	//
+	// TODO: Add a call to insert these in a single call.
+	for _, writeMessageId := range writeMessageIds {
+		err = queries.UpsertMessageId(ctx, UpsertMessageIdParams{
+			Topic:        topicName,
+			Ledgerid:     writeMessageId.LedgerID(),
+			Entryid:      writeMessageId.EntryID(),
+			Batchidx:     writeMessageId.BatchIdx(),
+			Partitionidx: writeMessageId.PartitionIdx(),
+		})
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+func CopyProtocolUpsert(ctx context.Context, tx pgx.Tx, topicName string, tableName string, schema string, records []interface{}) error {
+
+	// Write records into postgres.
+	// First, create a temporary table for loading data in bulk using the copy protocol.
+	// The table is created with the provided schema.
+	tempTableName := "insert" // TODO: Is this name unique per transaction?
+	_, err := tx.Exec(ctx, fmt.Sprintf("CREATE TEMPORARY TABLE %s %s ON COMMIT DROP;", tempTableName, schema))
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	// Use the postgres-specific COPY wire protocol to load data into the new table in a single operation.
+	// The COPY protocol can be faster than repeated inserts for as little as 5 rows; see
+	// https://www.postgresql.org/docs/current/populate.html
+	// https://pkg.go.dev/github.com/jackc/pgx/v4#hdr-Copy_Protocol
+	//
+	// We're guaranteed there is at least one record.
+	names, _ := NamesValuesFromRecord(records[0])
+	if len(names) < 2 {
+		return errors.Errorf("Names() must return at least 2 elements, but got %v", names)
+	}
+	n, err := tx.CopyFrom(ctx,
+		pgx.Identifier{tempTableName},
+		names,
+		pgx.CopyFromSlice(len(records), func(i int) ([]interface{}, error) {
+			// TODO: Are we guaranteed that values always come in the order listed in the record? Otherwise we need to control the order.
+			_, values := NamesValuesFromRecord(records[i])
+			return values, nil
+		}),
+	)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if n != int64(len(records)) {
+		return errors.Errorf("only %d out of %d rows were inserted", n, len(records))
+	}
+
+	// Move those rows into the main table, using ON CONFLICT rules to over-write existing rows.
+	var b strings.Builder
+	fmt.Fprintf(&b, "INSERT INTO %s SELECT * from %s ", tableName, tempTableName)
+	fmt.Fprintf(&b, "ON CONFLICT (%s) DO UPDATE SET ", names[0])
+	for i, name := range names[1:] {
+		fmt.Fprintf(&b, "%s = EXCLUDED.%s", name, name)
+		if i != len(names)-2 {
+			fmt.Fprintf(&b, ", ")
+		}
+	}
+	fmt.Fprint(&b, ";")
+	_, err = tx.Exec(ctx, b.String())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
 }
 
 func pulsarMessageIdFromPulsarRecord(pulsarRecord Pulsar) *pulsarutils.PulsarMessageId {
