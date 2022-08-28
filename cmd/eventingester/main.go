eda5fe4e1d866e960f2493105f18f1543c3edd58
awsdesktop\chrisma <council_tax@hotmail.com>
2022-08-28 13:14:53 +0100

    fix import

--- cmd/eventingester/main.go
+++ cmd/eventingester/main.go
@@ -1,30 +1,21 @@
 package main
 
 import (
-	ctx "context"
-
 	"github.com/G-Research/armada/internal/eventingester"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 
-	"github.com/G-Research/armada/internal/common/database"
-	"github.com/G-Research/armada/internal/eventingester/configuration"
-	"github.com/G-Research/armada/internal/eventingester/store/schema/statik"
-	"github.com/G-Research/armada/internal/lookout/postgres"
-
 	"github.com/G-Research/armada/internal/common"
+	"github.com/G-Research/armada/internal/eventingester/configuration"
 )
 
 const (
 	CustomConfigLocation string = "config"
-	MigrateDatabase      string = "migrateDatabase"
 )
 
 func init() {
 	pflag.StringSlice(CustomConfigLocation, []string{}, "Fully qualified path to application configuration file (for multiple config files repeat this arg or separate paths with commas)")
-	pflag.Bool(MigrateDatabase, false, "Migrate database instead of running server")
 	pflag.Parse()
 }
 
@@ -36,26 +27,5 @@ func main() {
 	userSpecifiedConfigs := viper.GetStringSlice(CustomConfigLocation)
 
 	common.LoadConfig(&config, "./config/eventingester", userSpecifiedConfigs)
-	if viper.GetBool(MigrateDatabase) {
-		err := migrateDatabase(config)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		eventingester.Run(&config)
-	}
-}
-
-func migrateDatabase(config configuration.EventIngesterConfiguration) error {
-	log.Infof("Opening connection pool to postgres")
-	db, err := postgres.OpenPgxPool(config.Postgres)
-	defer db.Close()
-	if err != nil {
-		return err
-	}
-	migrations, err := database.GetMigrations(statik.EventapiSql)
-	if err != nil {
-		return err
-	}
-	return database.UpdateDatabase(ctx.Background(), db, migrations)
+	eventingester.Run(&config)
 }
