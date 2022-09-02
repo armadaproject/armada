d30e3f8ff4e7765abc946203b6c571b798c80628
Albin Severinson <albin@severinson.org>
2022-09-02 14:49:41 +0000

    Add service to batch db ops

---
+++ internal/scheduler/opsbatching.go
@@ -0,0 +1,88 @@
+package scheduler
+
+import (
+	"context"
+	"time"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
+	"github.com/severinson/pulsar-client-go/pulsar"
+
+	"github.com/G-Research/armada/internal/common/eventutil"
+	"github.com/G-Research/armada/internal/common/logging"
+)
+
+// DbOperationsBatcher is a service that consumes event sequences
+// and produces optimised sequences of database operations.
+type DbOperationsBatcher struct {
+	In  chan *eventutil.EventSequenceWithMessageIds
+	Out chan *DbOperationsWithMessageIds
+	// Max number of Pulsar messages to include with each batch.
+	MaxMessages int
+	// Max time interval between batches.
+	MaxInterval time.Duration
+}
+
+func NewDbOperationsBatcher(in chan *eventutil.EventSequenceWithMessageIds) *DbOperationsBatcher {
+	return &DbOperationsBatcher{
+		In:          in,
+		Out:         make(chan *DbOperationsWithMessageIds),
+		MaxMessages: 1000,
+		MaxInterval: 5 * time.Second,
+	}
+}
+
+// DbOperationsWithMessageIds bundles a sequence of db ops with the ids of all Pulsar
+// messages that were consumed to produce it.
+type DbOperationsWithMessageIds struct {
+	Ops        []DbOperation
+	MessageIds []pulsar.MessageID
+}
+
+func (srv *DbOperationsBatcher) Run(ctx context.Context) error {
+	log := ctxlogrus.Extract(ctx)
+	ticker := time.NewTicker(srv.MaxInterval)
+	var batch *DbOperationsWithMessageIds
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if batch == nil {
+				break
+			}
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case srv.Out <- batch:
+			}
+			batch = nil
+		case sequenceWithIds := <-srv.In:
+			if sequenceWithIds == nil || sequenceWithIds.Sequence == nil {
+				continue
+			}
+			if batch == nil {
+				batch = &DbOperationsWithMessageIds{}
+			}
+
+			for i := range sequenceWithIds.Sequence.GetEvents() {
+				op, err := DbOpFromEventInSequence(sequenceWithIds.Sequence, i)
+				if err != nil {
+					logging.WithStacktrace(log, err).Error("failed to convert event to db op")
+					continue
+				} else if op == nil {
+					continue // No op corresponding to this event
+				}
+				batch.Ops = AppendDbOperation(batch.Ops, op)
+				batch.MessageIds = append(batch.MessageIds, sequenceWithIds.MessageIds...)
+			}
+			if len(batch.MessageIds) > srv.MaxMessages {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case srv.Out <- batch:
+				}
+				batch = nil
+			}
+		}
+	}
+}
