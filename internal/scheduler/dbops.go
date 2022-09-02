ae4690e0cb3ae875b949127e546f191db1483d84
Albin Severinson <albin@severinson.org>
2022-09-02 11:20:31 +0000

    Db op cleanup and minor fixes

--- internal/scheduler/dbops.go
+++ internal/scheduler/dbops.go
@@ -15,15 +15,21 @@ import (
 // - Job run operations (i.e., modify particular runs).
 //
 // To improve performance, several ops can be merged into a single op if of the same type.
-// To increase the number of ops that can be merged, ops can sometimes be reordered:
+// To increase the number of ops that can be merged, ops can sometimes be reordered.
+//
+// Specifically, an op can be applied before another if:
 // - Insert jobs: if prior op doesn't affect the job set.
-// - Insert runs: if prior op doesn't affect the job set or defines the job.
+// - Insert runs: if prior op doesn't affect the job set or defines the corresponding job.
 // - Job set operations: if not affecting a job defined in prior op.
 // - Job operations: if not affecting a job defined in a prior op.
 // - Job run operations: if not affecting a run defined in a prior op.
+//
+// In addition, UpdateJobPriorities can never be applied beforee UpdateJobSetPriorities
+// and vice versa, since one may overwrite values set by the other.
 type DbOperation interface {
-	// a.Merge(b) merges b into a.
+	// a.Merge(b) attempts to merge b into a, creating a single combined op.
 	// Returns true if merging was successful.
+	// If successful, modifies a in-place.
 	// If not successful, neither op is mutated.
 	Merge(DbOperation) bool
 	// a.CanBeAppliedBefore(b) returns true if a can be placed before b
@@ -63,20 +69,20 @@ func discardNilOps(ops []DbOperation) []DbOperation {
 }
 
 // Db operations (implements DbOperation).
-type InsertJobs map[uuid.UUID]Job
-type InsertRuns map[uuid.UUID]Run
-type InsertRunAssignments map[uuid.UUID]JobRunAssignment
-type UpdateJobSetPriorities map[string]int32
+type InsertJobs map[uuid.UUID]*Job
+type InsertRuns map[uuid.UUID]*Run
+type InsertRunAssignments map[uuid.UUID]*JobRunAssignment
+type UpdateJobSetPriorities map[string]int64
 type MarkJobSetsCancelled map[string]bool
 type MarkJobsCancelled map[uuid.UUID]bool
 type MarkJobsSucceeded map[uuid.UUID]bool
 type MarkJobsFailed map[uuid.UUID]bool
-type UpdateJobPriorities map[uuid.UUID]int32
+type UpdateJobPriorities map[uuid.UUID]int64
 type MarkRunsSucceeded map[uuid.UUID]bool
 type MarkRunsFailed map[uuid.UUID]bool
 type MarkRunsRunning map[uuid.UUID]bool
-type InsertJobErrors map[string]JobError
-type InsertJobRunErrors map[string]JobRunError
+type InsertJobErrors map[string]*JobError
+type InsertJobRunErrors map[string]*JobRunError
 
 type JobSetOperation interface {
 	AffectsJobSet(string) bool
@@ -89,19 +95,6 @@ func (a UpdateJobSetPriorities) AffectsJobSet(jobSet string) bool {
 func (a MarkJobSetsCancelled) AffectsJobSet(jobSet string) bool {
 	_, ok := a[jobSet]
 	return ok
-}
-
-type JobOperation interface {
-	AffectsJob(uuid.UUID) bool
-}
-
-func (a InsertJobs) AffectsJob(jobId uuid.UUID) bool {
-	_, ok := a[jobId]
-	return ok
-}
-
-type JobRunOperation interface {
-	AffectsJobRun(uuid.UUID) bool
 }
 
 func (a InsertJobs) Merge(b DbOperation) bool {
@@ -147,9 +140,9 @@ func (a InsertJobRunErrors) Merge(b DbOperation) bool {
 	return mergeInMap(a, b)
 }
 
-// mergeInMap merges a sql op b into a, provided that b is of the same type as a.
+// mergeInMap merges an op b into a, provided that b is of the same type as a.
 // For example, if a is of type MarkJobsCancelled, b is only merged if also of type MarkJobsCancelled.
-// Returns true if the operations were merged and false otherwise.
+// Returns true if the ops were merged and false otherwise.
 func mergeInMap[M ~map[K]V, K comparable, V any](a M, b DbOperation) bool {
 	// Using a type switch here, since using a type assertion
 	// (which should also work in theory) crashes the go1.19 compiler.
@@ -161,7 +154,6 @@ func mergeInMap[M ~map[K]V, K comparable, V any](a M, b DbOperation) bool {
 	return false
 }
 
-// Returns true if a can be placed before b.
 func (a InsertJobs) CanBeAppliedBefore(b DbOperation) bool {
 	// We don't check for job and run ops here,
 	// since job and run ops can never appear before the corresponding InsertJobs.
@@ -175,8 +167,6 @@ func (a InsertJobs) CanBeAppliedBefore(b DbOperation) bool {
 	}
 	return true
 }
-
-// Returns true if a can be placed before b.
 func (a InsertRuns) CanBeAppliedBefore(b DbOperation) bool {
 	// We don't check for run ops here,
 	// since run ops can never appear before the corresponding InsertRuns.
@@ -196,12 +186,14 @@ func (a InsertRuns) CanBeAppliedBefore(b DbOperation) bool {
 	}
 	return true
 }
-
 func (a InsertRunAssignments) CanBeAppliedBefore(b DbOperation) bool {
-	return !definesRun(a, b)
+	// Inserting assignments before a run is defined is ok.
+	// We only require that assignments are written to the db before the run is marked as running.
+	return true
 }
 func (a UpdateJobSetPriorities) CanBeAppliedBefore(b DbOperation) bool {
-	return !definesJobInSet(a, b)
+	_, isUpdateJobPriorities := b.(UpdateJobPriorities)
+	return !isUpdateJobPriorities && !definesJobInSet(a, b)
 }
 func (a MarkJobSetsCancelled) CanBeAppliedBefore(b DbOperation) bool {
 	return !definesJobInSet(a, b) && !definesRunInSet(a, b)
@@ -216,7 +208,8 @@ func (a MarkJobsFailed) CanBeAppliedBefore(b DbOperation) bool {
 	return !definesJob(a, b)
 }
 func (a UpdateJobPriorities) CanBeAppliedBefore(b DbOperation) bool {
-	return !definesJob(a, b)
+	_, isUpdateJobSetPriorities := b.(UpdateJobSetPriorities)
+	return !isUpdateJobSetPriorities && !definesJob(a, b)
 }
 func (a MarkRunsSucceeded) CanBeAppliedBefore(b DbOperation) bool {
 	return !definesRun(a, b)
@@ -228,11 +221,13 @@ func (a MarkRunsRunning) CanBeAppliedBefore(b DbOperation) bool {
 	return !definesRun(a, b)
 }
 func (a InsertJobErrors) CanBeAppliedBefore(b DbOperation) bool {
-	// We allow inserting errors before a job has been marked as failed.
+	// Inserting errors before a job has been marked as failed is ok.
+	// We only require that errors are written to the db before the job is marked as failed.
 	return true
 }
 func (a InsertJobRunErrors) CanBeAppliedBefore(b DbOperation) bool {
-	// We allow inserting errors before a run has been marked as failed.
+	// Inserting errors before a run has been marked as failed is ok.
+	// We only require that errors are written to the db before the run is marked as failed.
 	return true
 }
 
