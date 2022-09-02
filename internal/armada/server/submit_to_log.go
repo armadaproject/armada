6212ef9fff1f153c33383a33624ba46d0b8dde64
Chris Martin <council_tax@hotmail.com>
2022-09-02 11:39:00 +0100

    log missing deduplication (#1451)
    * review comment
    
    * log
    
    * format

--- internal/armada/server/submit_to_log.go
+++ internal/armada/server/submit_to_log.go
@@ -88,17 +88,25 @@ func (srv *PulsarSubmitServer) SubmitJobs(ctx context.Context, req *api.JobSubmi
 		// check for previous job submissions with the ClientId for this queue.
 		// If we find a duplicate, insert the previous jobId in the corresponding response
 		// and generate a job duplicate found event.
-		if apiJob.ClientId != "" && originalIds[apiJob.GetId()] != apiJob.GetId() {
-			jobDuplicateFoundEvents = append(jobDuplicateFoundEvents, &api.JobDuplicateFoundEvent{
-				JobId:         responses[i].JobId,
-				Queue:         req.Queue,
-				JobSetId:      req.JobSetId,
-				Created:       time.Now(),
-				OriginalJobId: originalIds[apiJob.GetId()],
-			})
-			responses[i].JobId = originalIds[apiJob.GetId()]
-			// The job shouldn't be submitted twice. Move on to the next job.
-			continue
+		originalId, found := originalIds[apiJob.GetId()]
+		if apiJob.ClientId != "" && originalId != apiJob.GetId() {
+			if found {
+				jobDuplicateFoundEvents = append(jobDuplicateFoundEvents, &api.JobDuplicateFoundEvent{
+					JobId:         responses[i].JobId,
+					Queue:         req.Queue,
+					JobSetId:      req.JobSetId,
+					Created:       time.Now(),
+					OriginalJobId: originalIds[apiJob.GetId()],
+				})
+				responses[i].JobId = originalIds[apiJob.GetId()]
+				// The job shouldn't be submitted twice. Move on to the next job.
+				continue
+			} else {
+				log.Warnf(
+					"ClientId %s was supplied for job %s but no original jobId could be found.  Deduplication will not be applied",
+					apiJob.ClientId,
+					apiJob.GetId())
+			}
 		}
 
 		if apiJob.PodSpec == nil && len(apiJob.PodSpecs) == 0 {
