119799d0df21a5d462097b514f2b45d4e84f6fce
awsdesktop\chrisma <council_tax@hotmail.com>
2022-09-04 17:22:31 +0100

    more linting fixes

--- internal/armada/repository/apimessages/conversions_test.go
+++ internal/armada/repository/apimessages/conversions_test.go
@@ -12,9 +12,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/G-Research/armada/pkg/armadaevents"
 	"github.com/google/uuid"
 	v1 "k8s.io/api/core/v1"
+
+	"github.com/G-Research/armada/pkg/armadaevents"
 )
 
 const (
