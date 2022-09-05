8156ce8afbac00f9a91670cd0da4913a5a1cf46c
Albin Severinson <albin@severinson.org>
2022-09-05 13:58:20 +0000

    Update WriteDbOp to handle all ops

--- internal/scheduler/writedbops.go
+++ internal/scheduler/writedbops.go
@@ -3,20 +3,20 @@ package scheduler
 import (
 	"context"
 
-	"github.com/google/uuid"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
+	"golang.org/x/exp/maps"
 )
 
 // TODO: The caller of this function should keep retrying on transient failures.
 func WriteDbOp(ctx context.Context, db *pgxpool.Pool, op DbOperation) error {
+	queries := New(db)
 	switch o := op.(type) {
 	case InsertJobs:
 		records := make([]interface{}, len(o))
 		i := 0
-		for _, job := range o {
-			records[i] = *job
+		for _, v := range o {
+			records[i] = *v
 			i++
 		}
 		err := Upsert(ctx, db, "jobs", JobsSchema(), records)
@@ -26,8 +26,8 @@ func WriteDbOp(ctx context.Context, db *pgxpool.Pool, op DbOperation) error {
 	case InsertRuns:
 		records := make([]interface{}, len(o))
 		i := 0
-		for _, run := range o {
-			records[i] = *run
+		for _, v := range o {
+			records[i] = *v
 			i++
 		}
 		err := Upsert(ctx, db, "runs", RunsSchema(), records)
@@ -37,8 +37,8 @@ func WriteDbOp(ctx context.Context, db *pgxpool.Pool, op DbOperation) error {
 	case InsertRunAssignments:
 		records := make([]interface{}, len(o))
 		i := 0
-		for _, jobRunAssignment := range o {
-			records[i] = *jobRunAssignment
+		for _, v := range o {
+			records[i] = *v
 			i++
 		}
 		err := Upsert(ctx, db, "job_run_assignments", JobRunAssignmentSchema(), records)
@@ -46,7 +46,6 @@ func WriteDbOp(ctx context.Context, db *pgxpool.Pool, op DbOperation) error {
 			return err
 		}
 	case UpdateJobSetPriorities:
-		queries := New(db)
 		for jobSet, priority := range o {
 			err := queries.UpdateJobPriorityByJobSet(
 				ctx,
@@ -60,53 +59,7 @@ func WriteDbOp(ctx context.Context, db *pgxpool.Pool, op DbOperation) error {
 			}
 		}
 	case MarkJobSetsCancelled:
-	case MarkJobsCancelled:
-	case MarkJobsSucceeded:
-	case MarkJobsFailed:
-	case UpdateJobPriorities:
-	case MarkRunsSucceeded:
-	case MarkRunsFailed:
-	case MarkRunsRunning:
-	case InsertJobErrors:
-	case InsertJobRunErrors:
-	default:
-		return errors.Errorf("received unexpected op %+v", op)
-	}
-	return nil
-}
-
-func (srv *Ingester) ProcessBatch(ctx context.Context, batch *Batch) error {
-	log := ctxlogrus.Extract(ctx)
-	queries := New(srv.Db)
-
-	// Jobs
-
-	log.Infof("wrote %d jobs into postgres", len(records))
-
-	// Job runs
-
-	// Reprioritisations
-	for _, reprioritisation := range batch.Reprioritisations {
-
-		// TODO: This will be slow if there's a large number of ids.
-		// Could be addressed by using a separate table for priority + upsert.
-		for jobId, priority := range reprioritisation.PrioritiesByJob {
-			err := queries.UpdateJobPriorityById(ctx, UpdateJobPriorityByIdParams{
-				JobID:    jobId,
-				Priority: priority,
-			})
-			if err != nil {
-				return errors.WithStack(err)
-			}
-		}
-	}
-
-	// Job sets cancelled
-	jobSets := make([]string, 0, len(batch.JobSetsCancelled))
-	for jobSet, _ := range batch.JobSetsCancelled {
-		jobSets = append(jobSets, jobSet)
-	}
-	if len(jobSets) > 0 {
+		jobSets := maps.Keys(o)
 		err := queries.MarkJobsCancelledBySets(ctx, jobSets)
 		if err != nil {
 			return errors.WithStack(err)
@@ -115,11 +68,8 @@ func (srv *Ingester) ProcessBatch(ctx context.Context, batch *Batch) error {
 		if err != nil {
 			return errors.WithStack(err)
 		}
-	}
-
-	// Jobs cancelled
-	if len(batch.JobsCancelled) > 0 {
-		jobIds := idsFromMap(batch.JobsCancelled)
+	case MarkJobsCancelled:
+		jobIds := maps.Keys(o)
 		err := queries.MarkJobsCancelledById(ctx, jobIds)
 		if err != nil {
 			return errors.WithStack(err)
@@ -128,82 +78,72 @@ func (srv *Ingester) ProcessBatch(ctx context.Context, batch *Batch) error {
 		if err != nil {
 			return errors.WithStack(err)
 		}
-	}
-
-	// Jobs succeeded
-	err = queries.MarkJobsSucceededById(ctx, idsFromMap(batch.JobsSucceeded))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	// Job errors
-	records = make([]interface{}, len(batch.JobErrors))
-	failedJobIds := make([]uuid.UUID, 0)
-	for i, jobError := range batch.JobErrors {
-		records[i] = jobError
-		if jobError.Terminal {
-			failedJobIds = append(failedJobIds, jobError.JobID)
+	case MarkJobsSucceeded:
+		jobIds := maps.Keys(o)
+		err := queries.MarkJobsSucceededById(ctx, jobIds)
+		if err != nil {
+			return errors.WithStack(err)
 		}
-	}
-	err = Upsert(ctx, srv.Db, "job_errors", JobErrorsSchema(), records)
-	if err != nil {
-		return err // TODO: Keep retrying on transient failures.
-	}
-	log.Infof("wrote %d errors into postgres", len(records))
-
-	// For terminal errors, mark the corresponding job as failed.
-	if len(failedJobIds) > 0 {
-		err := queries.MarkJobsFailedById(ctx, failedJobIds)
+	case MarkJobsFailed:
+		jobIds := maps.Keys(o)
+		err := queries.MarkJobsFailedById(ctx, jobIds)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	case UpdateJobPriorities:
+		// TODO: This will be slow if there's a large number of ids.
+		// Could be addressed by using a separate table for priority + upsert.
+		for jobId, priority := range o {
+			err := queries.UpdateJobPriorityById(ctx, UpdateJobPriorityByIdParams{
+				JobID:    jobId,
+				Priority: priority,
+			})
+			if err != nil {
+				return errors.WithStack(err)
+			}
+		}
+	case MarkRunsSucceeded:
+		runIds := maps.Keys(o)
+		err := queries.MarkJobRunsSucceededById(ctx, runIds)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	case MarkRunsFailed:
+		runIds := maps.Keys(o)
+		err := queries.MarkJobRunsFailedById(ctx, runIds)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	case MarkRunsRunning:
+		runIds := maps.Keys(o)
+		err := queries.MarkJobRunsRunningById(ctx, runIds)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	case InsertJobErrors:
+		records := make([]interface{}, len(o))
+		i := 0
+		for _, v := range o {
+			records[i] = *v
+			i++
+		}
+		err := Upsert(ctx, db, "job_errors", JobErrorsSchema(), records)
 		if err != nil {
 			return err
 		}
-	}
-
-	// Job runs running
-	if len(batch.JobRunsRunning) > 0 {
-		err := queries.MarkJobRunsRunningById(ctx, idsFromMap(batch.JobRunsRunning))
+	case InsertJobRunErrors:
+		records := make([]interface{}, len(o))
+		i := 0
+		for _, v := range o {
+			records[i] = *v
+			i++
+		}
+		err := Upsert(ctx, db, "job_run_errors", JobRunErrorsSchema(), records)
 		if err != nil {
-			return errors.WithStack(err)
+			return err
 		}
+	default:
+		return errors.Errorf("received unexpected op %+v", op)
 	}
-
-	// Job runs succeeded
-	if len(batch.JobRunsSucceeded) > 0 {
-		err := queries.MarkJobRunsSucceededById(ctx, idsFromMap(batch.JobRunsSucceeded))
-		if err != nil {
-			return errors.WithStack(err)
-		}
-	}
-
-	// Job run errors
-	records = make([]interface{}, len(batch.JobRunErrors))
-	failedJobRunIds := make([]uuid.UUID, 0)
-	for i, jobRunError := range batch.JobRunErrors {
-		records[i] = jobRunError
-		if jobRunError.Terminal {
-			failedJobRunIds = append(failedJobRunIds, jobRunError.RunID)
-		}
-	}
-	err = Upsert(ctx, srv.Db, "job_run_errors", JobRunErrorsSchema(), records)
-	if err != nil {
-		return err // TODO: Keep retrying on transient failures.
-	}
-	log.Infof("wrote %d run errors into postgres", len(records))
-
-	// For terminal errors, mark the corresponding job as failed.
-	if len(failedJobRunIds) > 0 {
-		queries.MarkJobRunsFailedById(ctx, failedJobRunIds)
-	}
-
 	return nil
-}
-
-func idsFromMap(set map[uuid.UUID]bool) []uuid.UUID {
-	ids := make([]uuid.UUID, len(set))
-	i := 0
-	for id := range set {
-		ids[i] = id
-		i++
-	}
-	return ids
 }
