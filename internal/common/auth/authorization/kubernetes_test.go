20442edef462fa080f21847eb0dbf60be80e3804
Daniel Rastelli <rastellidani@gmail.com>
2022-09-13 13:35:58 +0100

    lint

--- internal/common/auth/authorization/kubernetes_test.go
+++ internal/common/auth/authorization/kubernetes_test.go
@@ -6,8 +6,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/G-Research/armada/internal/common/auth/configuration"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/G-Research/armada/internal/common/auth/configuration"
 )
 
 // A token
