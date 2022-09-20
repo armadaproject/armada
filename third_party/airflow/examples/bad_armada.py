51dd72f19ee515f665c7882b55a54a00fe1557fd
Kevin Hannon <kannon1992@gmail.com>
2022-09-20 11:44:37 -0400

    rename queue to armada_queue

--- third_party/airflow/examples/bad_armada.py
+++ third_party/airflow/examples/bad_armada.py
@@ -72,7 +72,7 @@ with DAG(
     armada = ArmadaOperator(
         task_id="armada",
         name="armada",
-        queue="test",
+        armada_queue="test",
         job_service_client=job_service_client,
         armada_client=no_auth_client,
         job_request_items=submit_sleep_container(image="busybox"),
@@ -84,7 +84,7 @@ with DAG(
     bad_armada = ArmadaOperator(
         task_id="armada_fail",
         name="armada_fail",
-        queue="test",
+        armada_queue="test",
         job_service_client=job_service_client,
         armada_client=no_auth_client,
         job_request_items=submit_sleep_container(image="nonexistant"),
@@ -92,7 +92,7 @@ with DAG(
     good_armada = ArmadaOperator(
         task_id="good_armada",
         name="good_armada",
-        queue="test",
+        armada_queue="test",
         job_service_client=job_service_client,
         armada_client=no_auth_client,
         job_request_items=submit_sleep_container(image="busybox"),
