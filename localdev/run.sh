b2b4ee63eac09509742e407d64c30f694816535b
Geoff Wilson <G.Wilson@F5.com>
2022-10-03 14:02:36 -0400

    Add run file

---
+++ localdev/run.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+INFRA_SVCS="redis postgres pulsar stan"
+ARMADA_SVCS="armada-server lookout executor binoculars job-service"
+START_SVCS=""
+
+# Start all services mentioned in the compose file we use, with the
+# exception of the service(s) given as arguments to this script
+for SVC in $INFRA_SVCS $ARMADA_SVCS ; do
+  echo $* | grep -q $SVC
+  if [ $? -ne 0 ] ; then
+    START_SVCS="$START_SVCS $SVC"
+  fi
+done
+
+docker-compose up $START_SVCS
