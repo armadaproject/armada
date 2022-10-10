7274d52b7394f37f0b820101bdbba77c20862687
Geoff Wilson <G.Wilson@F5.com>
2022-10-10 13:43:53 -0400

    extract script for kube access and add comments

--- localdev/scripts/binoculars-start.sh
+++ localdev/scripts/binoculars-start.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
 
-# this will update kind kube config copied from host to work inside docker
-if [ -f /.dockerenv ]; then
-    DOCKER_HOST=$(ping -c 1 host.docker.internal | grep -m 1 -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-    echo "$DOCKER_HOST kubernetes.default" >> /etc/hosts
-    cp -R /tmp/.kube ~
-    sed -i s/127.0.0.1/kubernetes.default/ ~/.kube/config
-fi
-
+./localdev/scripts/kube-access.sh
 go run ./cmd/binoculars/main.go --config ./localdev/config/binoculars/base.yaml
--- localdev/scripts/executor-start.sh
+++ localdev/scripts/executor-start.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# this will update kind kube config copied from host to work inside docker
-if [ -f /.dockerenv ]; then
-    DOCKER_HOST=$(ping -c 1 host.docker.internal | grep -m 1 -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-    echo "$DOCKER_HOST kubernetes.default" >> /etc/hosts
-    cp -R /tmp/.kube ~
-    sed -i s/127.0.0.1/kubernetes.default/ ~/.kube/config
-fi
+./localdev/scripts/kube-access.sh
 ARMADA_APPLICATION_CLUSTERID=demo-a ARMADA_METRIC_PORT=9001 go run ./cmd/executor/main.go --config localdev/config/executor/config.yaml
