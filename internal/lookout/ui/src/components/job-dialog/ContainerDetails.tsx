cd58d623e2f5e17a1563f28a25cc3df890e850cf
kannon92 <kannon1992@gmail.com>
2022-10-10 19:38:08 +0000

    Container details use accordion and no state

--- internal/lookout/ui/src/components/job-dialog/ContainerDetails.tsx
+++ internal/lookout/ui/src/components/job-dialog/ContainerDetails.tsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useState } from "react"
+import React, { Fragment } from "react"
 
-import { List, Paper, ListItem, Collapse, ListItemText, Table, TableBody, TableContainer } from "@material-ui/core"
-import { ExpandLess } from "@material-ui/icons"
+import { Table, TableBody, TableContainer, Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core"
+import { ExpandMore } from "@material-ui/icons"
 
 import { getContainerInfoFromYaml } from "../../services/ComputeResourcesService"
 import DetailRow from "./DetailRow"
@@ -14,68 +14,58 @@ interface ContainerDetailProps {
 
 export default function ContainerDetails(props: ContainerDetailProps) {
   const containerInfo = getContainerInfoFromYaml(props.jobYaml)
-  const [visible, setVisible] = useState(false)
   return (
     <>
-      <h3 className="container-details-title">Container Details</h3>
-      <div className="container-details">
-        <List component={Paper}>
-          {containerInfo?.containerInfo &&
-            containerInfo?.containerInfo.map((container, index) => (
-              <Fragment key={container.name}>
-                <ListItem
-                  key={container.name + "-0"}
-                  button
-                  onClick={() => {
-                    setVisible(!visible)
-                  }}
-                >
-                  <ListItemText>{container.name}</ListItemText>
-                  <ExpandLess />
-                </ListItem>
-                <Collapse key={container.name + "-1"} in={visible} timeout="auto" unmountOnExit>
-                  <div className="nested-run">
-                    <TableContainer>
-                      <Table>
-                        <TableBody>
-                          {container.command && (
-                            <DetailRow key={"Command-" + index} name={"Command"} value={container.command} />
-                          )}
-                          {container.args && (
-                            <DetailRow key={"Argument-" + index} name={"Arguments"} value={container.args} />
-                          )}
-                          {container.resources.limits.cpu && (
-                            <DetailRow key={"CPU-" + index} name={"CPU"} value={container.resources.limits.cpu} />
-                          )}
-                          {container.resources.limits.memory && (
-                            <DetailRow
-                              key={"Memory-" + index}
-                              name={"Memory"}
-                              value={container.resources.limits.memory}
-                            />
-                          )}
-                          {container.resources.limits["nvidia.com/gpu"] && (
-                            <DetailRow
-                              key={"GPU-" + index}
-                              name={"GPU" + index}
-                              value={container.resources.limits["nvidia.com/gpu"]}
-                            />
-                          )}
-                          {container.resources.limits["ephemeral-storage"] && (
-                            <DetailRow
-                              key={"Ephermeral-Storage-" + index}
-                              name={"Ephermeral-Storage" + index}
-                              value={container.resources.limits["ephemeral-storage"]}
-                            />
-                          )}
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
-                  </div>
-                </Collapse>
-              </Fragment>
-            ))}
-        </List>
+      <div className="details-yaml-container">
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            <h3>Container Details</h3>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TableContainer>
+              <Table>
+                <TableBody>
+                  {containerInfo?.containerInfo &&
+                    containerInfo?.containerInfo.map((container, index) => (
+                      <Fragment key={(container.name + "-", index)}>
+                        {container.name && <DetailRow key={"Name-" + index} name={"Name"} value={container.name} />}
+                        {container.command && (
+                          <DetailRow key={"Command-" + index} name={"Command"} value={container.command} />
+                        )}
+                        {container.args && (
+                          <DetailRow key={"Argument-" + index} name={"Arguments"} value={container.args} />
+                        )}
+                        {container.resources.limits.cpu && (
+                          <DetailRow key={"CPU-" + index} name={"CPU"} value={container.resources.limits.cpu} />
+                        )}
+                        {container.resources.limits.memory && (
+                          <DetailRow
+                            key={"Memory-" + index}
+                            name={"Memory"}
+                            value={container.resources.limits.memory}
+                          />
+                        )}
+                        {container.resources.limits["nvidia.com/gpu"] && (
+                          <DetailRow
+                            key={"GPU-" + index}
+                            name={"GPU" + index}
+                            value={container.resources.limits["nvidia.com/gpu"]}
+                          />
+                        )}
+                        {container.resources.limits["ephemeral-storage"] && (
+                          <DetailRow
+                            key={"Ephermeral-Storage-" + index}
+                            name={"Ephermeral-Storage" + index}
+                            value={container.resources.limits["ephemeral-storage"]}
+                          />
+                        )}
+                      </Fragment>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </>
   )
