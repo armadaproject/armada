31c8c02814fccfb65a7aa3923e86ac65e405987b
Geoff Wilson <G.Wilson@F5.com>
2022-10-13 11:34:34 -0400

    ongoing work for localdev/e2e-test compatibility

--- localdev/scripts/executor-start.sh
+++ localdev/scripts/executor-start.sh
@@ -1,2 +1,1 @@
-./localdev/scripts/kube-access.sh
-ARMADA_APPLICATION_CLUSTERID=demo-a ARMADA_METRIC_PORT=9001 go run ./cmd/executor/main.go --config localdev/config/executor/config.yaml
+KUBECONFIG=./localdev/.kube/config ARMADA_APPLICATION_CLUSTERID=armada-test ARMADA_METRIC_PORT=9001 go run ./cmd/executor/main.go --config localdev/config/executor/config.yaml
