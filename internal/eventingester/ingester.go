fda2e7bfda2e483465121893c281c2218ac3b2c3
Chris Martin <council_tax@hotmail.com>
2022-10-17 20:58:11 +0100

    add list of fatal exception regexes (#1639)

--- internal/eventingester/ingester.go
+++ internal/eventingester/ingester.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"regexp"
 	"sync"
 
 	"github.com/apache/pulsar-client-go/pulsar"
@@ -27,6 +28,16 @@ func Run(config *configuration.EventIngesterConfiguration) {
 	ctx := ctxlogrus.ToContext(createContextWithShutdown(), log)
 
 	log.Info("Event Ingester Starting")
+
+	fatalRegexes := make([]*regexp.Regexp, len(config.FatalInsertionErrors))
+	for i, str := range config.FatalInsertionErrors {
+		rgx, err := regexp.Compile(str)
+		if err != nil {
+			log.Errorf("Error compiling regex %s", str)
+			panic(err)
+		}
+		fatalRegexes[i] = rgx
+	}
 
 	rc := redis.NewUniversalClient(&config.Redis)
 	defer func() {
@@ -78,7 +89,7 @@ func Run(config *configuration.EventIngesterConfiguration) {
 	// Insert into database
 	maxSize := 4 * 1024 * 1024
 	maxRows := 500
-	inserted := store.InsertEvents(ctx, eventDb, events, 5, maxSize, maxRows)
+	inserted := store.InsertEvents(ctx, eventDb, events, 5, maxSize, maxRows, fatalRegexes)
 
 	// Waitgroup that wil fire when the pipeline has been torn down
 	wg := &sync.WaitGroup{}
