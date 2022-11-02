6bb7c759654546da313e5b39f9b79d3dbb5cbb1e
Clif Houck <me@clifhouck.com>
2022-11-02 16:16:19 -0500

    Add lookout_url_template to ArmadaOperator (#1723)
    * Add lookout_url_template to ArmadaOperator
    
    Setting lookout_url_template properly causes ArmadaOperator to log the
    lookout URL of the job ID created upon execution.

---
+++ third_party/airflow/tests/unit/test_armada_operator.py
@@ -0,0 +1,33 @@
+from armada.operators.armada import ArmadaOperator
+import pytest
+
+get_lookout_url_test_cases = [
+    (
+        "http://localhost:8089/jobs?job_id=<job_id>",
+        "test_id",
+        "http://localhost:8089/jobs?job_id=test_id",
+    ),
+    (
+        "https://lookout.armada.domain/jobs?job_id=<job_id>",
+        "test_id",
+        "https://lookout.armada.domain/jobs?job_id=test_id",
+    ),
+    ("", "test_id", ""),
+]
+
+
+@pytest.mark.parametrize(
+    "lookout_url_template, job_id, expected_url", get_lookout_url_test_cases
+)
+def test_get_lookout_url(lookout_url_template, job_id, expected_url):
+    operator = ArmadaOperator(
+        task_id="test_task_id",
+        name="test_task",
+        armada_client=None,
+        job_service_client=None,
+        armada_queue="test_queue",
+        job_request_items=[],
+        lookout_url_template=lookout_url_template,
+    )
+
+    assert operator._get_lookout_url(job_id) == expected_url
