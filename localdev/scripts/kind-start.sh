d9a7a549569566cbc54db28c401d2b81d45d032a
Sharpz7 <adam.mcarthur62@gmail.com>
2022-11-10 15:09:06 +0000

    Added extra images and kind export

--- localdev/scripts/kind-start.sh
+++ localdev/scripts/kind-start.sh
@@ -1,20 +1,36 @@
 go install sigs.k8s.io/kind@v0.16.0
 
 kind create cluster --config ../e2e/setup/kind.yaml
+
+# Pull all images
+docker pull "alpine:3.10"
+docker pull "nginx:1.21.6"
+docker pull "registry.k8s.io/ingress-nginx/controller:v1.4.0"
+docker pull "registry.k8s.io/ingress-nginx/kube-webhook-certgen:v20220916-gd32f8c343"
+
+# Load images into kind
+kind load docker-image "alpine:3.10" --name armada-test
+kind load docker-image "nginx:1.21.6" --name armada-test
+kind load docker-image "registry.k8s.io/ingress-nginx/controller:v1.4.0" --name armada-test
+kind load docker-image "registry.k8s.io/ingress-nginx/kube-webhook-certgen:v20220916-gd32f8c343" --name armada-test
+
+# Ensures that kubectl can access the cluster
+kind export kubeconfig --internal --name armada-test
+
 # We need an ingress controller to enable cluster ingress
 kubectl apply -f ../e2e/setup/ingress-nginx.yaml --context kind-armada-test
+
 # Wait until the ingress controller is ready
 echo "Waiting for ingress controller to become ready"
+
 sleep 60 # calling wait immediately can result in "no matching resources found"
 kubectl wait --namespace ingress-nginx \
 	--for=condition=ready pod \
 	--selector=app.kubernetes.io/component=controller \
 	--timeout=90s \
 	--context kind-armada-test
-docker pull "alpine:3.10" # ensure alpine, which is used by tests, is available
-docker pull "nginx:1.21.6" # ensure nginx, which is used by tests, is available
-kind load docker-image "alpine:3.10" --name armada-test # needed to make alpine available to kind
-kind load docker-image "nginx:1.21.6" --name armada-test # needed to make nginx available to kind
+
+# Store the kubeconfig in the local directory for containers to access
 mkdir -p .kube
 kind get kubeconfig --internal --name armada-test > ./.kube/config
 
