80d87905442959e3eeaf5c9dccc490a77d343179
Chris Martin <chris@cmartinit.co.uk>
2022-11-12 14:05:48 +0000

    fix flaky test

--- internal/common/ingest/batch_test.go
+++ internal/common/ingest/batch_test.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	defaultMaxItems   = 3
-	defaultMaxTimeOut = 1 * time.Second
+	defaultMaxTimeOut = 5 * time.Second
 )
 
 func TestBatch_MaxItems(t *testing.T) {
@@ -35,29 +35,34 @@ func TestBatch_MaxItems(t *testing.T) {
 	inputChan <- 4
 	inputChan <- 5
 	inputChan <- 6
-	time.Sleep(1 * time.Second)
+	time.Sleep(500 * time.Millisecond)
 	assert.Equal(t, [][]int{{1, 2, 3}, {4, 5, 6}}, output)
 	cancel()
 }
 
 func TestBatch_Time(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	testClock := clock.NewFakeClock(time.Now())
-	inputChan := make(chan int)
-	output := make([][]int, 0)
-	batcher := NewBatcher[int](inputChan, defaultMaxItems, defaultMaxTimeOut, func(a []int) { output = append(output, a) })
-	batcher.clock = testClock
 
-	go func() {
-		batcher.Run(ctx)
-	}()
-	inputChan <- 1
-	inputChan <- 2
-	testClock.Step(2 * time.Second)
-	inputChan <- 3
-	inputChan <- 4
-	testClock.Step(2 * time.Second)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, [][]int{{1, 2}, {3, 4}}, output)
-	cancel()
+	for i := 0; i < 100; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		testClock := clock.NewFakeClock(time.Now())
+		inputChan := make(chan int)
+		output := make([][]int, 0)
+		batcher := NewBatcher[int](inputChan, defaultMaxItems, defaultMaxTimeOut, func(a []int) { output = append(output, a) })
+		batcher.clock = testClock
+
+		go func() {
+			batcher.Run(ctx)
+		}()
+		inputChan <- 1
+		inputChan <- 2
+		time.Sleep(100 * time.Millisecond)
+		testClock.Step(5 * time.Second)
+		inputChan <- 3
+		inputChan <- 4
+		time.Sleep(100 * time.Millisecond)
+		testClock.Step(5 * time.Second)
+		time.Sleep(500 * time.Millisecond)
+		assert.Equal(t, [][]int{{1, 2}, {3, 4}}, output)
+		cancel()
+	}
 }
