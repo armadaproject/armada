3ab50b10dcf7482856962329c7131f91750d1774
Albin Severinson <albin@severinson.org>
2022-12-01 15:40:50 +0000

    add golangci-lint to tools

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -5,6 +5,7 @@ package tools
 
 import (
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/gordonklaus/ineffassign"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
