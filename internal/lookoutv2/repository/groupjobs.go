444456e6b0b279e27dc05eadb553b35ecf243e49
carlocamurri <carlo.camurri98@gmail.com>
2022-12-06 16:53:43 +0000

    fmt

--- internal/lookoutv2/repository/groupjobs.go
+++ internal/lookoutv2/repository/groupjobs.go
@@ -3,12 +3,13 @@ package repository
 import (
 	"context"
 
-	"github.com/G-Research/armada/internal/common/database"
-	"github.com/G-Research/armada/internal/common/database/lookout"
-	"github.com/G-Research/armada/internal/lookoutv2/model"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
+
+	"github.com/G-Research/armada/internal/common/database"
+	"github.com/G-Research/armada/internal/common/database/lookout"
+	"github.com/G-Research/armada/internal/lookoutv2/model"
 )
 
 type GroupByResult struct {
