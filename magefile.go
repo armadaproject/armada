aaee07a77a9a83bc63c8e99779750bfb344f80f3
Albin Severinson <albin@severinson.org>
2022-12-08 08:57:16 +0000

    Add magefile target to create minimal goreleaser config

--- magefile.go
+++ magefile.go
@@ -16,13 +16,96 @@ import (
 	"runtime"
 	"strings"
 
+	goreleaserConfig "github.com/goreleaser/goreleaser/pkg/config"
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 	"github.com/pkg/errors"
+	"golang.org/x/exp/maps"
+	"sigs.k8s.io/yaml"
+
+	"github.com/G-Research/armada/pkg/client/util"
 )
 
 const PROTOC_VERSION_MIN = "3.21.8"
 const PROTOC_VERSION_DOWNLOAD = "21.8" // The "3." is omitted.
+
+// Write a minimal goreleaser config to .goreleaser-docker.yml
+// containing only the subset of targets in .goreleaser.yaml necessary
+// for building a set of specified Docker images.
+func DockerGoreleaserConfig() error {
+	// Docker targets to build and the build targets necessary to do so.
+	dockerIds := map[string]bool{
+		"bundle": true,
+	}
+	buildIds := map[string]bool{
+		"server":          true,
+		"executor":        true,
+		"binoculars":      true,
+		"lookoutingester": true,
+		"eventingester":   true,
+	}
+
+	goreleaserConfigPath := "./.goreleaser.yml"
+	config := goreleaserConfig.Project{}
+	if err := util.BindJsonOrYaml(goreleaserConfigPath, &config); err != nil {
+		return err
+	}
+
+	dockers := make([]goreleaserConfig.Docker, 0)
+	for _, docker := range config.Dockers {
+		if dockerIds[docker.ID] {
+			dockers = append(dockers, docker)
+		}
+	}
+	if len(dockers) == 0 {
+		return errors.Errorf("%v matched no dockers in %s", dockerIds, goreleaserConfigPath)
+	}
+
+	builds := make([]goreleaserConfig.Build, 0)
+	for _, build := range config.Builds {
+		if buildIds[build.ID] {
+			builds = append(builds, build)
+		}
+	}
+	if len(builds) == 0 {
+		return errors.Errorf("%v matched no builds in %s", buildIds, goreleaserConfigPath)
+	}
+
+	targets := make(map[string]interface{})
+	for _, docker := range dockers {
+		targets[fmt.Sprintf("%s_%s", docker.Goos, docker.Goarch)] = true
+	}
+	for i := range builds {
+		builds[i].Goos = nil
+		builds[i].Goarch = nil
+		builds[i].Targets = maps.Keys(targets)
+	}
+
+	minimalConfig := goreleaserConfig.Project{
+		ProjectName: config.ProjectName,
+		Dist:        config.Dist,
+		GoMod:       config.GoMod,
+		Env:         config.Env,
+		Builds:      builds,
+		Dockers:     dockers,
+	}
+	bytes, err := yaml.Marshal(minimalConfig)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(".goreleaser-docker.yml")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if n, err := f.Write(bytes); err != nil {
+		return err
+	} else if n != len(bytes) {
+		return errors.Errorf("only $d out of $d bytes were written", n, len(bytes))
+	}
+
+	return nil
+}
 
 func Kind() error {
 	mg.Deps(KindSetup)
