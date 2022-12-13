0e43e08dbf56cb84661cbb60bcf9ec6406626b57
Kevin Hannon <kannon1992@gmail.com>
2022-12-13 12:42:37 -0500

    exclude list for coverage

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-$GO_TEST_CMD go test -coverprofile internal_coverage.xml -v ./internal... 2>&1 | tee test_reports/internal.txt
+INTERNAL_COVERAGE_LIST=$($GO_TEST_CMD go list ./internal... | grep -v /testsuite | grep -v /statik | grep -v /metrics | grep -v model | grep -v gen)
+$GO_TEST_CMD go test -coverprofile internal_coverage.xml $INTERNAL_COVERAGE_LIST 2>&1 | tee test_reports/internal.txt
 $GO_TEST_CMD go test -coverprofile pkg_coverage.xml -v ./pkg... 2>&1 | tee test_reports/pkg.txt
 $GO_TEST_CMD go test -coverprofile cmd_coverage.xml -v ./cmd... 2>&1 | tee test_reports/cmd.txt
