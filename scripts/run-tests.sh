1d16b56807386410b8d29bddeed07e9ca2fd45a8
Kevin Hannon <kannon1992@gmail.com>
2022-12-13 15:54:22 -0500

    scheduler-objects remove from coverage report

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-INTERNAL_COVERAGE_LIST=$($GO_TEST_CMD go list ./internal... | grep -v /testsuite | grep -v /statik | grep -v /metrics | grep -v /model | grep -v /gen)
+$GO_TEST_CMD go test internal_coverage.xml ./internal... 2>&1 | tee test_reports/internal.txt
+$GO_TEST_CMD go test pkg_coverage.xml -v ./pkg... 2>&1 | tee test_reports/pkg.txt
+$GO_TEST_CMD go test cmd_coverage.xml -v ./cmd... 2>&1 | tee test_reports/cmd.txt
+
+# Run Coverage separately and exclude auto generated code from modules
+INTERNAL_COVERAGE_LIST=$($GO_TEST_CMD go list ./internal... | grep -v /testsuite | grep -v /statik | grep -v /metrics | grep -v /model | grep -v /gen | grep -v /schedulerobjects)
 $GO_TEST_CMD go test -coverprofile internal_coverage.xml $INTERNAL_COVERAGE_LIST 2>&1 | tee test_reports/internal.txt
-$GO_TEST_CMD go test -coverprofile pkg_coverage.xml -v ./pkg... 2>&1 | tee test_reports/pkg.txt
 $GO_TEST_CMD go test -coverprofile cmd_coverage.xml -v ./cmd... 2>&1 | tee test_reports/cmd.txt
