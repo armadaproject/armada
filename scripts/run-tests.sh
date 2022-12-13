cf8bf2bfe491cebf0d808b482fd9181cdc991c3e
Kevin Hannon <kannon1992@gmail.com>
2022-12-13 15:57:12 -0500

    coverage remove

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$GO_TEST_CMD go test internal_coverage.xml ./internal... 2>&1 | tee test_reports/internal.txt
-$GO_TEST_CMD go test pkg_coverage.xml -v ./pkg... 2>&1 | tee test_reports/pkg.txt
-$GO_TEST_CMD go test cmd_coverage.xml -v ./cmd... 2>&1 | tee test_reports/cmd.txt
+$GO_TEST_CMD go test -v ./internal... 2>&1 | tee test_reports/internal.txt
+$GO_TEST_CMD go test -v ./pkg... 2>&1 | tee test_reports/pkg.txt
+$GO_TEST_CMD go test -v ./cmd... 2>&1 | tee test_reports/cmd.txt
 
 # Run Coverage separately and exclude auto generated code from modules
 INTERNAL_COVERAGE_LIST=$($GO_TEST_CMD go list ./internal... | grep -v /testsuite | grep -v /statik | grep -v /metrics | grep -v /model | grep -v /gen | grep -v /schedulerobjects)
