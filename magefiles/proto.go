99df46715fd52a84426ec69b24456368182bfe7c
Sam Clark <12830+samclark@users.noreply.github.com>
2022-12-23 23:52:11 +0000

    updates

--- magefiles/proto.go
+++ magefiles/proto.go
@@ -18,29 +18,32 @@ func protoInstallProtocArmadaPlugin() error {
 
 func protoPrepareThirdPartyProtos() error {
 	// Go modules containing .proto dependencies we need.
-	modules := []string{
-		"github.com/gogo/protobuf",
-		"github.com/grpc-ecosystem/grpc-gateway",
-		"k8s.io/api",
-		"k8s.io/apimachinery",
+	modules := []struct {
+		name  string
+		roots []string
+	}{
+		{
+			name: "github.com/gogo/protobuf",
+			roots: []string{
+				filepath.FromSlash("github.com/gogo/protobuf/protobuf"),
+			},
+		},
+		{
+			name: "github.com/grpc-ecosystem/grpc-gateway",
+			roots: []string{
+				filepath.FromSlash("github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis"),
+			},
+		},
+		{
+			name:  "k8s.io/api",
+			roots: []string{},
+		},
+		{
+			name:  "k8s.io/apimachinery",
+			roots: []string{},
+		},
 	}
-	redirects := map[string]string{
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/any.proto"):                                filepath.Join("./google/protobuf/any.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/api.proto"):                                filepath.Join("./google/protobuf/api.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/compiler/plugin.proto"):                    filepath.Join("./google/protobuf/compiler/plugin.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/descriptor.proto"):                         filepath.Join("./google/protobuf/descriptor.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/duration.proto"):                           filepath.Join("./google/protobuf/duration.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/empty.proto"):                              filepath.Join("./google/protobuf/empty.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/field_mask.proto"):                         filepath.Join("./google/protobuf/field_mask.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/source_context.proto"):                     filepath.Join("./google/protobuf/source_context.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/struct.proto"):                             filepath.Join("./google/protobuf/struct.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/timestamp.proto"):                          filepath.Join("./google/protobuf/timestamp.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/type.proto"):                               filepath.Join("./google/protobuf/type.proto"),
-		filepath.Join("./github.com/gogo/protobuf/protobuf/google/protobuf/wrappers.proto"):                           filepath.Join("./google/protobuf/wrappers.proto"),
-		filepath.Join("./github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api/annotations.proto"): filepath.Join("./google/api/annotations.proto"),
-		filepath.Join("./github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api/http.proto"):        filepath.Join("./google/api/http.proto"),
-		filepath.Join("./github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api/httpbody.proto"):    filepath.Join("./google/api/httpbody.proto"),
-	}
+
 	goPath, err := goEnv("GOPATH")
 	if err != nil {
 		return errors.Errorf("error getting GOPATH: %v", err)
@@ -50,25 +53,26 @@ func protoPrepareThirdPartyProtos() error {
 	}
 	goModPath := filepath.Join(goPath, "pkg", "mod")
 	for _, module := range modules {
-		version, err := goModuleVersion(module)
+		version, err := goModuleVersion(module.name)
 		if err != nil {
 			return err
 		}
-		modulePath := filepath.Join(strings.Split(module, "/")...)
-		modulePathWithVersion := fmt.Sprintf("%s@%s", modulePath, version)
-		err = filepath.WalkDir(filepath.Join(goModPath, modulePathWithVersion), func(path string, d fs.DirEntry, err error) error {
+		relModPath := filepath.FromSlash(module.name)
+		relModPathWithVersion := relModPath + "@" + version
+		err = filepath.WalkDir(filepath.Join(goModPath, relModPathWithVersion), func(path string, d fs.DirEntry, err error) error {
 			if (d != nil && d.IsDir()) || filepath.Ext(path) != ".proto" {
 				return nil
 			}
-			dest, err := filepath.Rel(goModPath, path)
-			if err != nil {
-				return err
+			// get relative path
+			dest := strings.ReplaceAll(path, goModPath, ".")
+			// replace versioned path with clean path
+			dest = strings.ReplaceAll(dest, relModPathWithVersion, relModPath)
+			// root sub-directories (if applicable)
+			for _, root := range module.roots {
+				dest = strings.ReplaceAll(dest, root, ".")
 			}
-			dest = strings.ReplaceAll(dest, modulePathWithVersion, modulePath)
-			if redirect, ok := redirects[dest]; ok {
-				dest = redirect
-			}
-			dest = filepath.Join("./proto", dest)
+			// copy to proto folder
+			dest = filepath.Join("proto", dest)
 			return copy(path, dest)
 		})
 		if err != nil {
@@ -158,7 +162,7 @@ func protoProtocRun(armada, grpcGateway bool, swaggerFileName string, paths ...s
 	}
 
 	if grpcGateway {
-		args = append(args, "--grpc-gateway_out=logtostderr=true,paths=source_relative:.")
+		args = append(args, fmt.Sprintf("--grpc-gateway_out=logtostderr=true,paths=source_relative,%s:.", modules))
 	}
 
 	if swaggerFileName != "" {
