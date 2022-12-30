d7a2140f40f918c3ca85851690e7595d0bd569b8
Chris Martin <chris@cmartinit.co.uk>
2022-12-30 17:10:15 +0000

    fixed submit test

--- internal/armada/repository/event_store.go
+++ internal/armada/repository/event_store.go
@@ -5,3 +5,12 @@ import "github.com/G-Research/armada/pkg/api"
 type EventStore interface {
 	ReportEvents(message []*api.EventMessage) error
 }
+
+type TestEventStore struct {
+	ReceivedEvents []*api.EventMessage
+}
+
+func (es *TestEventStore) ReportEvents(message []*api.EventMessage) error {
+	es.ReceivedEvents = append(es.ReceivedEvents, message...)
+	return nil
+}
