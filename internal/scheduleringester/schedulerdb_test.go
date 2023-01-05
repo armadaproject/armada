374cc05b7e4fe1709aa33d7336fa03630fe11a4d
Chris Martin <chris@cmartinit.co.uk>
2023-01-05 07:10:14 +0000

    merge master

--- internal/scheduleringester/schedulerdb_test.go
+++ internal/scheduleringester/schedulerdb_test.go
@@ -266,7 +266,7 @@ func assertOpSuccess(t *testing.T, schedulerDb *SchedulerDb, serials map[string]
 				v.LastModified = job.LastModified
 			}
 		}
-		//assert.Equal(t, expected, actual)
+		// assert.Equal(t, expected, actual)
 		for k, v := range expected {
 			assert.Equal(t, v, actual[k])
 		}
