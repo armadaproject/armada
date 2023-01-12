3e96266b764b84d1e796143d030ff75772e9c948
carlocamurri <carlo.camurri98@gmail.com>
2023-01-12 14:47:38 +0000

    Lookout V2 use optimized batch cancellation endpoint

--- magefiles/kind.go
+++ magefiles/kind.go
@@ -81,7 +81,7 @@ func kindSetup() error {
 	images := []string{
 		"alpine:3.10",
 		"nginx:1.21.6",
-		"bitnami/kubectl:1.24.8",
+		"rancher/kubectl:v1.23.7",
 		"registry.k8s.io/ingress-nginx/controller:v1.4.0",
 		"registry.k8s.io/ingress-nginx/kube-webhook-certgen:v20220916-gd32f8c343",
 	}
