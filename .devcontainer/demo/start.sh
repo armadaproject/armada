cd5b496b3069ac89c479971ec6c4adfed234637a
Sharpz7 <adam.mcarthur62@gmail.com>
2023-02-08 12:38:58 +0000

    Added script

---
+++ .devcontainer/demo/start.sh
@@ -0,0 +1,10 @@
+cd ../../internal/lookout/ui
+yarn install
+yarn run openapi
+yarn run build
+cd localdev
+./scripts/kind-start.sh
+cd ..
+./localdev/run.sh
+cd localdev
+docker-compose logs -f --tail=10
