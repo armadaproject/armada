79c47091027ad62caada05b60d03ef549eaff771
mohamed <mohamedabdelfatah2027@gmail.com>
2023-03-02 20:52:14 +0200

    refactoring

--- internal/armada/metrics/domain_test.go
+++ internal/armada/metrics/domain_test.go
@@ -11,45 +11,30 @@ import (
 
 func TestRecordJobRuntime_ShouldReportRuntimeDurations(t *testing.T) {
 	jobMetricsRecorder := NewJobMetricsRecorder()
-	jobMetricsRecorder.RecordJobRuntime("cpu", "", time.Second)
-	jobMetricsRecorder.RecordJobRuntime("cpu", "", 2*time.Second)
-	jobMetricsRecorder.RecordJobRuntime("cpu", "", 3*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-1", "", time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-1", "", 2*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-1", "", 3*time.Second)
 
-	jobMetricsRecorder.RecordJobRuntime("gpu", "", 2*time.Second)
-	jobMetricsRecorder.RecordJobRuntime("gpu", "", 3*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-2", "", 2*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-2", "", 3*time.Second)
 
-	jobMetricsRecorder.RecordJobRuntime("memory", "", 6*time.Second)
-	jobMetricsRecorder.RecordJobRuntime("memory", "", 8*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-3", "", 6*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-3", "", 8*time.Second)
 
-	jobMetricsRecorder.RecordJobRuntime("disk", "", 8*time.Second)
-	jobMetricsRecorder.RecordJobRuntime("disk", "", 10*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-4", "", 8*time.Second)
+	jobMetricsRecorder.RecordJobRuntime("pool-4", "", 10*time.Second)
+	expected := map[string][]float64{
+		"pool-1": {1, 3, 6},
+		"pool-2": {2, 3, 5},
+		"pool-3": {6, 8, 14},
+		"pool-4": {8, 10, 18},
+	}
 	for _, queueMetric := range jobMetricsRecorder.Metrics() {
-		switch queueMetric.Pool {
-		case "cpu":
-			assert.Equal(t, float64(1), queueMetric.Durations.GetMin())
-			assert.Equal(t, float64(3), queueMetric.Durations.GetMax())
-			assert.Equal(t, float64(6), queueMetric.Durations.GetSum())
-			assert.Equal(t, uint64(3), queueMetric.Durations.GetCount())
-		case "gpu":
-			assert.Equal(t, float64(2), queueMetric.Durations.GetMin())
-			assert.Equal(t, float64(3), queueMetric.Durations.GetMax())
-			assert.Equal(t, float64(5), queueMetric.Durations.GetSum())
-			assert.Equal(t, uint64(2), queueMetric.Durations.GetCount())
-		case "memory":
-			assert.Equal(t, float64(6), queueMetric.Durations.GetMin())
-			assert.Equal(t, float64(8), queueMetric.Durations.GetMax())
-			assert.Equal(t, float64(14), queueMetric.Durations.GetSum())
-			assert.Equal(t, uint64(2), queueMetric.Durations.GetCount())
-		case "disk":
-			assert.Equal(t, float64(8), queueMetric.Durations.GetMin())
-			assert.Equal(t, float64(10), queueMetric.Durations.GetMax())
-			assert.Equal(t, float64(18), queueMetric.Durations.GetSum())
-			assert.Equal(t, uint64(2), queueMetric.Durations.GetCount())
-		}
+		assert.Equal(t, expected[queueMetric.Pool], []float64{queueMetric.Durations.GetMin(), queueMetric.Durations.GetMax(), queueMetric.Durations.GetSum()})
 	}
 }
 
-func TestRecordResources_ShouldReportRuntimResources(t *testing.T) {
+func TestRecordResources_ShouldReportRuntimeResources(t *testing.T) {
 	crf := armadaresource.ComputeResourcesFloat{
 		"cpu":    float64(2),
 		"memory": float64(3),
@@ -74,22 +59,14 @@ func TestRecordResources_ShouldReportRuntimResources(t *testing.T) {
 	jobMetricsRecorder.RecordResources("cpu", "", crf)
 	jobMetricsRecorder.RecordResources("cpu", "", crf2)
 	jobMetricsRecorder.RecordResources("cpu", "", crf3)
-
+	expected := map[string][]float64{
+		"cpu":    {8, 2, 5, 15},
+		"gpu":    {5, 2, 3, 10},
+		"memory": {9, 3, 7, 19},
+		"disk":   {9, 5, 7, 14},
+	}
 	for _, queueMetric := range jobMetricsRecorder.Metrics() {
-		assert.Equal(t, float64(8), queueMetric.Resources["cpu"].GetMax())
-		assert.Equal(t, float64(2), queueMetric.Resources["cpu"].GetMin())
-		assert.Equal(t, float64(5), queueMetric.Resources["cpu"].GetMedian())
-
-		assert.Equal(t, float64(5), queueMetric.Resources["gpu"].GetMax())
-		assert.Equal(t, float64(2), queueMetric.Resources["gpu"].GetMin())
-		assert.Equal(t, float64(3), queueMetric.Resources["gpu"].GetMedian())
-
-		assert.Equal(t, float64(9), queueMetric.Resources["memory"].GetMax())
-		assert.Equal(t, float64(3), queueMetric.Resources["memory"].GetMin())
-		assert.Equal(t, float64(7), queueMetric.Resources["memory"].GetMedian())
-
-		assert.Equal(t, float64(9), queueMetric.Resources["disk"].GetMax())
-		assert.Equal(t, float64(5), queueMetric.Resources["disk"].GetMin())
-		assert.Equal(t, float64(7), queueMetric.Resources["disk"].GetMedian())
+		assert.Equal(t, expected[queueMetric.Pool], []float64{queueMetric.Resources[queueMetric.Pool].GetMax(), queueMetric.Resources[queueMetric.Pool].GetMin(),
+			queueMetric.Resources[queueMetric.Pool].GetMedian(), queueMetric.Resources[queueMetric.Pool].GetSum()})
 	}
 }
