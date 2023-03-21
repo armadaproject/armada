5a3f0c207a7636fcc5a915787aadc3b5d70db477
Albin Severinson <albin@severinson.org>
2023-03-21 16:19:38 +0000

    Add GetSchedulingReport to armadactl

--- internal/armadactl/scheduling.go
+++ internal/armadactl/scheduling.go
@@ -3,10 +3,25 @@ package armadactl
 import (
 	"fmt"
 
+	"github.com/gogo/protobuf/types"
+
 	"github.com/armadaproject/armada/internal/common"
 	"github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
 	"github.com/armadaproject/armada/pkg/client"
 )
+
+func (a *App) GetSchedulingReport(jobId string) error {
+	return client.WithSchedulerReportingClient(a.Params.ApiConnectionDetails, func(c schedulerobjects.SchedulerReportingClient) error {
+		ctx, cancel := common.ContextWithDefaultTimeout()
+		defer cancel()
+		report, err := c.GetSchedulingReport(ctx, &types.Empty{})
+		if err != nil {
+			return err
+		}
+		fmt.Fprint(a.Out, report.Report)
+		return nil
+	})
+}
 
 func (a *App) GetQueueSchedulingReport(queue string) error {
 	return client.WithSchedulerReportingClient(a.Params.ApiConnectionDetails, func(c schedulerobjects.SchedulerReportingClient) error {
