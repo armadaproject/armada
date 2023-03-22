811d95a2f136eaa78d117884159a864b9f17ccfb
Chris Martin <chris@cmartinit.co.uk>
2023-03-22 14:05:27 +0000

    linting

--- internal/scheduler/legacyscheduler.go
+++ internal/scheduler/legacyscheduler.go
@@ -58,7 +58,7 @@ type SchedulingConstraints struct {
 	// Per-queue resource limits.
 	// Map from resource type to the limit for that resource.
 	MaximalResourceFractionPerQueue map[string]float64
-	// Limit- as a fraction of total resources across worker clusters- of resource types at each priority.
+	// Limit, as a fraction of total resources across worker clusters, of resource types at each priority.
 	// The limits are cumulative, i.e., the limit at priority p includes all higher levels.
 	MaximalCumulativeResourceFractionPerQueueAndPriority map[int32]map[string]float64
 	// Max resources to schedule per queue at a time.
@@ -141,7 +141,7 @@ func PreemptedJobsFromSchedulerResult[T LegacySchedulerJob](sr *SchedulerResult)
 	return rv
 }
 
-// ScheduledJobsFromScheduleResult returns the slice of scheduled jobs in the result,
+// ScheduledJobsFromSchedulerResult returns the slice of scheduled jobs in the result,
 // cast to type T.
 func ScheduledJobsFromSchedulerResult[T LegacySchedulerJob](sr *SchedulerResult) []T {
 	rv := make([]T, len(sr.ScheduledJobs))
@@ -683,7 +683,7 @@ func (sch *Rescheduler) updateGangAccounting(preemptedJobs, scheduledJobs []Lega
 // Compare the NodeJobDiff with expected preempted/scheduled jobs to ensure NodeDb is consistent.
 // This is only to validate that nothing unexpected happened during scheduling.
 func (sch *Rescheduler) reschedulerAssertions(
-	ctx context.Context,
+	_ context.Context,
 	snapshot *memdb.Txn,
 	preemptedJobsById,
 	scheduledJobsById map[string]LegacySchedulerJob,
@@ -1093,7 +1093,7 @@ func NewLegacyScheduler(
 		)
 	}
 	if ResourceListAsWeightedApproximateFloat64(constraints.ResourceScarcity, nodeDb.totalResources) == 0 {
-		// This refers to the resources currently considered for schedling.
+		// This refers to the resources currently considered for scheduling.
 		return nil, errors.Errorf(
 			"no resources with non-zero weight available for scheduling in NodeDb: resource scarcity %v, total resources %v",
 			constraints.ResourceScarcity, nodeDb.totalResources,
@@ -1884,6 +1884,21 @@ func isEvictedJob(job LegacySchedulerJob) bool {
 func targetNodeIdFromLegacySchedulerJob(job LegacySchedulerJob) (string, bool) {
 	nodeId, ok := job.GetAnnotations()[TargetNodeIdAnnotation]
 	return nodeId, ok
+}
+
+func GangIdAndCardinalityFromLegacySchedulerJob(job LegacySchedulerJob, priorityClasses map[string]configuration.PriorityClass) (string, int, bool, error) {
+	reqs := job.GetRequirements(priorityClasses)
+	if reqs == nil {
+		return "", 0, false, nil
+	}
+	if len(reqs.ObjectRequirements) != 1 {
+		return "", 0, false, errors.Errorf("expected exactly one object requirement in %v", reqs)
+	}
+	podReqs := reqs.ObjectRequirements[0].GetPodRequirements()
+	if podReqs == nil {
+		return "", 0, false, nil
+	}
+	return GangIdAndCardinalityFromAnnotations(podReqs.Annotations)
 }
 
 func GangIdAndCardinalityFromAnnotations(annotations map[string]string) (string, int, bool, error) {
