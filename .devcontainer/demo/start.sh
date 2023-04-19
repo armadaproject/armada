5fba44e423d54f43f95ee21e9105301c07b68f58
Sharpz7 <adam.mcarthur62@gmail.com>
2023-04-19 14:51:59 +0000

    Bugfixes

--- .devcontainer/demo/start.sh
+++ .devcontainer/demo/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-../../docs/local/armadactl.sh
+sh ../../docs/local/armadactl.sh
 
 cd ../../
 
 export ARMADA_IMAGE=armada-full-bundle-dev
 export ARMADA_IMAGE_TAG=a08830b4911c6784d67d1227367f8505243fd167
+
+# Install mage
+go install github.com/magefile/mage@latest
 
 # Run the demo
 mage localdev
