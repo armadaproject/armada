0fd303cde511dcdb37925409cfad7023ce2931e0
Geoff Wilson <geoff@gr-oss.io>
2023-05-01 13:42:55 -0400

    Test for event stream subscription with concurrency

--- internal/jobservice/eventstojobs/eventstojobs_test.go
+++ internal/jobservice/eventstojobs/eventstojobs_test.go
@@ -3,7 +3,9 @@ package eventstojobs_test
 import (
 	"context"
 	"errors"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -21,6 +23,7 @@ func Test_SubscribeToJobSetId(t *testing.T) {
 		ttlSecs              int64
 		wantErr              bool
 		wantSubscriptionErr  bool
+		highConcurrency      bool
 	}{
 		{
 			name:    "it exits with error after expiration even if messages are received",
@@ -56,6 +59,19 @@ func Test_SubscribeToJobSetId(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name:    "it behaves well with high latency and concurrency",
+			ttlSecs: int64(1),
+			jobEventMessageFn: func(context.Context, *api.JobSetRequest) (*api.EventStreamMessage, error) {
+				time.Sleep(10 * time.Millisecond)
+				return &api.EventStreamMessage{Message: &api.EventMessage{}}, nil
+			},
+			isJobSetSubscribedFn: func(context.Context, string, string) (bool, string, error) {
+				return false, "", nil
+			},
+			wantErr: false,
+			highConcurrency: true,
+		},
 	}
 
 	for _, tt := range tests {
@@ -73,25 +89,37 @@ func Test_SubscribeToJobSetId(t *testing.T) {
 				UnsubscribeJobSetFunc:                     func(context.Context, string, string) (int64, error) { return 0, nil },
 			}
 
-			service := eventstojobs.NewEventsToJobService(
-				"somestring",
-				"someJobSetId",
-				&mockJobEventReader,
-				&mockJobRepo,
-			)
-			result := service.SubscribeToJobSetId(context.Background(), tt.ttlSecs, "")
-			if tt.wantErr {
-				assert.Error(t, result)
-			} else {
-				assert.Nil(t, result)
+			concurrency := 1
+			if tt.highConcurrency {
+				concurrency = 50
 			}
-			if tt.wantSubscriptionErr {
-				assert.True(t, len(mockJobRepo.SetSubscriptionErrorCalls()) > 0)
-				assert.Equal(t, 0, len(mockJobRepo.AddMessageIdAndClearSubscriptionErrorCalls()))
-			} else {
-				assert.Equal(t, 0, len(mockJobRepo.SetSubscriptionErrorCalls()))
-				assert.True(t, len(mockJobRepo.AddMessageIdAndClearSubscriptionErrorCalls()) > 0)
+			wg := sync.WaitGroup{}
+			for i :=0; i < concurrency; i++ {
+				go func() {
+					wg.Add(1)
+					defer wg.Done()
+					service := eventstojobs.NewEventsToJobService(
+						"somestring",
+						"someJobSetId",
+						&mockJobEventReader,
+						&mockJobRepo,
+					)
+					result := service.SubscribeToJobSetId(context.Background(), tt.ttlSecs, "")
+					if tt.wantErr {
+						assert.Error(t, result)
+					} else {
+						assert.Nil(t, result)
+					}
+					if tt.wantSubscriptionErr {
+						assert.True(t, len(mockJobRepo.SetSubscriptionErrorCalls()) > 0)
+						assert.Equal(t, 0, len(mockJobRepo.AddMessageIdAndClearSubscriptionErrorCalls()))
+					} else {
+						assert.Equal(t, 0, len(mockJobRepo.SetSubscriptionErrorCalls()))
+						assert.True(t, len(mockJobRepo.AddMessageIdAndClearSubscriptionErrorCalls()) > 0)
+					}
+				}()
 			}
+			wg.Wait()
 		})
 	}
 }
