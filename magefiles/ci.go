bf81907337db2309b0c90547f47980bd0ad09d53
Kevin Hannon <kannon1992@gmail.com>
2023-05-08 11:56:36 -0400

    add back ci cluster for python integration tests (#2437)

--- magefiles/ci.go
+++ magefiles/ci.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -34,6 +35,52 @@ func TestSuite() error {
 	}
 
 	fmt.Println(out)
+	return nil
+}
+
+func ciSetup() error {
+	if err := os.MkdirAll(".kube", os.ModeDir|0o755); err != nil {
+		return err
+	}
+	err := dockerComposeRun("up", "-d", "redis", "postgres", "pulsar")
+	if err != nil {
+		return err
+	}
+
+	mg.Deps(CheckForPulsarRunning)
+
+	// By starting the executor first,
+	// we can ensure that the server will be able to register the executor cluster
+	// on its first attempt.
+	err = dockerComposeRun("up", "-d", "executor")
+	if err != nil {
+		return err
+	}
+	err = dockerComposeRun("up", "-d", "server")
+	if err != nil {
+		return err
+	}
+
+	err = goRun("run", "cmd/armadactl/main.go", "create", "queue", "e2e-test-queue")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Build images, spin up a test environment, and run the integration tests against it.
+func ciRunTests() error {
+	mg.Deps(CheckForArmadaRunning)
+
+	out, err := goOutput("run", "cmd/testsuite/main.go", "test",
+		"--tests", "testsuite/testcases/basic/*",
+		"--junit", "junit.xml",
+	)
+	fmt.Println(out)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
