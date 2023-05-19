f2865acc83e280d0e3d20a88caa2478465de1de2
JamesMurkin <jamesmurkin@hotmail.com>
2023-05-19 13:40:31 +0100

    Revert "v0.0.0"
    This reverts commit c49b862199f551bcd2b4381202e64578f291b14d.

--- format.sh
+++
@@ -1,4 +0,0 @@
-ineffassign ./...
-go install golang.org/x/tools/cmd/goimports@v0.1.1
-goimports -l -local "github.com/armadaproject/armada" ./internal
-gofumpt -l ./internal
--- setup.sh
+++
@@ -1,1 +0,0 @@
-sudo sysctl fs.inotify.max_user_watches=524288
