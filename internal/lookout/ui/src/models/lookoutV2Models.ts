6991ebb03b41f814ad92a1421ef60f0a72731e6f
JamesMurkin <jamesmurkin@hotmail.com>
2023-05-23 15:41:37 +0100

    Fix state ordering + colouring

--- internal/lookout/ui/src/models/lookoutV2Models.ts
+++ internal/lookout/ui/src/models/lookoutV2Models.ts
@@ -1,16 +1,17 @@
 // Values must match the server-side states
 export enum JobState {
   Queued = "QUEUED",
+  Leased = "LEASED",
   Pending = "PENDING",
   Running = "RUNNING",
   Succeeded = "SUCCEEDED",
   Failed = "FAILED",
   Cancelled = "CANCELLED",
   Preempted = "PREEMPTED",
-  Leased = "LEASED",
 }
 
 export const jobStateDisplayInfo: Record<JobState, ColoredState> = {
+  [JobState.Leased]: { displayName: "Leased", color: "#f5c056" },
   [JobState.Queued]: { displayName: "Queued", color: "#ffff00" },
   [JobState.Pending]: { displayName: "Pending", color: "#ff9900" },
   [JobState.Running]: { displayName: "Running", color: "#00ff00" },
@@ -18,7 +19,6 @@ export const jobStateDisplayInfo: Record<JobState, ColoredState> = {
   [JobState.Failed]: { displayName: "Failed", color: "#ff0000" },
   [JobState.Cancelled]: { displayName: "Cancelled", color: "#999999" },
   [JobState.Preempted]: { displayName: "Preempted", color: "#f8bbd0" },
-  [JobState.Leased]: { displayName: "Leased", color: "#f5c056" }
 }
 
 const terminatedJobStates = new Set([JobState.Succeeded, JobState.Failed, JobState.Cancelled, JobState.Preempted])
@@ -49,7 +49,7 @@ export const jobRunStateDisplayInfo: Record<JobRunState, { displayName: string }
   [JobRunState.RunPreempted]: { displayName: "Preempted" },
   [JobRunState.RunLeaseExpired]: { displayName: "Lease Expired" },
   [JobRunState.RunMaxRunsExceeded]: { displayName: "Max Runs Exceeded" },
-  [JobRunState.RunLeased]: { displayName: "Leased" }
+  [JobRunState.RunLeased]: { displayName: "Leased" },
 }
 
 type ColoredState = {
