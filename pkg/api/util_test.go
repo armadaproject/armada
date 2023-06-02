2b92a715b4b23d88cb1c16e44662af5c962f8334
Albin Severinson <albin@severinson.org>
2023-06-02 11:37:35 +0100

    Add tests

--- pkg/api/util_test.go
+++ pkg/api/util_test.go
@@ -8,21 +8,24 @@ import (
 	resource "k8s.io/apimachinery/pkg/api/resource"
 
 	"github.com/armadaproject/armada/internal/armada/configuration"
+	"github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
 )
 
 const (
-	PriorityClass0 = "priority-0"
-	PriorityClass1 = "priority-1"
-	PriorityClass2 = "priority-2"
-	PriorityClass3 = "priority-3"
+	PriorityClassEmpty = ""
+	PriorityClass0     = "priority-0"
+	PriorityClass1     = "priority-1"
+	PriorityClass2     = "priority-2"
+	PriorityClass3     = "priority-3"
 )
 
 var (
 	TestPriorityClasses = map[string]configuration.PriorityClass{
-		PriorityClass0: {Priority: 0, Preemptible: true},
-		PriorityClass1: {Priority: 1, Preemptible: true},
-		PriorityClass2: {Priority: 2, Preemptible: true},
-		PriorityClass3: {Priority: 3, Preemptible: false},
+		PriorityClassEmpty: {Priority: 0, Preemptible: true},
+		PriorityClass0:     {Priority: 0, Preemptible: true},
+		PriorityClass1:     {Priority: 1, Preemptible: true},
+		PriorityClass2:     {Priority: 2, Preemptible: true},
+		PriorityClass3:     {Priority: 3, Preemptible: false},
 	}
 	TestDefaultPriorityClass = PriorityClass3
 	TestPriorities           = []int32{0, 1, 2, 3}
@@ -259,6 +262,250 @@ func QuantityWithMilliValue(v int64) resource.Quantity {
 	q := resource.Quantity{}
 	q.SetMilli(v)
 	return q
+}
+
+func TestJobGetRequirements(t *testing.T) {
+	tests := map[string]struct {
+		job      *Job
+		expected *schedulerobjects.JobSchedulingInfo
+	}{
+		"queue priority": {
+			job: &Job{
+				Priority: 10,
+				PodSpec:  &v1.PodSpec{
+					// Priority: pointerFromValue(int32(10)),
+					// PriorityClassName: ,
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				Priority: 10,
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: make(v1.ResourceList),
+									Limits:   make(v1.ResourceList),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"priorityClass priority": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{
+					PriorityClassName: PriorityClass1,
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				PriorityClassName: PriorityClass1,
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								Priority:         1,
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: make(v1.ResourceList),
+									Limits:   make(v1.ResourceList),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"preemptionPolicy": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{
+					PreemptionPolicy: pointerFromValue(v1.PreemptNever),
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								PreemptionPolicy: string(v1.PreemptNever),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: make(v1.ResourceList),
+									Limits:   make(v1.ResourceList),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"targeting": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{
+					NodeSelector: map[string]string{"label": "value"},
+					Affinity: &v1.Affinity{
+						NodeAffinity: &v1.NodeAffinity{
+							RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+								NodeSelectorTerms: []v1.NodeSelectorTerm{
+									{
+										MatchExpressions: []v1.NodeSelectorRequirement{
+											{
+												Key: "affinityKey",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					Tolerations: []v1.Toleration{
+						{
+							Key: "tolerationKey",
+						},
+					},
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								NodeSelector: map[string]string{"label": "value"},
+								Affinity: &v1.Affinity{
+									NodeAffinity: &v1.NodeAffinity{
+										RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+											NodeSelectorTerms: []v1.NodeSelectorTerm{
+												{
+													MatchExpressions: []v1.NodeSelectorRequirement{
+														{
+															Key: "affinityKey",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+								Tolerations: []v1.Toleration{
+									{
+										Key: "tolerationKey",
+									},
+								},
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: make(v1.ResourceList),
+									Limits:   make(v1.ResourceList),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"annotations": {
+			job: &Job{
+				Annotations: map[string]string{"key": "value"},
+				PodSpec:     &v1.PodSpec{},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								Annotations:      map[string]string{"key": "value"},
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: make(v1.ResourceList),
+									Limits:   make(v1.ResourceList),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"schedulingResourceRequirements": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Resources: v1.ResourceRequirements{
+								Requests: v1.ResourceList{"foo": QuantityWithMilliValue(1000)},
+								Limits:   v1.ResourceList{"bar": QuantityWithMilliValue(2000)},
+							},
+						},
+					},
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: v1.ResourceList{"foo": QuantityWithMilliValue(1000)},
+									Limits:   v1.ResourceList{"bar": QuantityWithMilliValue(2000)},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"schedulingResourceRequirements pre-computed requests": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{},
+				SchedulingResourceRequirements: v1.ResourceRequirements{
+					Requests: v1.ResourceList{"foo": resource.MustParse("1")},
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: v1.ResourceList{"foo": resource.MustParse("1")},
+									Limits:   nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"schedulingResourceRequirements pre-computed limits": {
+			job: &Job{
+				PodSpec: &v1.PodSpec{},
+				SchedulingResourceRequirements: v1.ResourceRequirements{
+					Limits: v1.ResourceList{"foo": resource.MustParse("1")},
+				},
+			},
+			expected: &schedulerobjects.JobSchedulingInfo{
+				ObjectRequirements: []*schedulerobjects.ObjectRequirements{
+					{
+						&schedulerobjects.ObjectRequirements_PodRequirements{
+							PodRequirements: &schedulerobjects.PodRequirements{
+								PreemptionPolicy: string(v1.PreemptLowerPriority),
+								ResourceRequirements: v1.ResourceRequirements{
+									Requests: nil,
+									Limits:   v1.ResourceList{"foo": resource.MustParse("1")},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, tc.job.GetRequirements(TestPriorityClasses))
+		})
+	}
 }
 
 func TestPriorityFromPodSpec(t *testing.T) {
