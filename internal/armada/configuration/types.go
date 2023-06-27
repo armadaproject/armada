1c7514178f7b779ded72f8780d3d7b4743398aa3
Albin Severinson <albin@severinson.org>
2023-06-27 11:54:42 +0100

    Comment

--- internal/armada/configuration/types.go
+++ internal/armada/configuration/types.go
@@ -209,7 +209,7 @@ type PreemptionConfig struct {
 	// the probability of evicting jobs on oversubscribed nodes, i.e.,
 	// nodes on which the total resource requests are greater than the available resources.
 	NodeOversubscriptionEvictionProbability float64
-	// Jobs from queues allocated less than this fraction of their fair share are not evicted.
+	// Queues allocated less than this fraction of their fair share are not considered for preemption.
 	ProtectedFractionOfFairShare float64
 	// If true, the Armada scheduler will add to scheduled pods a node selector
 	// NodeIdLabel: <value of label on node selected by scheduler>.
