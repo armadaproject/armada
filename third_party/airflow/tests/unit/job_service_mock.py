1c62adc764f8717f7eddc5a882ac799f0b893554
Pavan Kongara <pavan.kongara@nurture.farm>
2023-07-07 12:12:14 +0530

    test_added_for_tenacity

--- third_party/airflow/tests/unit/job_service_mock.py
+++ third_party/airflow/tests/unit/job_service_mock.py
@@ -1,4 +1,5 @@
 from armada.jobservice import jobservice_pb2, jobservice_pb2_grpc
+import tenacity
 
 
 # TODO - Make this a bit smarter, so we can hit at least one full
@@ -22,10 +23,30 @@ def mock_dummy_mapper_terminal(request):
 
 
 class JobService(jobservice_pb2_grpc.JobServiceServicer):
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(4),
+        wait=tenacity.wait_exponential(),
+        reraise=True,
+    )
     def GetJobStatus(self, request, context):
         return mock_dummy_mapper_terminal(request)
-
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(4),
+        wait=tenacity.wait_exponential(),
+        reraise=True,
+    )
     def Health(self, request, context):
         return jobservice_pb2.HealthCheckResponse(
             status=jobservice_pb2.HealthCheckResponse.SERVING
         )
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(4),
+        wait=tenacity.wait_exponential(),
+        reraise=True,
+    )
+    def tenacity_example(target_count):
+        current_count = JobService.tenacity_example.retry.statistics['attempt_number']
+        if current_count<target_count:
+            raise IOError("dummy error")
+        else:
+            return
