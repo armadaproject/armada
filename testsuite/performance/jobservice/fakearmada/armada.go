a677fb908e88b3a125e7d4c7e85c43f972d20a5d
Clif Houck <me@clifhouck.com>
2023-07-31 17:01:10 -0500

    Jobservice performance testing tool (#2778)

---
+++ testsuite/performance/jobservice/fakearmada/armada.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/armadaproject/armada/pkg/api"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/encoding/gzip"
+)
+
+func ServePerformanceTestArmadaServer(port int) error {
+	comp := encoding.GetCompressor(gzip.Name)
+	encoding.RegisterCompressor(comp)
+
+	server := grpc.NewServer([]grpc.ServerOption{}...)
+
+	performanceTestEventServer := NewPerformanceTestEventServer()
+
+	api.RegisterEventServer(server, performanceTestEventServer)
+
+	log.Infof("Armada performanceTestEventServer gRPC server listening on %d", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	return server.Serve(lis)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		err := ServePerformanceTestArmadaServer(1337)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
