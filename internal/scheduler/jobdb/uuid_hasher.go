a5950e775b87bd45e3b2b8bc0b70835edfd4da5b
Chris Martin <chris@cmartinit.co.uk>
2023-08-12 10:42:55 +0100

    added uuid hasher

---
+++ internal/scheduler/jobdb/uuid_hasher.go
@@ -0,0 +1,23 @@
+package jobdb
+
+import (
+	"bytes"
+	"github.com/google/uuid"
+)
+
+// UUIDHasher is an implementation of Hasher for UUID.
+type UUIDHasher struct{}
+
+// Hash computes a hash for a UUID.
+func (h UUIDHasher) Hash(key uuid.UUID) uint32 {
+	var hash uint32
+	for _, b := range key {
+		hash = hash*31 + uint32(b)
+	}
+	return hash
+}
+
+// Equal checks if two UUIDs are equal.
+func (h UUIDHasher) Equal(a, b uuid.UUID) bool {
+	return bytes.Equal(a[:], b[:])
+}
---
+++ internal/scheduler/jobdb/uuid_hasher_test.go
@@ -0,0 +1,74 @@
+package jobdb
+
+import (
+	"github.com/google/uuid"
+
+	"testing"
+)
+
+func TestUUIDHasher_Hash(t *testing.T) {
+	hasher := UUIDHasher{}
+
+	tests := []struct {
+		name string
+		key  uuid.UUID
+	}{
+		{
+			name: "Test with zero UUID",
+			key:  uuid.UUID{},
+		},
+		{
+			name: "Test with random UUID",
+			key:  uuid.New(),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := hasher.Hash(tt.key)
+
+			// Assert the hash value is non-negative (a simple check)
+			if got < 0 {
+				t.Errorf("Expected non-negative hash, but got %v", got)
+			}
+		})
+	}
+}
+
+func TestUUIDHasher_Equal(t *testing.T) {
+	hasher := UUIDHasher{}
+
+	tests := []struct {
+		name string
+		a, b uuid.UUID
+		want bool
+	}{
+		{
+			name: "Test with two zero UUIDs",
+			a:    uuid.UUID{},
+			b:    uuid.UUID{},
+			want: true,
+		},
+		{
+			name: "Test with two different UUIDs",
+			a:    uuid.New(),
+			b:    uuid.New(),
+			want: false,
+		},
+		{
+			name: "Test with two same UUIDs",
+			a:    uuid.New(),
+			b:    uuid.MustParse("f47ac10b-58cc-4372-a567-0e02b2c3d479"), // Example UUID, replace with any fixed UUID
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := hasher.Equal(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Expected %v, but got %v", tt.want, got)
+			}
+		})
+	}
+}
