3f0325b221d9021ccb6294b59e8674c57ce9ed7b
Mohamed Abdelfatah <39927413+Mo-Fatah@users.noreply.github.com>
2023-08-15 18:14:44 +0300

    Lookoutv2: Remove unnecessary transactions (#2807)
    * Lookoutv2: Remove unnecessary transactions
    
    * better error handling

--- internal/lookoutv2/repository/getjobspec.go
+++ internal/lookoutv2/repository/getjobspec.go
@@ -31,21 +31,13 @@ func NewSqlGetJobSpecRepository(db *pgxpool.Pool, decompressor compress.Decompre
 
 func (r *SqlGetJobSpecRepository) GetJobSpec(ctx context.Context, jobId string) (*api.Job, error) {
 	var rawBytes []byte
-	err := pgx.BeginTxFunc(ctx, r.db, pgx.TxOptions{
-		IsoLevel:       pgx.RepeatableRead,
-		AccessMode:     pgx.ReadOnly,
-		DeferrableMode: pgx.Deferrable,
-	}, func(tx pgx.Tx) error {
-		err := tx.QueryRow(ctx, "SELECT job_spec FROM job WHERE job_id = $1", jobId).Scan(&rawBytes)
-		if err == pgx.ErrNoRows {
-			return errors.Errorf("job with id %s not found", jobId)
-		}
-		return err
-	})
+	err := r.db.QueryRow(ctx, "SELECT job_spec FROM job WHERE job_id = $1", jobId).Scan(&rawBytes)
 	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, errors.Errorf("job with id %s not found", jobId)
+		}
 		return nil, err
 	}
-
 	decompressed, err := r.decompressor.Decompress(rawBytes)
 	if err != nil {
 		log.WithError(err).Error("failed to decompress")
