d0b9f13f15eddb1e6e63a8fbc9a0da0578a6b194
Daniel Rastelli <rastellidani@gmail.com>
2023-08-22 14:30:51 +0100

    Add jobs considered and refactor to counters

--- internal/scheduler/scheduler_metrics.go
+++ internal/scheduler/scheduler_metrics.go
@@ -3,11 +3,12 @@ package scheduler
 import (
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/armadaproject/armada/internal/armada/configuration"
+	schedulercontext "github.com/armadaproject/armada/internal/scheduler/context"
 	"github.com/armadaproject/armada/internal/scheduler/interfaces"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
@@ -21,9 +22,11 @@ type SchedulerMetrics struct {
 	// Cycle time when reconciling, as leader or follower.
 	reconcileCycleTime prometheus.Histogram
 	// Number of jobs scheduled per queue.
-	scheduledJobsPerQueue prometheus.GaugeVec
+	scheduledJobsPerQueue prometheus.CounterVec
 	// Number of jobs preempted per queue.
-	preemptedJobsPerQueue prometheus.GaugeVec
+	preemptedJobsPerQueue prometheus.CounterVec
+	// Number of jobs considered per queue/pool.
+	consideredJobs prometheus.CounterVec
 }
 
 func NewSchedulerMetrics(config configuration.SchedulerMetricsConfig) *SchedulerMetrics {
@@ -53,8 +56,8 @@ func NewSchedulerMetrics(config configuration.SchedulerMetricsConfig) *Scheduler
 		},
 	)
 
-	scheduledJobs := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
+	scheduledJobs := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
 			Namespace: NAMESPACE,
 			Subsystem: SUBSYSTEM,
 			Name:      "scheduled_jobs",
@@ -66,8 +69,8 @@ func NewSchedulerMetrics(config configuration.SchedulerMetricsConfig) *Scheduler
 		},
 	)
 
-	preemptedJobs := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
+	preemptedJobs := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
 			Namespace: NAMESPACE,
 			Subsystem: SUBSYSTEM,
 			Name:      "preempted_jobs",
@@ -79,10 +82,24 @@ func NewSchedulerMetrics(config configuration.SchedulerMetricsConfig) *Scheduler
 		},
 	)
 
+	consideredJobs := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: NAMESPACE,
+			Subsystem: SUBSYSTEM,
+			Name:      "considered_jobs",
+			Help:      "Number of jobs considered each round per queue and pool.",
+		},
+		[]string{
+			"queue",
+			"pool",
+		},
+	)
+
 	prometheus.MustRegister(scheduleCycleTime)
 	prometheus.MustRegister(reconcileCycleTime)
 	prometheus.MustRegister(scheduledJobs)
 	prometheus.MustRegister(preemptedJobs)
+	prometheus.MustRegister(consideredJobs)
 
 	return &SchedulerMetrics{
 		scheduleCycleTime:     scheduleCycleTime,
@@ -101,8 +118,13 @@ func (metrics *SchedulerMetrics) ReportReconcileCycleTime(cycleTime time.Duratio
 }
 
 func (metrics *SchedulerMetrics) ReportSchedulerResult(result *SchedulerResult) {
+	// Report the total scheduled jobs (possibly we can get these out of contexts?)
 	metrics.reportScheduledJobs(result.ScheduledJobs)
 	metrics.reportPreemptedJobs(result.PreemptedJobs)
+
+	// TODO: When more metrics are added, consider consolidating into a single loop over the data.
+	// Report the number of considered jobs.
+	metrics.reportNumberOfJobsConsidered(result.SchedulingContexts)
 }
 
 func (metrics *SchedulerMetrics) reportScheduledJobs(scheduledJobs []interfaces.LegacySchedulerJob) {
@@ -132,8 +154,8 @@ func aggregateJobs[S ~[]E, E interfaces.LegacySchedulerJob](scheduledJobs S) map
 	return groups
 }
 
-// observeJobAggregates reports a set of job aggregates to a given HistogramVec by queue and priorityClass.
-func observeJobAggregates(metric prometheus.GaugeVec, jobAggregates map[collectionKey]int) {
+// observeJobAggregates reports a set of job aggregates to a given CounterVec by queue and priorityClass.
+func observeJobAggregates(metric prometheus.CounterVec, jobAggregates map[collectionKey]int) {
 	for key, count := range jobAggregates {
 		queue := key.queue
 		priorityClassName := key.priorityClass
@@ -142,6 +164,37 @@ func observeJobAggregates(metric prometheus.GaugeVec, jobAggregates map[collecti
 
 		if err != nil {
 			// A metric failure isn't reason to kill the programme.
+			log.Error(err)
+		} else {
+			observer.Add(float64(count))
+		}
+	}
+}
+
+type poolQueueKey struct {
+	pool  string
+	queue string
+}
+
+func (metrics *SchedulerMetrics) reportNumberOfJobsConsidered(schedulingContexts []*schedulercontext.SchedulingContext) {
+	consideredJobs := make(map[poolQueueKey]int)
+
+	for _, schedContext := range schedulingContexts {
+		for _, queueContext := range schedContext.QueueSchedulingContexts {
+			consideredJobs[poolQueueKey{
+				pool:  schedContext.Pool,
+				queue: queueContext.Queue,
+			}] += len(queueContext.UnsuccessfulJobSchedulingContexts) + len(queueContext.SuccessfulJobSchedulingContexts)
+		}
+	}
+
+	for key, count := range consideredJobs {
+		pool := key.pool
+		queue := key.queue
+
+		observer, err := metrics.consideredJobs.GetMetricWithLabelValues(queue, pool)
+
+		if err != nil {
 			log.Error(err)
 		} else {
 			observer.Add(float64(count))
