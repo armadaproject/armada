5a45a914325a8599670e749017923bba7aa180a8
Noah Held <41909795+zuqq@users.noreply.github.com>
2023-12-19 11:30:48 +0000

    Set `Cache-Control` header for `index.html` (#3202)

--- internal/common/serve/static.go
+++ internal/common/serve/static.go
@@ -9,22 +9,6 @@ import (
 	"github.com/armadaproject/armada/internal/common/logging"
 )
 
-type dirWithIndexFallback struct {
-	dir http.Dir
-}
-
-func CreateDirWithIndexFallback(path string) http.FileSystem {
-	return dirWithIndexFallback{http.Dir(path)}
-}
-
-func (d dirWithIndexFallback) Open(name string) (http.File, error) {
-	file, err := d.dir.Open(name)
-	if err != nil {
-		return d.dir.Open("index.html")
-	}
-	return file, err
-}
-
 // ListenAndServe calls server.ListenAndServe().
 // Additionally, it calls server.Shutdown() if ctx is cancelled.
 func ListenAndServe(ctx *armadacontext.Context, server *http.Server) error {
