c73285d70b4d4063dac57bb6c786bee16d680198
Chris Martin <chris@cmartinit.co.uk>
2024-02-04 18:48:29 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/armadactl/queue.go
+++ internal/armadactl/queue.go
@@ -2,8 +2,6 @@ package armadactl
 
 import (
 	"fmt"
-	"sort"
-
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 
@@ -52,30 +50,6 @@ func (a *App) DeleteQueue(name string) error {
 		return errors.Errorf("[armadactl.DeleteQueue] error deleting queue %s: %s", name, err)
 	}
 	fmt.Fprintf(a.Out, "Deleted queue %s (or it did not exist)\n", name)
-	return nil
-}
-
-// DescribeQueue calls app.QueueAPI.Describe with the provided parameters.
-func (a *App) DescribeQueue(name string) error {
-	fmt.Fprintf(a.Out, "Queue: %s\n", name)
-	queueInfo, err := a.Params.QueueAPI.GetInfo(name)
-	if err != nil {
-		return errors.Errorf("[armadactl.DescribeQueue] error describing queue %s: %s", name, err)
-	}
-
-	jobSets := queueInfo.ActiveJobSets
-	sort.SliceStable(jobSets, func(i, j int) bool {
-		return jobSets[i].Name < jobSets[j].Name
-	})
-
-	if len(jobSets) == 0 {
-		fmt.Fprintf(a.Out, "No queued or running jobs\n")
-	} else {
-		for _, jobSet := range jobSets {
-			fmt.Fprintf(a.Out, "[job set: %s] Running: %d, Queued: %d\n", jobSet.Name, jobSet.LeasedJobs, jobSet.QueuedJobs)
-		}
-	}
-
 	return nil
 }
 
