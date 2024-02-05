c3e761e411490a3e2f3cd268bbce6b1a6c1ed253
Chris Martin <chris@cmartinit.co.uk>
2024-02-05 10:13:18 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/executor/service/job_requester_test.go
+++ internal/executor/service/job_requester_test.go
@@ -55,7 +55,7 @@ func TestRequestJobsRuns_ConstructsCorrectLeaseRequest(t *testing.T) {
 
 	capacityReport := &utilisation.ClusterAvailableCapacityReport{
 		AvailableCapacity: &armadaresource.ComputeResources{
-			"cpu":    resource.MustParse("100s0"),
+			"cpu":    resource.MustParse("100s"),
 			"memory": resource.MustParse("1000Gi"),
 		},
 		Nodes: []executorapi.NodeInfo{
