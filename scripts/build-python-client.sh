cd6aba486402abc011ff5fafbd732ce2e3c6eef5
Chris Martin <chris@cmartinit.co.uk>
2024-02-05 10:04:44 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- scripts/build-python-client.sh
+++ scripts/build-python-client.sh
@@ -11,7 +11,7 @@ cd proto
 python3 -m grpc_tools.protoc -I. --plugin=protoc-gen-mypy=$(which protoc-gen-mypy) --python_out=../client/python/armada_client --grpc_python_out=../client/python/armada_client --mypy_out=../client/python/armada_client \
     google/api/annotations.proto \
     google/api/http.proto \
-    armada/event.proto armada/queue.proto armada/submit.proto armada/usage.proto armada/health.proto \
+    armada/event.proto armada/submit.proto armada/usage.proto armada/health.proto \
     github.com/gogo/protobuf/gogoproto/gogo.proto \
     k8s.io/api/core/v1/generated.proto \
     k8s.io/apimachinery/pkg/api/resource/generated.proto \
