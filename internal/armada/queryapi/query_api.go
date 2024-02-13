fa9a98510831ac5ef0abe8b0601098bfe17f8116
Chris Martin <chris@cmartinit.co.uk>
2024-02-13 07:21:47 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/armada/queryapi/query_api.go
+++ internal/armada/queryapi/query_api.go
@@ -2,24 +2,24 @@ package queryapi
 
 import (
 	"context"
+	"github.com/armadaproject/armada/pkg/api"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/armadaproject/armada/internal/armada/queryapi/database"
 	"github.com/armadaproject/armada/internal/common/database/lookout"
-	"github.com/armadaproject/armada/pkg/queryapi"
 )
 
 // JobStateMap is a mapping between database state and api states
-var JobStateMap = map[int16]queryapi.JobStatus{
-	lookout.JobLeasedOrdinal:    queryapi.JobStatus_LEASED,
-	lookout.JobQueuedOrdinal:    queryapi.JobStatus_QUEUED,
-	lookout.JobPendingOrdinal:   queryapi.JobStatus_PENDING,
-	lookout.JobRunningOrdinal:   queryapi.JobStatus_RUNNING,
-	lookout.JobSucceededOrdinal: queryapi.JobStatus_SUCCEEDED,
-	lookout.JobFailedOrdinal:    queryapi.JobStatus_FAILED,
-	lookout.JobCancelledOrdinal: queryapi.JobStatus_CANCELLED,
-	lookout.JobPreemptedOrdinal: queryapi.JobStatus_PREEMPTED,
+var JobStateMap = map[int16]api.JobStatus{
+	lookout.JobLeasedOrdinal:    api.JobStatus_LEASED,
+	lookout.JobQueuedOrdinal:    api.JobStatus_QUEUED,
+	lookout.JobPendingOrdinal:   api.JobStatus_PENDING,
+	lookout.JobRunningOrdinal:   api.JobStatus_RUNNING,
+	lookout.JobSucceededOrdinal: api.JobStatus_SUCCEEDED,
+	lookout.JobFailedOrdinal:    api.JobStatus_FAILED,
+	lookout.JobCancelledOrdinal: api.JobStatus_CANCELLED,
+	lookout.JobPreemptedOrdinal: api.JobStatus_PREEMPTED,
 }
 
 type QueryApi struct {
@@ -30,7 +30,7 @@ func New(db *pgxpool.Pool) *QueryApi {
 	return &QueryApi{db: db}
 }
 
-func (q *QueryApi) GetJobStatus(ctx context.Context, req *queryapi.JobStatusRequest) (*queryapi.JobStatusResponse, error) {
+func (q *QueryApi) GetJobStatus(ctx context.Context, req *api.JobStatusRequest) (*api.JobStatusResponse, error) {
 	queries := database.New(q.db)
 	queryResult, err := queries.GetJobState(ctx, req.JobId)
 	if err != nil {
@@ -42,9 +42,9 @@ func (q *QueryApi) GetJobStatus(ctx context.Context, req *queryapi.JobStatusRequ
 	}
 	apiStatus, ok := JobStateMap[status]
 	if !ok {
-		apiStatus = queryapi.JobStatus_UNKNOWN
+		apiStatus = api.JobStatus_UNKNOWN
 	}
-	return &queryapi.JobStatusResponse{
+	return &api.JobStatusResponse{
 		JobId:     req.JobId,
 		JobStatus: apiStatus,
 	}, nil
