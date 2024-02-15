d2a2d93382054c7d134f6423a58c6afaa0eb8338
shivam <shivambharadwaj822@gmail.com>
2024-02-16 03:43:40 +0530

    Created instance for mockfiles
    Signed-off-by: shivam <shivambharadwaj822@gmail.com>

--- internal/common/database/migrations_test.go
+++ internal/common/database/migrations_test.go
@@ -8,11 +8,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testFS struct {
+	files map[string]string
+}
+
+func (myFS *testFS) Open(name string) (fs.File, error) {
+	content, ok := myFS.files[name]
+	if !ok {
+		return nil, fs.ErrNotExist
+	}
+	return fs.FileString(content), nil
+}
+
 func TestReadMigrations(t *testing.T) {
     basePathScheduler := "internal/scheduler/database/migrations"
 	basePathLookoutV2 := "internal/lookoutv2/schema/migrations"
 
-	mockFS := createMockFileSystem([]string{
+	mockFiles := []string{
         "001_initial_schema.sql",
         "002_cancel_reason.sql",
         "003_run_leased_column.sql",
@@ -24,22 +36,29 @@ func TestReadMigrations(t *testing.T) {
         "005_add_runs_timestamps.up.sql",
         "006_add_run_pod_requirements.up.sql",
         "007_add_queue_jobset_index.sql",
-    })
+    }
 
-	migrations, err := ReadMigrations(mockFS, basePath)
+	mockFS := createMockFileSystem(mockFiles)
+	migrations, err := ReadMigrations(mockFS, basePathScheduler, basePathLookoutV2)
 
 	for _, migration := range migrations {
 		assert.True(t, strings.HasSuffix(migration.name, ".sql"), "Invalid file format for %v", migration.name)
 	}
     
 	for i := 1; i < len(migrations); i++ {
-		assert.True(t, migrations[i-1].name <= migrations[i].name, "Not sorted correctly")
+		assert.LessOrEqual(t, migrations[i-1].name <= migrations[i].name, "Not sorted correctly")
+	}
+
+	for _, migration := range migrations {
+		assert.IsType(t, 0, migration.id, "Should be of type int")
+		assert.IsType(t, "", migration.name, "Should be of type string")
+		assert.IsType(t, "", migration.sql, "Should be of type string")
 	}
     
 	assert.Equal(t, migrations[0].id, 1, "Incorrect ID for 001_initial_schema.sql")
 	assert.Equal(t, migrations[1].id, 2, "Incorrect ID for 002_cancel_reason.sql")
 	assert.Equal(t, migrations[2].id, 3, "Incorrect ID for 003_run_leased_column.sql")
-	assert.Equal(t, migrations[3].id, 4, "Incorrect ID for 004_job_namespace.sqlz")
+	assert.Equal(t, migrations[3].id, 4, "Incorrect ID for 004_job_namespace.sql")
 	assert.Equal(t, migrations[4].id, 1, "Incorrect ID for 001_initialize_schema.up.sql")
 	assert.Equal(t, migrations[5].id, 2, "Incorrect ID for 002_add_runs_timestamps.up.sql")
 	assert.Equal(t, migrations[6].id, 3, "Incorrect ID for 003_add_runs_scheduled_at_priority.up.sql")
@@ -53,12 +72,14 @@ func TestReadMigrations(t *testing.T) {
 	}
 
 	for _, migration := range migrations {
-		assert.IsType(t, 0, migration.id, "Should be of type int")
-		assert.IsType(t, "", migration.name, "Should be of type string")
-		assert.IsType(t, "", migration.sql, "Should be of type string")
-	}
-
-	for _, migration := range migrations {
 		assert.NotEmpty(t, migration.sql, "Content should not be empty for %v", migration.sql)
 	}
+}
+
+func createMockFileSystem(files []string) fs.FS {
+	mockFiles := make(map[string]string)
+	for _, file := range files {
+		mockFiles[file] = "content of " + file
+	}
+	return &testFS{files: mockFiles}
 }
