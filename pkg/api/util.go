41acc0c921159a202e2d8c0e3e966fd7e5361b1f
Chris Martin <chris@cmartinit.co.uk>
2024-02-18 15:25:27 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- pkg/api/util.go
+++ pkg/api/util.go
@@ -2,33 +2,12 @@ package api
 
 import (
 	"fmt"
-	"math"
-	"strings"
-	"time"
-
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
-	"golang.org/x/exp/maps"
 	v1 "k8s.io/api/core/v1"
+	"strings"
 
-	"github.com/armadaproject/armada/internal/common/logging"
-	armadaresource "github.com/armadaproject/armada/internal/common/resource"
 	"github.com/armadaproject/armada/internal/common/types"
-	"github.com/armadaproject/armada/internal/scheduler/interfaces"
 	"github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
 )
-
-// IsTerminal returns true if the JobState s corresponds to a state
-// that indicates the job has been terminated.
-func (s JobState) IsTerminal() bool {
-	switch s {
-	case JobState_SUCCEEDED:
-		return true
-	case JobState_FAILED:
-		return true
-	}
-	return false
-}
 
 func NodeIdFromExecutorAndNodeName(executor, nodeName string) string {
 	return fmt.Sprintf("%s-%s", executor, nodeName)
@@ -50,48 +29,6 @@ func JobRunStateFromApiJobState(s JobState) schedulerobjects.JobRunState {
 		return schedulerobjects.JobRunState_UNKNOWN
 	}
 	return schedulerobjects.JobRunState_UNKNOWN
-}
-
-func (job *Job) GetPerQueuePriority() uint32 {
-	priority := job.Priority
-	if priority < 0 {
-		return 0
-	}
-	if priority > math.MaxUint32 {
-		return math.MaxUint32
-	}
-	return uint32(math.Round(priority))
-}
-
-func (job *Job) GetSubmitTime() time.Time {
-	return job.Created
-}
-
-func (job *Job) GetPodRequirements(priorityClasses map[string]types.PriorityClass) *schedulerobjects.PodRequirements {
-	podSpec := job.GetMainPodSpec()
-
-	priority, ok := PriorityFromPodSpec(podSpec, priorityClasses)
-	if priorityClasses != nil && !ok {
-		// Ignore this error if priorityByPriorityClassName is explicitly set to nil.
-		// We assume that in this case the caller is sure the priority does not need to be set.
-		err := errors.Errorf("unknown priorityClassName %s", podSpec.PriorityClassName)
-		logging.WithStacktrace(logrus.NewEntry(logrus.New()), err).Error("failed to get priority from priorityClassName")
-	}
-
-	preemptionPolicy := string(v1.PreemptLowerPriority)
-	if podSpec.PreemptionPolicy != nil {
-		preemptionPolicy = string(*podSpec.PreemptionPolicy)
-	}
-
-	return &schedulerobjects.PodRequirements{
-		NodeSelector:         podSpec.NodeSelector,
-		Affinity:             podSpec.Affinity,
-		Tolerations:          podSpec.Tolerations,
-		Annotations:          maps.Clone(job.Annotations),
-		Priority:             priority,
-		PreemptionPolicy:     preemptionPolicy,
-		ResourceRequirements: job.GetResourceRequirements(),
-	}
 }
 
 // SchedulingResourceRequirementsFromPodSpec returns resource requests and limits necessary for scheduling a pod.
@@ -164,93 +101,6 @@ func PriorityFromPodSpec(podSpec *v1.PodSpec, priorityClasses map[string]types.P
 	return 0, false
 }
 
-func (job *Job) GetPriorityClassName() string {
-	if podSpec := job.GetMainPodSpec(); podSpec != nil {
-		return podSpec.PriorityClassName
-	}
-	return ""
-}
-
-func (job *Job) GetScheduledAtPriority() (int32, bool) {
-	return -1, false
-}
-
-func (job *Job) GetNodeSelector() map[string]string {
-	podSpec := job.GetMainPodSpec()
-	return podSpec.NodeSelector
-}
-
-func (job *Job) GetAffinity() *v1.Affinity {
-	podSpec := job.GetMainPodSpec()
-	return podSpec.Affinity
-}
-
-func (job *Job) GetTolerations() []v1.Toleration {
-	podSpec := job.GetMainPodSpec()
-	return podSpec.Tolerations
-}
-
-func (job *Job) GetResourceRequirements() v1.ResourceRequirements {
-	// Use pre-computed schedulingResourceRequirements if available.
-	// Otherwise compute it from the containers in podSpec.
-	podSpec := job.GetMainPodSpec()
-	if len(job.SchedulingResourceRequirements.Requests) > 0 || len(job.SchedulingResourceRequirements.Limits) > 0 {
-		return job.SchedulingResourceRequirements
-	} else {
-		return SchedulingResourceRequirementsFromPodSpec(podSpec)
-	}
-}
-
-// GetSchedulingKey returns the scheduling key associated with a job.
-// The second return value is always false since scheduling keys are not pre-computed for these jobs.
-func (job *Job) GetSchedulingKey() (schedulerobjects.SchedulingKey, bool) {
-	return schedulerobjects.SchedulingKey{}, false
-}
-
-// SchedulingOrderCompare defines the order in which jobs in a particular queue should be scheduled,
-func (job *Job) SchedulingOrderCompare(other interfaces.LegacySchedulerJob) int {
-	// We need this cast for now to expose this method via an interface.
-	// This is safe since we only ever compare jobs of the same type.
-	return SchedulingOrderCompare(job, other.(*Job))
-}
-
-// SchedulingOrderCompare defines the order in which jobs in a queue should be scheduled
-// (both when scheduling new jobs and when re-scheduling evicted jobs).
-// Specifically, compare returns
-//   - 0 if the jobs have equal job id,
-//   - -1 if job should be scheduled before other,
-//   - +1 if other should be scheduled before other.
-func SchedulingOrderCompare(job, other *Job) int {
-	if job.Id == other.Id {
-		return 0
-	}
-
-	// Jobs with higher in queue-priority come first.
-	if job.Priority < other.Priority {
-		return -1
-	} else if job.Priority > other.Priority {
-		return 1
-	}
-
-	// Jobs that have been queuing for longer are scheduled first.
-	if cmp := job.Created.Compare(other.Created); cmp != 0 {
-		return cmp
-	}
-
-	// Tie-break by jobId, which must be unique.
-	// This ensure there is a total order between jobs, i.e., no jobs are equal from an ordering point of view.
-	if job.Id < other.Id {
-		return -1
-	} else if job.Id > other.Id {
-		return 1
-	}
-	panic("We should never get here. Since we check for job id equality at the top of this function.")
-}
-
-func (job *Job) GetJobSet() string {
-	return job.JobSetId
-}
-
 func (job *Job) GetMainPodSpec() *v1.PodSpec {
 	if job.PodSpec != nil {
 		return job.PodSpec
@@ -270,11 +120,6 @@ func (job *JobSubmitRequestItem) GetMainPodSpec() *v1.PodSpec {
 		return job.PodSpecs[0]
 	}
 	return nil
-}
-
-func (job *Job) TotalResourceRequest() armadaresource.ComputeResources {
-	podSpec := job.GetMainPodSpec()
-	return armadaresource.TotalPodResourceRequest(podSpec)
 }
 
 func ShortStringFromEventMessages(msgs []*EventMessage) string {
