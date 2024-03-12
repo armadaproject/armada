8dc46bf4dae0e30f5c9f2aeea7a260d0854fb8a8
Chris Martin <chris@cmartinit.co.uk>
2024-03-12 07:39:49 +0000

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/schedulerapp.go
+++ internal/scheduler/schedulerapp.go
@@ -174,6 +174,18 @@ func Run(config schedulerconfig.Configuration) error {
 	// ////////////////////////////////////////////////////////////////////////
 	ctx.Infof("setting up scheduling loop")
 
+	submitChecker := NewSubmitChecker(
+		30*time.Minute,
+		config.Scheduling,
+		executorRepository,
+	)
+	services = append(services, func() error {
+		return submitChecker.Run(ctx)
+	})
+	if err != nil {
+		return errors.WithMessage(err, "error creating submit checker")
+	}
+
 	schedulingContextRepository, err := NewSchedulingContextRepository(config.Scheduling.MaxJobSchedulingContextsPerExecutor)
 	if err != nil {
 		return errors.WithMessage(err, "error creating scheduling context repository")
@@ -235,6 +247,7 @@ func Run(config schedulerconfig.Configuration) error {
 		schedulingAlgo,
 		leaderController,
 		pulsarPublisher,
+		submitChecker,
 		config.CyclePeriod,
 		config.SchedulePeriod,
 		config.ExecutorTimeout,
