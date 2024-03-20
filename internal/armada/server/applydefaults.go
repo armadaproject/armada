567b1a80236161d870596276f526f5719d0739b5
Chris Martin <chris@cmartinit.co.uk>
2024-03-20 08:01:22 +0000

    format
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/armada/server/applydefaults.go
+++ internal/armada/server/applydefaults.go
@@ -131,7 +131,7 @@ func applyDefaultActiveDeadlineSecondsToPodSpec(spec *v1.PodSpec, config configu
 // Similarly, if a Container specifies its own CPU limit, but does not specify a CPU request, automatically
 // assigns a CPU request that matches the limit.
 func fillContainerRequestsAndLimits(containers []v1.Container) string {
-	var infoMsg = ""
+	infoMsg := ""
 	for index := range containers {
 		if containers[index].Resources.Limits == nil {
 			containers[index].Resources.Limits = v1.ResourceList{}
