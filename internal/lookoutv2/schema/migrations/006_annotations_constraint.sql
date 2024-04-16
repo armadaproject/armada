e963a66d541460b88617b4d32f02b3939787f462
JamesMurkin <jamesmurkin@hotmail.com>
2024-04-16 12:35:53 +0100

    Update constraint name
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>

--- internal/lookoutv2/schema/migrations/006_annotations_constraint.sql
+++ internal/lookoutv2/schema/migrations/006_annotations_constraint.sql
@@ -1,2 +1,2 @@
-ALTER TABLE job ADD CONSTRAINT annotations_not_null CHECK (annotations IS NOT NULL) NOT VALID;
-ALTER TABLE job VALIDATE CONSTRAINT annotations_not_null;
+ALTER TABLE job ADD CONSTRAINT job_annotations_not_null CHECK (annotations IS NOT NULL) NOT VALID;
+ALTER TABLE job VALIDATE CONSTRAINT job_annotations_not_null;
