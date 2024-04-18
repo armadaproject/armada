30ba84fef64e12102d3ebfd4777b1d4b3a7d5547
Chris Martin <council_tax@hotmail.com>
2024-04-18 13:55:23 +0100

    Update internal/armada/submit/submit.go
    Co-authored-by: JamesMurkin <jamesmurkin@hotmail.com>

--- internal/armada/submit/submit.go
+++ internal/armada/submit/submit.go
@@ -74,7 +74,7 @@ func NewServer(
 }
 
 // SubmitJobs allows users to submit jobs to Armada.  On receipt of a request, the following actions are performed:
-//   - The request is validated to make sure it is well formed.  If this fails ten an error is returned
+//   - The request is validated to make sure it is well formed.  If this fails then an error is returned
 //   - Each JobRequestItem inside the request is checked to see if it is a duplicate
 //   - Each non-duplicate is converted into an armadaevents.SubmitMessage
 //   - All SubmitMessages are checked to see if the job they define can be scheduled (an example of a job tht cannot
