97e01efa87d647d745df39352407bcdabe597cdf
Chris Martin <chris@cmartinit.co.uk>
2024-04-22 07:42:38 +0100

    sort out database configuration
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- developer/dependencies/postgres-init.sh
+++ developer/dependencies/postgres-init.sh
@@ -3,8 +3,10 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER docker;
-    CREATE DATABASE postgresv2;
-    GRANT ALL PRIVILEGES ON DATABASE postgresv2 TO docker;
+    CREATE DATABASE lookout;
+    GRANT ALL PRIVILEGES ON DATABASE lookout TO docker;
     CREATE DATABASE jobservice;
     GRANT ALL PRIVILEGES ON DATABASE jobservice TO docker;
+    CREATE DATABASE scheduler;
+    GRANT ALL PRIVILEGES ON DATABASE scheduler TO docker;
 EOSQL
