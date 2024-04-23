29a6b03e2fbb5b42b61e34179859f741f3b9c870
Christopher Martin <Chris.Martin@gresearch.co.uk>
2024-04-23 12:54:47 +0000

    Require that queue and jobset are set when cancelling jobs using armadactl (#118)
    * Require that queue and jobset are set when cancelling jobs using armadactl
    
    * Update cmd/armadactl/cmd/cancel.go
    
    Co-authored-by: James Murkin <James.Murkin@gresearch.co.uk>
    
    ---------
    
    Co-authored-by: James Murkin <James.Murkin@gresearch.co.uk>

--- cmd/armadactl/cmd/cancel.go
+++ cmd/armadactl/cmd/cancel.go
@@ -11,7 +11,7 @@ func cancelCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "cancel",
 		Short: "Cancels jobs in armada.",
-		Long:  `Cancels jobs either by jobId or by combination of queue & job set.`,
+		Long:  `Cancels jobs.  If queue and jobset are provided then all jobs in that jobset will be cancelled.  A job id may also be provided in which case only that job is cancelled`,
 		Args:  cobra.ExactArgs(0),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return initParams(cmd, a.Params)
@@ -23,8 +23,10 @@ func cancelCmd() *cobra.Command {
 			return a.Cancel(queue, jobSetId, jobId)
 		},
 	}
-	cmd.Flags().String("jobId", "", "job to cancel")
-	cmd.Flags().String("queue", "", "queue to cancel jobs from (requires job set to be specified)")
-	cmd.Flags().String("jobSet", "", "jobSet to cancel (requires queue to be specified)")
+	cmd.Flags().String("jobId", "", "job to cancel (optional)")
+	cmd.Flags().String("queue", "", "queue to cancel jobs from")
+	cmd.Flags().String("jobSet", "", "jobSet to cancel")
+	cmd.MarkFlagRequired("queue")
+	cmd.MarkFlagRequired("jobSet")
 	return cmd
 }
