336a648ec8d8e3054cbb72001a8f79d46404b9e2
Chris Martin <chris@cmartinit.co.uk>
2024-04-25 09:47:02 +0100

    revert migration change
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/lookoutv2/schema/schema.go
+++ internal/lookoutv2/schema/schema.go
@@ -10,7 +10,6 @@ import (
 var fs embed.FS
 
 func LookoutMigrations() ([]database.Migration, error) {
-
 	migrations, err := database.ReadMigrations(fs, "migrations")
 	if err != nil {
 		return nil, err
