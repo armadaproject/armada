f18dfc7396c2f3f6f49c22f5f59852cb6dff4bae
Carlos Santana <csantana23@gmail.com>
2024-06-14 18:57:37 -0400

    feat: Download latest version of armdactl in quickstart
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- docs/local/armadactl.sh
+++ docs/local/armadactl.sh
@@ -12,25 +12,32 @@ else
   UNARCHIVE="tar xzf -"
 fi
 
+ARCHITECTURE=arm64
+[ $(uname -m) = x86_64 ] && ARCHITECTURE=amd64
+[ $(uname -m) = aarch64 ] && ARCHITECTURE=arm64
+# if $SYSTEM equals "darwin" then set ARCHITECTURE=all
+if [ "$SYSTEM" = "darwin" ]; then
+  ARCHITECTURE=all
+fi
+
 # Find the latest Armada version
 LATEST_GH_URL=$(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/armadaproject/armada/releases/latest)
 
-# Hard version set required until https://github.com/armadaproject/armada/pull/2384 is released
-# ARMADA_VERSION=${LATEST_GH_URL##*/}
-ARMADA_VERSION="v0.3.61"
-ARMADACTL_URL="https://github.com/armadaproject/armada/releases/download/$ARMADA_VERSION/armadactl-$ARMADA_VERSION-$SYSTEM-amd64.$ARCHIVE_TYPE"
+ARMADA_VERSION=${LATEST_GH_URL##*/}
+ARMADA_VERSION_NO_V=${ARMADA_VERSION#v}
+ARMADACTL_URL="https://github.com/armadaproject/armada/releases/download/$ARMADA_VERSION/armadactl_${ARMADA_VERSION_NO_V}_${SYSTEM}_$ARCHITECTURE.$ARCHIVE_TYPE"
 
 # Download and untar/unzip armadactl
 if curl -sL $ARMADACTL_URL | sh -c "$UNARCHIVE" ; then
 	echo "armadactl downloaded successfully"
-  
+
   # Move armadactl binary to a directory in user's PATH
     TARGET_DIR="$HOME/bin" # Change this to the desired target directory in your user's home
     mkdir -p "$TARGET_DIR"
     cp armadactl "$TARGET_DIR/"
     export PATH="$TARGET_DIR:$PATH"
 
-    echo "armadactl is now available on your PATH"
+    echo "armadactl is now available on your PATH at $TARGET_DIR/armadactl"
 
 else
 	echo "Something is amiss!"
