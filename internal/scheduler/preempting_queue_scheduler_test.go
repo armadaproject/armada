1cb01976ee3f9cc0a8c77bca41f5b13d746b35ba
Chris Martin <chris@cmartinit.co.uk>
2024-06-23 09:36:40 +0100

    extra test
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/preempting_queue_scheduler_test.go
+++ internal/scheduler/preempting_queue_scheduler_test.go
@@ -1276,6 +1276,42 @@ func TestPreemptingQueueScheduler(t *testing.T) {
 				"C": 1,
 			},
 		},
+		"ProtectedFractionOfFairShare reshared": {
+			SchedulingConfig: testfixtures.WithProtectedFractionOfFairShareConfig(
+				1.0,
+				testfixtures.TestSchedulingConfig(),
+			),
+			Nodes: testfixtures.N32CpuNodes(1, testfixtures.TestPriorities),
+			Rounds: []SchedulingRound{
+				{
+					JobsByQueue: map[string][]*jobdb.Job{
+						"A": testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass2NonPreemptible, 16), // not preemptible
+						"B": testfixtures.N1Cpu4GiJobs("B", testfixtures.PriorityClass0, 11),
+						"C": testfixtures.N1Cpu4GiJobs("C", testfixtures.PriorityClass0, 3),
+						"D": testfixtures.N1Cpu4GiJobs("D", testfixtures.PriorityClass0, 2),
+					},
+					ExpectedScheduledIndices: map[string][]int{
+						"A": testfixtures.IntRange(0, 15),
+						"B": testfixtures.IntRange(0, 10),
+						"C": testfixtures.IntRange(0, 2),
+						"D": testfixtures.IntRange(0, 1),
+					},
+				},
+				{
+					// D submits one more job. No preemption occurs because B is below adjusted fair share
+					JobsByQueue: map[string][]*jobdb.Job{
+						"D": testfixtures.N1Cpu4GiJobs("D", testfixtures.PriorityClass0, 1),
+					},
+				},
+				{}, // Empty round to make sure nothing changes.
+			},
+			PriorityFactorByQueue: map[string]float64{
+				"A": 1,
+				"B": 1,
+				"C": 1,
+				"D": 1,
+			},
+		},
 		"DominantResourceFairness": {
 			SchedulingConfig: testfixtures.TestSchedulingConfig(),
 			Nodes:            testfixtures.N32CpuNodes(1, testfixtures.TestPriorities),
