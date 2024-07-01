63f96386159622296716e56db729949f3787f72a
Chris Martin <chris@cmartinit.co.uk>
2024-07-01 08:25:55 +0100

    fix
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- pkg/client/queue/queue_test.go
+++ pkg/client/queue/queue_test.go
@@ -1,6 +1,8 @@
 package queue
 
 import (
+	"encoding/json"
+	"github.com/armadaproject/armada/pkg/api"
 	"reflect"
 	"testing"
 	"testing/quick"
@@ -23,26 +25,50 @@ func TestQueue(t *testing.T) {
 }
 
 func TestQueueMarshalUnmarshal(t *testing.T) {
-	/*
-		testCase := func(queue1 Queue) bool {
-			var queue2 Queue
-			data, err := json.Marshal(queue1)
-			if err != nil {
-				t.Error(err)
-				return false
-			}
 
-			if err := json.Unmarshal(data, &queue2); err != nil {
-				t.Error(err)
-				return false
-			}
-
-			return reflect.DeepEqual(queue1, queue2)
+	testCase := func(queue1 Queue) bool {
+		var queue2 Queue
+		data, err := json.Marshal(queue1)
+		if err != nil {
+			t.Error(err)
+			return false
 		}
 
-		if err := quick.Check(testCase, nil); err != nil {
-			t.Fatal(err)
-		}*/
+		if err := json.Unmarshal(data, &queue2); err != nil {
+			t.Error(err)
+			return false
+		}
+
+		// json.Unmarshal will unmarshall empty maps to nil
+		// TODO: refactor these tests/code so this nonsense is not necessary
+		newResourceLimits := make(map[string]api.PriorityClassResourceLimits, len(queue2.ResourceLimitsByPriorityClassName))
+		for k1, v := range queue2.ResourceLimitsByPriorityClassName {
+			if v.MaximumResourceFractionByPool == nil {
+				v.MaximumResourceFractionByPool = map[string]*api.PriorityClassPoolResourceLimits{}
+			}
+			for k, v2 := range v.MaximumResourceFractionByPool {
+				if v2 != nil && v2.MaximumResourceFraction == nil {
+					v.MaximumResourceFractionByPool[k] = &api.PriorityClassPoolResourceLimits{
+						MaximumResourceFraction: map[string]float64{},
+					}
+				}
+				if v2 != nil && v2.MaximumResourceFraction == nil {
+					v2.MaximumResourceFraction = map[string]float64{}
+				}
+			}
+			if v.MaximumResourceFraction == nil {
+				v.MaximumResourceFraction = map[string]float64{}
+			}
+			newResourceLimits[k1] = v
+		}
+		queue2.ResourceLimitsByPriorityClassName = newResourceLimits
+
+		return reflect.DeepEqual(queue1, queue2)
+	}
+
+	if err := quick.Check(testCase, nil); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestQueueHasPermissionTrue(t *testing.T) {
