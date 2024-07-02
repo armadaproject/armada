167825bbd223e9932d8273696ae10029aa474dc6
Chris Martin <chris@cmartinit.co.uk>
2024-07-02 18:52:29 +0100

    lint
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/executor/util/uuid_test.go
+++ internal/executor/util/uuid_test.go
@@ -1,9 +1,10 @@
 package util
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
 
 	"github.com/armadaproject/armada/pkg/armadaevents"
 )
