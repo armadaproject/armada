889cda833ea0928318e26e119c5d26d98a21db2b
robertdavidsmith <34475852+robertdavidsmith@users.noreply.github.com>
2024-07-08 17:59:55 +0100

    Scheduler: confirm can locally calculate fields of Node in nodedb

--- internal/scheduler/nodedb/nodedb.go
+++ internal/scheduler/nodedb/nodedb.go
@@ -37,6 +37,52 @@ const (
 var empty struct{}
 
 func (nodeDb *NodeDb) create(node *schedulerobjects.Node) (*internaltypes.Node, error) {
+		if len(node.AllocatedByJobId) > 0 {
+		 panic("AllocatedByJobId")
+	}
+	if len(node.AllocatedByQueue) > 0 {
+		 panic("AllocatedByQueue")
+	}
+	if len(node.EvictedJobRunIds) > 0 {
+		 panic("EvictedJobRunIds")
+	}
+
+	allowedPriorities := []int32{}
+	for _, p := range nodeDb.nodeDbPriorities {
+		 if p != evictedPriority {
+			  allowedPriorities = append(allowedPriorities, p)
+		 }
+	}
+
+	robAllocatable := schedulerobjects.NewAllocatableByPriorityAndResourceType(
+		 allowedPriorities,
+		 node.TotalResources,
+	)
+	for p, rl := range node.NonArmadaAllocatedResources {
+		 robAllocatable.MarkAllocated(p, rl)
+	}
+
+	if len(robAllocatable) != len(node.AllocatableByPriorityAndResource) {
+		 fmt.Printf("existing %v, rob %v", node.AllocatableByPriorityAndResource, robAllocatable)
+		 panic("AllocatableByPriorityAndResource lengths")
+	}
+
+	for p, a := range node.AllocatableByPriorityAndResource {
+		 robA, exists := robAllocatable[p]
+		 if !exists {
+			  fmt.Printf("existing %v, rob %v", node.AllocatableByPriorityAndResource, robAllocatable)
+			  panic("AllocatableByPriorityAndResource missing")
+		 }
+		 cpy := a.DeepCopy()
+		 cpy.Sub(robA)
+		 if !cpy.IsZero() {
+			  fmt.Printf("existing %v, rob %v", node.AllocatableByPriorityAndResource, robAllocatable)
+			  panic("AllocatableByPriorityAndResource ne")
+		 }
+	}
+
+	fmt.Printf("All is good for %s\n", node.Name)
+
 	taints := node.GetTaints()
 	if node.Unschedulable {
 		taints = append(koTaint.DeepCopyTaintsInternStrings(taints, nodeDb.stringInterner.Intern), UnschedulableTaint())
