c6c374e28979ffb6ad89daef10dbbc717ad7198c
Chris Martin <chris@cmartinit.co.uk>
2024-07-19 16:30:59 +0100

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

---
+++ internal/scheduler/metricsnew/constants.go
@@ -0,0 +1,10 @@
+package metricsnew
+
+const (
+	MetricsPrefix = "armada_scheduler_"
+)
+
+var (
+	QueueAndPool          = []string{"queue", "pool"}
+	QueueAndPriorityClass = []string{"queue", "priority_class"}
+)
