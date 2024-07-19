24bcdf9e223e0a4f78c52b6471e6853cf7142467
JamesMurkin <jamesmurkin@hotmail.com>
2024-07-19 09:28:49 +0100

    [armadactl] Remove client side podspec validation (#3792)
    * [armadactl] Remove client side podspec validation
    
    Reasons for removal are:
     - It isn't very useful, kubernetes client side validation is pretty weak
     - It can't be used without access to the internet
    
    Alternatively we could add it back and embed the podspec schema, so it can be used offline.
    
    However I think it provides too little value in reality to worth doing right now.
    
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>
    
    * go mod tidy
    
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>
    
    ---------
    
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>

--- pkg/client/validation/submit_file.go
+++ pkg/client/validation/submit_file.go
@@ -3,11 +3,6 @@ package validation
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-
-	"github.com/instrumenta/kubeval/kubeval"
-	log "github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/armadaproject/armada/pkg/client/util"
 )
@@ -18,21 +13,6 @@ type rawJobSubmitFile struct {
 
 type rawJobRequest struct {
 	PodSpec *json.RawMessage `json:"PodSpec,omitempty"`
-}
-
-type rawKubernetesType struct {
-	metav1.TypeMeta `json:",inline"`
-	Spec            *json.RawMessage `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
-}
-
-func rawPod(spec *json.RawMessage) *rawKubernetesType {
-	return &rawKubernetesType{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pod",
-			APIVersion: "v1",
-		},
-		Spec: spec,
-	}
 }
 
 func ValidateSubmitFile(filePath string) (bool, error) {
@@ -46,31 +26,5 @@ func ValidateSubmitFile(filePath string) (bool, error) {
 		return false, errors.New("Warning: You have provided no jobs to submit.")
 	}
 
-	for i, job := range submitFile.Jobs {
-		rawPod := rawPod(job.PodSpec)
-		result, err := validate(rawPod)
-
-		if !result[0].ValidatedAgainstSchema {
-			log.Warn("Warning: Unable to validate jobs. You must be offline as pod schemas cannot be retrieved to perform validation")
-			break
-		}
-
-		if err != nil {
-			return false, err
-		}
-
-		if len(result[0].Errors) > 0 {
-			return false, fmt.Errorf("Validation error in job[%d]: %s", i, result[0].Errors[0].Description())
-		}
-	}
-
 	return true, nil
-}
-
-func validate(value *rawKubernetesType) ([]kubeval.ValidationResult, error) {
-	output, _ := json.Marshal(value)
-	config := kubeval.NewDefaultConfig()
-	config.Strict = true
-	config.IgnoreMissingSchemas = true
-	return kubeval.Validate(output, config)
 }
