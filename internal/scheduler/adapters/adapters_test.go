6231ad4934e89965f12585e045b72c781d85747d
Chris Martin <chris@cmartinit.co.uk>
2024-07-22 13:12:17 +0100

    fix some tests
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/adapters/adapters_test.go
+++ internal/scheduler/adapters/adapters_test.go
@@ -6,26 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-
-	"github.com/armadaproject/armada/internal/common/types"
-	"github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
-)
-
-const (
-	PriorityClass0 = "priority-0"
-	PriorityClass1 = "priority-1"
-	PriorityClass2 = "priority-2"
-	PriorityClass3 = "priority-3"
 )
 
 var (
-	priorityByPriorityClassName = map[string]types.PriorityClass{
-		PriorityClass0: {Priority: 0, Preemptible: true},
-		PriorityClass1: {Priority: 1, Preemptible: true},
-		PriorityClass2: {Priority: 2, Preemptible: true},
-		PriorityClass3: {Priority: 3, Preemptible: false},
-	}
-
 	priority int32 = 1
 
 	containerObj = []v1.Container{
@@ -41,21 +24,6 @@ var (
 				},
 			},
 		},
-	}
-
-	expectedResourceRequirement = v1.ResourceRequirements{
-		Limits: v1.ResourceList{
-			v1.ResourceName("cpu"):    *resource.NewMilliQuantity(5300, resource.DecimalSI),
-			v1.ResourceName("memory"): *resource.NewQuantity(5*1024*1024*1024, resource.BinarySI),
-		},
-		Requests: v1.ResourceList{
-			v1.ResourceName("cpu"):    *resource.NewMilliQuantity(300, resource.DecimalSI),
-			v1.ResourceName("memory"): *resource.NewQuantity(2*1024*1024*1024, resource.BinarySI),
-		},
-	}
-	expectedScheduler = &schedulerobjects.PodRequirements{
-		ResourceRequirements: expectedResourceRequirement,
-		PreemptionPolicy:     string(v1.PreemptLowerPriority),
 	}
 )
 
