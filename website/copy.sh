f1f3d7c5bf381d096c0b7796c9e871e84036d2a2
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-07-22 17:39:17 +0200

    update copy.sh: override existing files
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- website/copy.sh
+++ website/copy.sh
@@ -3,8 +3,8 @@
 ##### Copy markdown files from root directory and docs/ to current directory #####
 ##################################################################################
 
-# Copy CODE_OF_CONDUCT.md and CONTRIBUTING.md to _includes/ but don't overwrite existing files
-rsync -av --ignore-existing ../CODE_OF_CONDUCT.md ../CONTRIBUTING.md _includes/
+# Copy CODE_OF_CONDUCT.md and CONTRIBUTING.md to _includes/ and overwrite existing files
+rsync -av ../CODE_OF_CONDUCT.md ../CONTRIBUTING.md _includes/
 
-# Copy everything from docs/ to current directory but don't overwrite existing files
-rsync -av --ignore-existing ../docs/* ./
+# Copy everything from docs/ to current directory and overwrite existing files
+rsync -av ../docs/* ./
