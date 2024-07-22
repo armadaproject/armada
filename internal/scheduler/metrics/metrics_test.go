e1cd3e615bfbb7547604f7ab061ecbad76b0d5c5
Chris Martin <chris@cmartinit.co.uk>
2024-07-22 08:36:53 +0100

    unit test
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/metrics/metrics_test.go
+++ internal/scheduler/metrics/metrics_test.go
@@ -1,16 +1,44 @@
 package metrics
 
 import (
-	"github.com/armadaproject/armada/internal/scheduler/schedulerresult"
 	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestReportSchedulerResult(t *testing.T) {
-	result := schedulerresult.SchedulerResult{
-		PreemptedJobs:                nil,
-		ScheduledJobs:                nil,
-		NodeIdByJobId:                nil,
-		SchedulingContexts:           nil,
-		AdditionalAnnotationsByJobId: nil,
-	}
+func TestReportScheduleCycleTime(t *testing.T) {
+	metrics, err := New(nil, nil)
+	require.NoError(t, err)
+
+	// Observe some values
+	metrics.ReportScheduleCycleTime(10)
+	metrics.ReportScheduleCycleTime(11.0)
+	metrics.ReportScheduleCycleTime(12.0)
+
+	sum := testutil.ToFloat64(scheduleCycleTimeMetric.(prometheus.Collector))
+	count := testutil.CollectAndCount(scheduleCycleTimeMetric)
+
+	assert.Equal(t, 33.0, sum)
+	assert.Equal(t, 3, count)
+
+}
+
+func TestReportReconcileCycleTime(t *testing.T) {
+	metrics, err := New(nil, nil)
+	require.NoError(t, err)
+
+	// Observe some values
+	metrics.ReportReconcileCycleTime(10)
+	metrics.ReportReconcileCycleTime(11.0)
+	metrics.ReportReconcileCycleTime(12.0)
+
+	sum := testutil.ToFloat64(reconciliationCycleTimeMetric.(prometheus.Collector))
+	count := testutil.CollectAndCount(reconciliationCycleTimeMetric)
+
+	assert.Equal(t, 33.0, sum)
+	assert.Equal(t, 3, count)
+
 }
