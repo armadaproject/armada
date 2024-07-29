29c293e43ea97ab36ef96302dbbf86238c7fffb3
Chris Martin <council_tax@hotmail.com>
2024-07-29 10:01:50 +0100

    Remove StringInterner from nodedb (#3844)
    * remove stringinterner from nodedb
    
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>
    
    * fix case of labels being nil
    
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>
    
    ---------
    
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>
    Co-authored-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/gang_scheduler_test.go
+++ internal/scheduler/gang_scheduler_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/armadaproject/armada/internal/common/armadacontext"
 	armadaslices "github.com/armadaproject/armada/internal/common/slices"
-	"github.com/armadaproject/armada/internal/common/stringinterner"
 	"github.com/armadaproject/armada/internal/common/types"
 	"github.com/armadaproject/armada/internal/common/util"
 	"github.com/armadaproject/armada/internal/scheduler/configuration"
@@ -528,7 +527,6 @@ func TestGangScheduler(t *testing.T) {
 				tc.SchedulingConfig.IndexedTaints,
 				tc.SchedulingConfig.IndexedNodeLabels,
 				tc.SchedulingConfig.WellKnownNodeTypes,
-				stringinterner.New(1024),
 				testfixtures.TestResourceListFactory,
 			)
 			require.NoError(t, err)
