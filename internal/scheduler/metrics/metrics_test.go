f04cdae6d2adf0c40c2d88e4dfa5d79759e352ab
Chris Martin <chris@cmartinit.co.uk>
2024-07-29 07:31:22 +0100

    wip
    Signed-off-by: Chris Martin <chris@cmartinit.co.uk>

--- internal/scheduler/metrics/metrics_test.go
+++ internal/scheduler/metrics/metrics_test.go
@@ -1,42 +1,22 @@
 package metrics
 
 import (
-	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/armadaproject/armada/internal/scheduler/schedulerresult"
 	"github.com/stretchr/testify/require"
 	"testing"
-	"time"
 )
 
-func TestReportCycleTimes(t *testing.T) {
+func TestReportSchedulerResult(t *testing.T) {
 	tests := map[string]struct {
-		scheduleCycleTimes             []time.Duration
-		reconcileCycleTimes            []time.Duration
-		expectedScheduleCycleTimesSum  float64
-		expectedReconciliationTimesSum float64
+		schedulerResult schedulerresult.SchedulerResult
 	}{
-		//"No entries": {},
-		//"One entry": {
-		//	scheduleCycleTimes:  []time.Duration{1 * time.Second},
-		//	reconcileCycleTimes: []time.Duration{2 * time.Second},
-		//},
-		"two entries": {
-			scheduleCycleTimes:  []time.Duration{1 * time.Second, 1 * time.Millisecond},
-			reconcileCycleTimes: []time.Duration{2 * time.Second, 2 * time.Millisecond},
-		},
+		"JobsConsidered": {},
 	}
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-
 			metrics, err := New(nil, nil)
 			require.NoError(t, err)
-
-			for _, x := range tc.scheduleCycleTimes {
-				metrics.ReportScheduleCycleTime(x)
-			}
-			for _, x := range tc.reconcileCycleTimes {
-				metrics.ReportReconcileCycleTime(x)
-			}
-			testutil.ToFloat64(scheduleCycleTimeMetric)
+			metrics.ReportSchedulerResult(x)
 		})
 	}
 }
