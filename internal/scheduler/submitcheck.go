0e7307cb9f3101a0d2e3b6a5f74de8c0b6e3fdc1
Eleanor Pratt <101330560+eleanorpratt@users.noreply.github.com>
2024-09-05 17:44:59 +0100

    remove DummySubmitCheck from config and remove logic that used it (#219) (#3910)
    Co-authored-by: Eleanor Pratt <Eleanor.Pratt@gresearch.co.uk>

--- internal/scheduler/submitcheck.go
+++ internal/scheduler/submitcheck.go
@@ -41,17 +41,6 @@ type SubmitScheduleChecker interface {
 	Check(ctx *armadacontext.Context, jobs []*jobdb.Job) (map[string]schedulingResult, error)
 }
 
-// DummySubmitChecker  is a  SubmitScheduleChecker that allows every job
-type DummySubmitChecker struct{}
-
-func (srv *DummySubmitChecker) Check(_ *armadacontext.Context, jobs []*jobdb.Job) (map[string]schedulingResult, error) {
-	results := make(map[string]schedulingResult, len(jobs))
-	for _, job := range jobs {
-		results[job.Id()] = schedulingResult{isSchedulable: true}
-	}
-	return results, nil
-}
-
 type SubmitChecker struct {
 	schedulingConfig    configuration.SchedulingConfig
 	executorRepository  database.ExecutorRepository
