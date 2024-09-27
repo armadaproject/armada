cc61d2f9991d5da1e045612f5df3172688e9d446
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2024-09-27 15:47:29 +0200

    add deps for generated javax.annotation

--- client/java/src/main/java/armadaproject/Main.java
+++ client/java/src/main/java/armadaproject/Main.java
@@ -1,9 +1,11 @@
 package armadaproject;
 
+import api.EventGrpc;
+
 public class Main {
 
   public static void main(String[] args) {
-    
+    EventGrpc.EventBlockingStub stub = EventGrpc.newBlockingStub(null);
   }
 
 }
