8cc24b4460f1efa77db79e965fa1ae1619a703e9
Chris Martin <chris@cmartinit.co.uk>
2024-12-11 08:04:10 +0000

    remove mocks

--- internal/common/mocks/generate.go
+++ internal/common/mocks/generate.go
@@ -3,5 +3,3 @@ package mocks
 // Mock implementations used by tests
 //go:generate mockgen -destination=./mock_pulsar.go -package=mocks "github.com/apache/pulsar-client-go/pulsar" Client,Producer,Message
 //go:generate mockgen -destination=./mock_executorapi.go -package=mocks "github.com/armadaproject/armada/pkg/executorapi" ExecutorApiClient,ExecutorApi_LeaseJobRunsClient
-//go:generate mockgen -destination=./controlplaneevents/mock_publisher.go -package=controlplaneevents "github.com/armadaproject/armada/internal/common/pulsarutils/controlplaneevents" Publisher
-//go:generate mockgen -destination=./jobsetevents/mock_publisher.go -package=jobsetevents "github.com/armadaproject/armada/internal/common/pulsarutils/jobsetevents" Publisher
