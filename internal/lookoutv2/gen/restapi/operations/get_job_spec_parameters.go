b26683c9a0d6cc0690367df49a6af16f9bba8085
Maurice Yap <Maurice.Yap@gresearch.co.uk>
2025-01-09 13:51:10 +0000

    Run mage generate; Add VS Code Go debugger files to gitignore (#324)
    Raising this as a separate PR to keep my future PR with changes to generated code focused on the logical changes.

--- internal/lookoutv2/gen/restapi/operations/get_job_spec_parameters.go
+++ internal/lookoutv2/gen/restapi/operations/get_job_spec_parameters.go
@@ -6,7 +6,6 @@ package operations
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"context"
 	"io"
 	"net/http"
 
@@ -64,7 +63,7 @@ func (o *GetJobSpecParams) BindRequest(r *http.Request, route *middleware.Matche
 				res = append(res, err)
 			}
 
-			ctx := validate.WithOperationRequest(context.Background())
+			ctx := validate.WithOperationRequest(r.Context())
 			if err := body.ContextValidate(ctx, route.Formats); err != nil {
 				res = append(res, err)
 			}
