5833afb132b65bf1ee056c325ef4645d7f22d0c9
Chris Martin <chris@cmartinit.co.uk>
2025-01-14 07:31:33 +0000

    add prometheus hook

--- internal/common/logging/prometheus.go
+++ internal/common/logging/prometheus.go
@@ -5,13 +5,13 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// implements zerolog.Hook
-type prometheusHook struct {
+// PrometheusHook implements zerolog.Hook
+type PrometheusHook struct {
 	counters map[zerolog.Level]prometheus.Counter
 }
 
 // NewPrometheusHook creates and registers Prometheus counters for each log level.
-func NewPrometheusHook() *prometheusHook {
+func NewPrometheusHook() *PrometheusHook {
 	counters := make(map[zerolog.Level]prometheus.Counter)
 
 	for _, level := range []zerolog.Level{
@@ -31,10 +31,10 @@ func NewPrometheusHook() *prometheusHook {
 		prometheus.MustRegister(counter)
 		counters[level] = counter
 	}
-	return &prometheusHook{counters: counters}
+	return &PrometheusHook{counters: counters}
 }
 
-func (h *prometheusHook) Run(_ *zerolog.Event, level zerolog.Level, _ string) {
+func (h *PrometheusHook) Run(_ *zerolog.Event, level zerolog.Level, _ string) {
 	if counter, ok := h.counters[level]; ok {
 		counter.Inc()
 	}
