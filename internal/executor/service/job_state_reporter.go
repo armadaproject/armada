cac263f3e6bf5fecebc1b0e4433e01c80ff42385
JamesMurkin <jamesmurkin@hotmail.com>
2025-01-20 13:38:12 +0000

    Use common logger
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>

--- internal/executor/service/job_state_reporter.go
+++ internal/executor/service/job_state_reporter.go
@@ -3,10 +3,10 @@ package service
 import (
 	"time"
 
-	log "github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
 
+	log "github.com/armadaproject/armada/internal/common/logging"
 	clusterContext "github.com/armadaproject/armada/internal/executor/context"
 	domain2 "github.com/armadaproject/armada/internal/executor/domain"
 	"github.com/armadaproject/armada/internal/executor/reporter"
