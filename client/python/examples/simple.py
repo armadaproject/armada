a33ddc2cc4eaf1bab6bc40a3119e78bf4f057efe
Rich Scott <richscott@sent.com>
2025-01-20 09:44:01 -0700

    Revert python test change to master version

--- client/python/examples/simple.py
+++ client/python/examples/simple.py
@@ -54,8 +54,7 @@ def simple_workflow():
     """
 
     # The queue and job_set_id that will be used for all jobs
-    # queue = f"simple-queue-{uuid.uuid1()}"
-    queue = "simple-test"
+    queue = f"simple-queue-{uuid.uuid1()}"
     job_set_id = f"simple-jobset-{uuid.uuid1()}"
 
     # Ensures that the correct channel type is generated
