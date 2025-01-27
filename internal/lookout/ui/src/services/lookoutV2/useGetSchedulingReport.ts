0a60f20845df7fd9094d160b3ec169005e4c312d
Maurice Yap <Maurice.Yap@gresearch.co.uk>
2025-01-27 12:42:28 +0000

    Authenticate requests for scheduling reports
    On the Lookout UI, the fetch requests for scheduling reports require authentication via the user's access token.

--- internal/lookout/ui/src/services/lookoutV2/useGetSchedulingReport.ts
+++ internal/lookout/ui/src/services/lookoutV2/useGetSchedulingReport.ts
@@ -1,20 +1,32 @@
 import { useQuery } from "@tanstack/react-query"
 
 import { useGetUiConfig } from "./useGetUiConfig"
+import { getAccessToken, getAuthorizationHeaders, useUserManager } from "../../oidc"
 import { SchedulerReportingApi, Configuration, SchedulerobjectsSchedulingReport } from "../../openapi/schedulerobjects"
 import { getErrorMessage } from "../../utils"
 
 export const useGetSchedulingReport = (verbosity: number, enabled = true) => {
+  const userManager = useUserManager()
+
   const { data: uiConfig } = useGetUiConfig(enabled)
   const armadaApiBaseUrl = uiConfig?.armadaApiBaseUrl
 
-  const schedulerReportingApiConfiguration: Configuration = new Configuration({ basePath: armadaApiBaseUrl })
+  const schedulerReportingApiConfiguration: Configuration = new Configuration({
+    basePath: armadaApiBaseUrl,
+    credentials: "include",
+  })
   const schedulerReportingApi = new SchedulerReportingApi(schedulerReportingApiConfiguration)
 
   return useQuery<SchedulerobjectsSchedulingReport, string>({
     queryKey: ["getSchedulingReport", verbosity],
     queryFn: async ({ signal }) => {
       try {
+        const headers: HeadersInit = {}
+
+        if (userManager !== undefined) {
+          Object.assign(headers, getAuthorizationHeaders(await getAccessToken(userManager)))
+        }
+
         return await schedulerReportingApi.getSchedulingReport({ verbosity }, { signal })
       } catch (e) {
         throw await getErrorMessage(e)
