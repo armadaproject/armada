620c0d1deb43ebadc98d2994d30fccddb5d8a005
Chris Martin <chris@cmartinit.co.uk>
2025-02-11 07:55:07 +0000

    remove gogo

--- internal/scheduler/simulator/test_utils.go
+++ internal/scheduler/simulator/test_utils.go
@@ -9,6 +9,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 
+	protoutil "github.com/armadaproject/armada/internal/common/proto"
 	armadaslices "github.com/armadaproject/armada/internal/common/slices"
 	"github.com/armadaproject/armada/internal/common/types"
 	"github.com/armadaproject/armada/internal/scheduler/configuration"
@@ -90,7 +91,7 @@ func GetBasicSchedulingConfig() configuration.SchedulingConfig {
 func NodeTemplate32Cpu(n int64) *NodeTemplate {
 	return &NodeTemplate{
 		Number: n,
-		TotalResources: schedulerobjects.ResourceList{
+		TotalResources: &schedulerobjects.ResourceList{
 			Resources: map[string]resource.Quantity{
 				"cpu":    resource.MustParse("32"),
 				"memory": resource.MustParse("256Gi"),
@@ -102,7 +103,7 @@ func NodeTemplate32Cpu(n int64) *NodeTemplate {
 func NodeTemplateGpu(n int64) *NodeTemplate {
 	return &NodeTemplate{
 		Number: n,
-		TotalResources: schedulerobjects.ResourceList{
+		TotalResources: &schedulerobjects.ResourceList{
 			Resources: map[string]resource.Quantity{
 				"cpu":            resource.MustParse("128"),
 				"memory":         resource.MustParse("4096Gi"),
@@ -128,7 +129,7 @@ func WithDependenciesJobTemplate(jobTemplate *JobTemplate, dependencyIds ...stri
 }
 
 func WithMinSubmitTimeJobTemplate(jobTemplate *JobTemplate, minSubmitTime time.Duration) *JobTemplate {
-	jobTemplate.EarliestSubmitTime = minSubmitTime
+	jobTemplate.EarliestSubmitTime = protoutil.ToDuration(minSubmitTime)
 	return jobTemplate
 }
 
@@ -145,7 +146,8 @@ func JobTemplate32Cpu(n int64, jobSet, priorityClassName string) *JobTemplate {
 				},
 			},
 		},
-		RuntimeDistribution: ShiftedExponential{Minimum: 1 * time.Minute},
+
+		RuntimeDistribution: &ShiftedExponential{Minimum: protoutil.ToDuration(1 * time.Minute)},
 	}
 }
 
@@ -168,7 +170,7 @@ func JobTemplate1Cpu(n int64, jobSet, priorityClassName string) *JobTemplate {
 				},
 			},
 		},
-		RuntimeDistribution: ShiftedExponential{Minimum: time.Minute},
+		RuntimeDistribution: &ShiftedExponential{Minimum: protoutil.ToDuration(time.Minute)},
 	}
 }
 
