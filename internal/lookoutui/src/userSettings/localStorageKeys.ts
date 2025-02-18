823b4e4455b26e4cad28d57be3eac2daf4e6b59c
Maurice Yap <mauriceyap@hotmail.co.uk>
2025-02-18 14:04:07 +0000

    Allow user to set font size and text wrapping for logs display (#4216)
    In the Lookout UI, this allows users to set the following preferences, which are each stored and persisted in local storage:
    - text wrapping of job run logs
    - text size of job run logs (small/medium/large)
    - whether to show timestamps for job run log lines (this was not previously persisted)
    - text wrapping for code snippets, such as commands

---
+++ internal/lookoutui/src/userSettings/localStorageKeys.ts
@@ -0,0 +1,7 @@
+const LOCAL_STORAGE_KEY_PREFIX = "lookout-user-settings-"
+
+export const JOB_RUN_LOGS_SHOW_TIMESTAMPS_KEY = `${LOCAL_STORAGE_KEY_PREFIX}job-run-logs-show-timestamps`
+export const JOB_RUN_LOGS_TEXT_SIZE_KEY = `${LOCAL_STORAGE_KEY_PREFIX}job-run-logs-text-size`
+export const JOB_RUN_LOGS_WRAP_LINES_KEY = `${LOCAL_STORAGE_KEY_PREFIX}job-run-logs-wrap-lines`
+
+export const CODE_SNIPPETS_WRAP_LINES_KEY = `${LOCAL_STORAGE_KEY_PREFIX}code-snippets-wrap-lines`
