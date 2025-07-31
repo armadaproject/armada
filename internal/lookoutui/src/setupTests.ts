c3eaac1fa423f290622cb9ca83a2d6fd08c20ead
Maurice Yap <mauriceyap@hotmail.co.uk>
2025-07-31 13:52:49 +0100

    Update setupTests.ts

--- internal/lookoutui/src/setupTests.ts
+++ internal/lookoutui/src/setupTests.ts
@@ -3,3 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+
+export const FAKE_ARMADA_API_BASE_URL = "https://test-armada-api.armada.com"
