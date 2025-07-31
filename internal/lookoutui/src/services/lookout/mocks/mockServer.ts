3f2acb59bce9513c327a2f977a1d042ba5539e17
Maurice Yap <mauriceyap@hotmail.co.uk>
2025-07-31 16:35:34 +0100

    Fix unhandled request warning in Lookout UI tests (#4425)
    When running the tests in `Sidebar.test.tsx`, we get the following error
    because the fetch requests to retrieve the job run error and job run
    debug message are not being mocked:
    ```
    [MSW] Error: intercepted a request without a matching request handler:
    
      • POST /api/v1/jobRunError
    
      • Request body: {"runId":"1234-5678"}
    
    If you still wish to intercept this unhandled request, please create a request handler for it.
    ```
    
    This PR removes that warning by creating the request handlers.

--- internal/lookoutui/src/services/lookout/mocks/mockServer.ts
+++ internal/lookoutui/src/services/lookout/mocks/mockServer.ts
@@ -4,6 +4,9 @@ import { setupServer, SetupServerApi } from "msw/node"
 import { FAKE_ARMADA_API_BASE_URL } from "../../../setupTests"
 
 const GET_QUEUES_ENDPOINT = `${FAKE_ARMADA_API_BASE_URL}/v1/batched/queues`
+const POST_JOB_RUN_ERROR_ENDPOINT = "/api/v1/jobRunError"
+const POST_JOB_RUN_DEBUG_MESSAGE_ENDPOINT = "/api/v1/jobRunDebugMessage"
+
 export class MockServer {
   private server: SetupServerApi
 
@@ -35,6 +38,31 @@ export class MockServer {
             )
             .join("\n"),
         ),
+      ),
+    )
+  }
+
+  setPostJobRunErrorResponseForRunId(runId: string, errorMessage: string) {
+    this.server.use(
+      http.post<object, { runId: string }, { errorMessage: string }>(POST_JOB_RUN_ERROR_ENDPOINT, async (req) => {
+        const reqJson = await req.request.json()
+        if (reqJson.runId === runId) {
+          return HttpResponse.json({ errorMessage })
+        }
+      }),
+    )
+  }
+
+  setPostJobRunDebugMessageResponseForRunId(runId: string, errorMessage: string) {
+    this.server.use(
+      http.post<object, { runId: string }, { errorMessage: string }>(
+        POST_JOB_RUN_DEBUG_MESSAGE_ENDPOINT,
+        async (req) => {
+          const reqJson = await req.request.json()
+          if (reqJson.runId === runId) {
+            return HttpResponse.json({ errorMessage })
+          }
+        },
       ),
     )
   }
