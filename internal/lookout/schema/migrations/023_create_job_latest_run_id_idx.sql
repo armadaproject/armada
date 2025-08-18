62020a0ad9793aa664c75509d97b939fd1f57d63
Maurice Yap <mauriceyap@hotmail.co.uk>
2025-08-18 09:19:51 +0100

    fillfactor 80

--- internal/lookout/schema/migrations/023_create_job_latest_run_id_idx.sql
+++ internal/lookout/schema/migrations/023_create_job_latest_run_id_idx.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_job_latest_run_id ON job (latest_run_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_job_latest_run_id ON job (latest_run_id) WITH (fillfactor = 80);
