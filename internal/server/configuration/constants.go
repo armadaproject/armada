415d04d1dc3eada4bb9e2aae58b89d5a853cb0d6
Mustafa Ilyas <Mustafa.Ilyas@gresearch.co.uk>
2025-09-26 17:22:42 +0100

    ARMADA-3619 - Filtering irrelevant annotations from submitted jobs (#452)
    * ARMADA-3619 - Filtering irrelevant annotations from submitted jobs in the scheduler ingester
    
    * Code cleanup

--- internal/server/configuration/constants.go
+++ internal/server/configuration/constants.go
@@ -24,9 +24,14 @@ var schedulingAnnotations = map[string]bool{
 	GangCardinalityAnnotation:         true,
 	GangNodeUniformityLabelAnnotation: true,
 	FailFastAnnotation:                true,
+	JobPriceBand:                      true,
 }
 
 func IsSchedulingAnnotation(annotation string) bool {
 	_, ok := schedulingAnnotations[annotation]
 	return ok
+}
+
+func SchedulingAnnotationCount() int {
+	return len(schedulingAnnotations)
 }
