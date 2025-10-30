3901e8f9ae282bde352f2be38f522188cd80756f
Martynas Asipauskas <me@martynas.co.uk>
2025-10-30 09:06:53 +0000

    Update and rename 026_update_cancellation_index.sql to 026_drop_cancellation_index_for_queue_jobset.sql

--- internal/scheduler/database/migrations/026_drop_cancellation_index_for_queue_jobset.sql
+++ internal/scheduler/database/migrations/026_drop_cancellation_index_for_queue_jobset.sql
@@ -1,4 +1,3 @@
 -- Drop the old cancellation index
 DROP INDEX CONCURRENTLY IF EXISTS idx_queue_job_set_cancelled_succeeded_failed;
--- Drop the old terminal jobs index
-DROP INDEX CONCURRENTLY IF EXISTS idx_jobs_terminal;
+
