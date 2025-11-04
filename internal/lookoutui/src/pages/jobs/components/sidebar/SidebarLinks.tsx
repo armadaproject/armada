8d552832508942beef70ec17ab1dd0d1c90060cf
Maurice Yap <mauriceyap@hotmail.co.uk>
2025-11-04 10:57:31 -0600

    [Lookout] allow configuration of job links in the sidebar
    Allows configuration for link chips to appear at the top of the job table sidebar, based on values from the job spec, such as annotation values. This is useful for better integrating with external tooling, such as CI/CD. workflow orchestration, MLOps and data processing.
    
    Links will not be rendered for jobs which do not have values present, which are required to generate a link according to the configuration,
    
    For example, we can add this to the Lookout configuration:
    ```yaml
    uiConfig:
      jobLinks:
        - label: Apache Airflow Job
          colour: "#00c7d4"
          linkTemplate: 'https://your-airflow.internal/dags/{{ annotations["armadaproject.io/apache-airflow-dag-id"] }}/grid?dag_run_id={{ annotations["armadaproject.io/apache-airflow-dag-run-id"] }}&task_id={{ annotations["armadaproject.io/apache-airflow-task-id"] }}'
        - label: Jenkins build
          colour: "#335061"
          linkTemplate: 'https://your-jenkins.internal/job/{{ annotations["armadaproject.io/jenkins-job-name"] }}/{{ annotations["armadaproject.io/jenkins-build-number"] }}'
        - label: MLflow run
          colour: "#3578e5"
          linkTemplate: 'https://your-mlflow.internal/#/experiments/{{ annotations["armadaproject.io/mlflow-experiment-id"] }}/runs/{{ annotations["armadaproject.io/mlflow-run-id"] }}'
    ```
    
    It would cause Lookout to render three link chips in the sidebar for a job with these annotations:
    ```yaml
    jobs:
      - annotations:
        armadaproject.io/apache-airflow-dag-id: my_dag_id
          armadaproject.io/apache-airflow-dag-run-id: run_20250101
          armadaproject.io/apache-airflow-task-id: "my_task"
          armadaproject.io/jenkins-job-name: "my_job"
          armadaproject.io/jenkins-build-number: "1234"
          armadaproject.io/mlflow-experiment-id: "5"
          armadaproject.io/mlflow-run-id: abcdabcd-0000-0000-0000-0123456789ab
    ```

---
+++ internal/lookoutui/src/pages/jobs/components/sidebar/SidebarLinks.tsx
@@ -0,0 +1,87 @@
+import { Link } from "@mui/icons-material"
+import { Stack, Chip, alpha, styled } from "@mui/material"
+import { blend } from "@mui/system"
+import { template } from "lodash"
+
+import { SPACING } from "../../../../common/spacing"
+import { getConfig } from "../../../../config"
+import { Job } from "../../../../models/lookoutModels"
+
+const LinkChip = styled(Chip<"a">)<{ linkChipColour: string }>(({ theme, linkChipColour }) => ({
+  backgroundColor: linkChipColour,
+  color: theme.palette.getContrastText(linkChipColour),
+  transitionProperty: "background-color, box-shadow, color",
+
+  "&:hover": {
+    backgroundColor: alpha(linkChipColour, 0.5),
+    color: theme.palette.getContrastText(blend(theme.palette.background.paper, linkChipColour, 0.5)),
+  },
+}))
+
+const { jobLinks, userAnnotationPrefix } = getConfig()
+
+interface JobLink {
+  label: string
+  colour: string
+  href: string
+}
+
+const getJobLinks = (job: Job): JobLink[] => {
+  // Template links using all job annoation keys, plus all job annotation keys prepended with userAnnotationPrefix
+  const templateContext = { ...job, annotations: { ...job.annotations } }
+  Object.entries(templateContext.annotations).forEach(([k, v]) => {
+    templateContext.annotations[userAnnotationPrefix + k] = v
+  })
+  // Wrap job annotations in a Proxy that throws on missing annotation keys.
+  templateContext.annotations = new Proxy(templateContext.annotations, {
+    get(target: any, prop) {
+      if (!(prop in target)) {
+        throw new Error(`Missing key: ${String(prop)}`)
+      }
+      return target[prop]
+    },
+  })
+
+  return jobLinks.flatMap<JobLink>(({ label, colour, linkTemplate }) => {
+    try {
+      const compiledTemplate = template(linkTemplate, { interpolate: /{{([\s\S]+?)}}/g })
+      const templated = compiledTemplate(templateContext)
+      return [{ label, colour, href: templated }]
+    } catch {
+      // Do not return a link if templating fails for any reason
+      return []
+    }
+  })
+}
+
+export interface SidebarLinksProps {
+  job: Job
+}
+
+export const SidebarLinks = ({ job }: SidebarLinksProps) => {
+  const linksForJob = getJobLinks(job)
+
+  if (linksForJob.length === 0) {
+    return null
+  }
+
+  return (
+    <Stack spacing={SPACING.xs} direction="row">
+      {linksForJob.map(({ label, colour, href }) => (
+        <div key={label + colour + href}>
+          <LinkChip
+            component="a"
+            clickable
+            label={label}
+            href={href}
+            target="_blank"
+            variant="filled"
+            size="small"
+            icon={<Link color="inherit" />}
+            linkChipColour={colour}
+          />
+        </div>
+      ))}
+    </Stack>
+  )
+}
