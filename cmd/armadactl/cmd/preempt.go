ef1e327d0e694dfe729c8dce39979d1677223f9a
Nikola Jokic <jokicnikola07@gmail.com>
2026-01-29 14:26:44 +0100

    Add preempt and cancel jobs on node for specified executor (#4612)
    <!-- Thanks for sending a pull request! Here are some tips for you: -->
    
    #### What type of PR is this?
    
    Enhancement
    
    #### What this PR does / why we need it
    
    Jobs may need to be preempted or canceled on a node. The server and
    `armadactl` should support these use cases natively.
    
    ---------
    
    Signed-off-by: Nikola Jokic <jokicnikola07@gmail.com>

--- cmd/armadactl/cmd/preempt.go
+++ cmd/armadactl/cmd/preempt.go
@@ -19,6 +19,7 @@ func preemptCmd() *cobra.Command {
 	cmd.AddCommand(
 		preemptJobCmd(),
 		preemptExecutorCmd(),
+		preemptNodeCmd(),
 		preemptQueuesCmd(),
 	)
 	return cmd
@@ -81,6 +82,64 @@ func preemptExecutorCmd() *cobra.Command {
 		"Preempt jobs on executor matching the specified queue names. If no queues are provided, jobs across all queues will be preempted. Provided queues should be comma separated, as in the following example: queueA,queueB,queueC.",
 	)
 	cmd.Flags().StringSliceP("priority-classes", "p", []string{}, "Preempt jobs on executor matching the specified priority classes. Provided priority classes should be comma separated, as in the following example: armada-default,armada-preemptible.")
+	return cmd
+}
+
+func preemptNodeCmd() *cobra.Command {
+	a := armadactl.New()
+	cmd := &cobra.Command{
+		Use:   "node <name>",
+		Short: "Preempts jobs on node for specified executor.",
+		Long:  `Preempts jobs on node for specified exeucotr with provided node name, executor name, priority classes and queues.`,
+		Args:  cobra.ExactArgs(1),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmd.MarkFlagRequired("priority-classes"); err != nil {
+				return fmt.Errorf("error marking priority-class flag as required: %s", err)
+			}
+			if err := cmd.MarkFlagRequired("executor"); err != nil {
+				return fmt.Errorf("error marking executor flag as required: %s", err)
+			}
+			return initParams(cmd, a.Params)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			onNode := args[0]
+
+			priorityClasses, err := cmd.Flags().GetStringSlice("priority-classes")
+			if err != nil {
+				return fmt.Errorf("error reading priority-class selection: %s", err)
+			}
+
+			queues, err := cmd.Flags().GetStringSlice("queues")
+			if err != nil {
+				return fmt.Errorf("error reading queue selection: %s", err)
+			}
+
+			executor, err := cmd.Flags().GetString("executor")
+			if err != nil {
+				return fmt.Errorf("error reading executor flag: %s", err)
+			}
+
+			return a.PreemptOnNode(onNode, executor, queues, priorityClasses)
+		},
+	}
+	cmd.Flags().StringSliceP(
+		"queues",
+		"q",
+		[]string{},
+		"Preempt jobs on node for specified executor matching the specified queue names. If no queues are provided, jobs across all queues will be preempted. Provided queues should be comma separated, as in the following example: queueA,queueB,queueC.",
+	)
+	cmd.Flags().StringSliceP(
+		"priority-classes",
+		"p",
+		[]string{},
+		"Preempt jobs on node for specified executor matching the specified priority classes. Provided priority classes should be comma separated, as in the following example: armada-default,armada-preemptible.",
+	)
+	cmd.Flags().StringP(
+		"executor",
+		"e",
+		"",
+		"Name of the executor that owns the node where jobs will be preempted.",
+	)
 	return cmd
 }
 
