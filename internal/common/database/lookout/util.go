9060ffdec9215f8fb3cb79874f254d5cc585cea7
Maurice Yap <mauriceyap@hotmail.co.uk>
2026-04-23 09:48:38 +0100

    Run all existing Lookout database tests on Lookout HC
    Extend WithLookoutDb to also run against the lookout HC schema, and
    update queryapi tests to use UpsertPartitionedWithTransaction. No
    changes to application request-handling code.
    
    Signed-off-by: Maurice Yap <mauriceyap@hotmail.co.uk>

--- internal/common/database/lookout/util.go
+++ internal/common/database/lookout/util.go
@@ -4,13 +4,22 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/armadaproject/armada/internal/common/database"
-	"github.com/armadaproject/armada/internal/lookout/schema"
+	lookoutschema "github.com/armadaproject/armada/internal/lookout/schema"
+	lookouthcschema "github.com/armadaproject/armada/internal/lookouthc/schema"
 )
 
 func WithLookoutDb(action func(db *pgxpool.Pool) error) error {
-	migrations, err := schema.LookoutMigrations()
+	lookoutMigrations, err := lookoutschema.LookoutMigrations()
 	if err != nil {
 		return err
 	}
-	return database.WithTestDb(migrations, action)
+	if err := database.WithTestDb(lookoutMigrations, action); err != nil {
+		return err
+	}
+
+	lookoutHCMigrations, err := lookouthcschema.LookoutHCMigrations()
+	if err != nil {
+		return err
+	}
+	return database.WithTestDb(lookoutHCMigrations, action)
 }
