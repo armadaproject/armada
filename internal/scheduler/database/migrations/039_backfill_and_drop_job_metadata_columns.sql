05f81a7e822cf17fc41184fb3c971356da558678
Trey Guckian <24757349+tgucks@users.noreply.github.com>
2026-06-26 08:32:30 -0500

    Simplify migration to improve plan
    Signed-off-by: Trey Guckian <24757349+tgucks@users.noreply.github.com>

--- internal/scheduler/database/migrations/039_backfill_and_drop_job_metadata_columns.sql
+++ internal/scheduler/database/migrations/039_backfill_and_drop_job_metadata_columns.sql
@@ -6,7 +6,6 @@ INSERT INTO job_metadata (job_id, submit_message, groups)
 SELECT j.job_id, j.submit_message, j.groups
 FROM jobs j
 WHERE j.submit_message IS NOT NULL
-  AND NOT EXISTS (SELECT 1 FROM job_metadata jm WHERE jm.job_id = j.job_id)
 ON CONFLICT (job_id) DO NOTHING;
 
 ALTER TABLE jobs DROP COLUMN IF EXISTS submit_message;
