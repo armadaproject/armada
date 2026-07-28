22053d66eeb3d5f34c59e4708041ebc84adbde87
JamesMurkin <jamesmurkin@hotmail.com>
2026-07-28 15:56:58 +0100

    Fix job eviction ordering bug (#5053)
    Currently when jobs are evicted, we do small scheduling round to decide
    which order these jobs are evicted in
    - This simply determines which order preempting jobs should consider the
    evicted jobs for preemption
    
    However there is a bug where the allocation isn't getting updated during
    this scheduling round, resulting in the code falling back to
    tie-breaking on queue each time
    
    In effect this means evicted jobs are purely ordered by queue name, so
    queue A's evicted jobs are all at the front of the evicted list, then
    queue B's and so on
    
    An example of this bug shown below, each letter represents a job in the
    named A - i.e A = job in queue A
    
    Before PR - evicted order of jobs :
    `A A A B B B C C C`
    
    After PR - evicted order of jobs:
    `A B C A B C A B C`
    
    This is clearly unfair on certain queues and not the intended behaviour.
    
    This PR fixes the bug and adds a test to assert jobs get evicted back in
    the appropriate order
    
    ---------
    
    Signed-off-by: JamesMurkin <jamesmurkin@hotmail.com>

--- internal/scheduler/scheduling/preempting_queue_scheduler_test.go
+++ internal/scheduler/scheduling/preempting_queue_scheduler_test.go
@@ -152,7 +152,8 @@ func TestEvictOversubscribed(t *testing.T) {
 	evictor := NewOversubscribedEvictor(
 		testQueueContextChecker{},
 		jobDbTxn,
-		nodeDb)
+		nodeDb,
+	)
 	result, err := evictor.Evict(armadacontext.Background(), nodeDbTxn)
 	require.NoError(t, err)
 
@@ -1222,9 +1223,11 @@ func TestPreemptingQueueScheduler(t *testing.T) {
 			Rounds: []SchedulingRound{
 				{
 					JobsByQueue: map[string][]*jobdb.Job{
-						"A": append(append(
-							testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass0, 10),
-							testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass1, 10)...),
+						"A": append(
+							append(
+								testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass0, 10),
+								testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass1, 10)...,
+							),
 							testfixtures.N1Cpu4GiJobs("A", testfixtures.PriorityClass2, 10)...,
 						),
 					},
@@ -2368,7 +2371,8 @@ func TestPreemptingQueueScheduler(t *testing.T) {
 					armadaslices.Map(
 						maps.Keys(tc.PriorityFactorByQueue),
 						func(qn string) *api.Queue { return &api.Queue{Name: qn} },
-					))
+					),
+				)
 				sctx.UpdateFairShares()
 
 				tc.SchedulingConfig.EnablePreferLargeJobOrdering = true
