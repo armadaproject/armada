0d54c0652edc1c89c5c4666ef26b03bb76e81b7a
Gabriel Galera <gabriel.galera@nmc2.ai>
2026-09-25 10:37:02 -0500

    Regenerate Armada event protobuf
    Signed-off-by: Gabriel Galera <gabriel.galera@nmc2.ai>

--- pkg/armadaevents/events.pb.go
+++ pkg/armadaevents/events.pb.go
@@ -5,15 +5,17 @@ package armadaevents
 
 import (
 	fmt "fmt"
-	schedulerobjects "github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+
 	proto "github.com/gogo/protobuf/proto"
 	types "github.com/gogo/protobuf/types"
-	io "io"
 	v11 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/networking/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
-	math "math"
-	math_bits "math/bits"
+
+	schedulerobjects "github.com/armadaproject/armada/internal/scheduler/schedulerobjects"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
